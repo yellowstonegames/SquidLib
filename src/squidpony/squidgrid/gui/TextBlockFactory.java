ca129ade355de51a7332304870186afb58cf47d8
Eben Howard <aimaisan@hotmail.com>
2012-04-23 13:17:51 -0600

    Backwards compatibility broken!!!
    Changed from row, column to panelWidth, panelHeight effectively switching the order. Old code will have the width and height of the panel be opposite of what is expected.
    
    Interfaces removed, structure streamlined

--- src/squidpony/squidgrid/gui/TextBlockFactory.java
+++ src/squidpony/squidgrid/gui/TextBlockFactory.java
@@ -1,57 +1,78 @@
-package squidpony.squidgrid;
+package squidpony.squidgrid.gui;
 
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.TreeMap;
 
 /**
- * Singleton class for creating text blocks. Stores images in a TreeMap to
- * prevent duplicate object creation where not needed. For applications with
- * many repeated text blocks, this is an ideal way to hold and create them.
+ * Singleton class for creating text blocks.
  *
- * @author SquidPony
+ * The default characters guaranteed to fit are ASCII 33 through 125, which are
+ * the commonly used symbols, numbers, and letters. Whitespace defaults to being
+ * used on all characters.
+ *
+ * @author Eben Howard - http://squidpony.com
  */
 public class TextBlockFactory {
     private static TextBlockFactory instance = new TextBlockFactory();
-    TreeMap<String, SGImageBlock> blocks = new TreeMap<String, SGImageBlock>();
-    int width = 10, height = 60;
+    TreeMap<String, BufferedImage> blocks = new TreeMap<String, BufferedImage>();
+    int cellWidth = 10, cellHeight = 60;
     int verticalOffset = 0;//how far the baseline needs to be moved based on squeezing the cell size vertically
     Font font;
+    char[] fitting;
+    private boolean whiteSpace = true;
 
     private TextBlockFactory() {
-    }
-
-    public void initializeByFont(Font font, boolean whiteSpace) {
-        this.font = font;
-        blocks = new TreeMap<String, SGImageBlock>();
-        verticalOffset = 0;
-        sizeCellByFont(whiteSpace);
-        blocks = new TreeMap<String, SGImageBlock>();
+        fitting = new char[126 - 33];
+        for (char c = 33; c <= 125; c++) {
+            fitting[c - 33] = c;
+        }
     }
 
     /**
-     * Sets up the factory to provide images based on the cell width and height
-     * passed in. The font size will be used as a starting point, and reduced as
-     * needed to fit within the cell.
+     * Sets the array of characters that will be checked to ensure they all fit
+     * in the text block. One of the provided initialization methods must be
+     * called to then make this take effect.
      *
-     * @param width
-     * @param height
+     * @param fit
+     * @param whiteSpace true if there must be white space around all characters
+     */
+    public void setFitCharacters(char[] fit, boolean whiteSpace) {
+        fitting = fit;
+        this.whiteSpace = whiteSpace;
+    }
+
+    /**
+     * Sets up the factory to provide images based on the font's size.
+     *
+     * @param font
+     */
+    public void initializeByFont(Font font) {
+        this.font = font;
+        blocks = new TreeMap<String, BufferedImage>();
+        verticalOffset = 0;
+        sizeCellByFont();
+        blocks = new TreeMap<String, BufferedImage>();
+    }
+
+    /**
+     * Sets up the factory to provide images based on the cell cellWidth and
+     * cellHeight passed in. The font size will be used as a starting point, and
+     * reduced as needed to fit within the cell.
+     *
+     * @param cellWidth
+     * @param cellHeight
      * @param font
      * @param whiteSpace
      */
-    public void initializeBySize(int width, int height, Font font, boolean whiteSpace) {
-        this.width = width;
-        this.height = height;
+    public void initializeBySize(int cellWidth, int cellHeight, Font font) {
+        this.cellWidth = cellWidth;
+        this.cellHeight = cellHeight;
         this.font = font;
-        blocks = new TreeMap<String, SGImageBlock>();
+        blocks = new TreeMap<String, BufferedImage>();
         verticalOffset = 0;
-        this.sizeCellByDimension(whiteSpace);
-        blocks = new TreeMap<String, SGImageBlock>();
+        this.sizeCellByDimension();
+        blocks = new TreeMap<String, BufferedImage>();
     }
 
     /**
@@ -64,20 +85,7 @@ public class TextBlockFactory {
     }
 
     public Dimension getCellDimension() {
-        return new Dimension(width, height);
-    }
-
-    /**
-     * Allows directly setting the desired cell dimensions.
-     *
-     * @param dimension
-     */
-    public void setCellDimension(Dimension dimension) {
-        if (width != dimension.width || height != dimension.height) {//make sure resizing has to happen
-            width = dimension.width;
-            height = dimension.height;
-            blocks = new TreeMap<String, SGImageBlock>();//invalidate old treemap
-        }
+        return new Dimension(cellWidth, cellHeight);
     }
 
     /**
@@ -87,32 +95,32 @@ public class TextBlockFactory {
      * @param whiteSpace true if an extra pixel should be ensured around the
      * largest characters.
      */
-    private void sizeCellByFont(boolean whiteSpace) {
+    private void sizeCellByFont() {
         //build a sample image to get actual font metrics
-        Graphics2D graphics = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR).createGraphics();
+        Graphics2D graphics = new BufferedImage(cellWidth, cellHeight, BufferedImage.TYPE_4BYTE_ABGR).createGraphics();
         graphics.setFont(font);
         FontMetrics metrics = graphics.getFontMetrics();
 
-        //set width and height to largest possible values according to the font metrics
-        width = metrics.getMaxAdvance() * 2;
-        height = (metrics.getMaxAscent() + metrics.getMaxDescent()) * 2;
+        //set cellWidth and cellHeight to largest possible values according to the font metrics
+        cellWidth = metrics.getMaxAdvance() * 2;
+        cellHeight = (metrics.getMaxAscent() + metrics.getMaxDescent()) * 2;
 
         //set up working variables
-        int testWidthLeft = width - 1;
+        int testWidthLeft = cellWidth - 1;
         int testWidthRight = 0;
-        int testHeightTop = height - 1;
+        int testHeightTop = cellHeight - 1;
         int testHeightBottom = 0;
         BufferedImage testImage;
 
         //loop through basic printable characters and outside edges
-        for (char c = 33; c <= 125; c++) {
+        for (char c : fitting) {
             if (!Character.isISOControl(c)) {//make sure it's a printable character
                 testImage = getImageFor(c, Color.BLACK, Color.WHITE);
 
                 //measure widest spot
                 leftCheckLoop:
-                for (int x = 0; x < width; x++) {//check for left side
-                    for (int y = 0; y < height; y++) {
+                for (int x = 0; x < cellWidth; x++) {//check for left side
+                    for (int y = 0; y < cellHeight; y++) {
                         if (testImage.getRGB(x, y) != Color.WHITE.getRGB()) {
                             testWidthLeft = Math.min(x, testWidthLeft);//set farthest left seen
                             break leftCheckLoop;
@@ -121,8 +129,8 @@ public class TextBlockFactory {
                 }
 
                 rightCheckLoop:
-                for (int x = width - 1; x >= 0; x--) {//check for right side
-                    for (int y = 0; y < height; y++) {
+                for (int x = cellWidth - 1; x >= 0; x--) {//check for right side
+                    for (int y = 0; y < cellHeight; y++) {
                         if (testImage.getRGB(x, y) != Color.WHITE.getRGB()) {
                             testWidthRight = Math.max(x, testWidthRight);//set farthest right seen
                             break rightCheckLoop;
@@ -132,8 +140,8 @@ public class TextBlockFactory {
 
                 //measure tallest spot
                 topCheckLoop:
-                for (int y = 0; y < height; y++) {//check for top
-                    for (int x = 0; x < width; x++) {
+                for (int y = 0; y < cellHeight; y++) {//check for top
+                    for (int x = 0; x < cellWidth; x++) {
                         if (testImage.getRGB(x, y) != Color.WHITE.getRGB()) {
                             testHeightTop = Math.min(y, testHeightTop);//set highest seen
                             break topCheckLoop;
@@ -143,8 +151,8 @@ public class TextBlockFactory {
 
                 //measure lowest spot
                 bottomCheckLoop:
-                for (int y = height - 1; y >= 0; y--) {//check for bottom
-                    for (int x = 0; x < width; x++) {
+                for (int y = cellHeight - 1; y >= 0; y--) {//check for bottom
+                    for (int x = 0; x < cellWidth; x++) {
                         if (testImage.getRGB(x, y) != Color.WHITE.getRGB()) {
                             testHeightBottom = Math.max(y, testHeightBottom);//set farthest down seen
                             break bottomCheckLoop;
@@ -154,27 +162,26 @@ public class TextBlockFactory {
             }
         }
 
-        //set width and height to widest values found
+        //set cellWidth and cellHeight to widest values found
         verticalOffset = testHeightTop;
-        width = testWidthRight - testWidthLeft + 2;//have to add one since right is the index found at
-        height = testHeightBottom - testHeightTop + 2;//have to add one since bottom is the index found at
+        cellWidth = testWidthRight - testWidthLeft + 2;//have to add one since right is the index found at
+        cellHeight = testHeightBottom - testHeightTop + 2;//have to add one since bottom is the index found at
 
         if (whiteSpace) {//add enough to guarantee pixel on all four sides even with odd centering
-            width += 4;
-            height += 4;
+            cellWidth += 4;
+            cellHeight += 4;
             verticalOffset -= 2;
         }
     }
 
-    private void sizeCellByDimension(boolean whiteSpace) {
+    private void sizeCellByDimension() {
         int fontSize = font.getSize();
         boolean rightSize = false;
         do {
-            System.out.println("Size: " + fontSize);
             font = new Font(font.getName(), font.getStyle(), fontSize);
-            blocks = new TreeMap<String, SGImageBlock>();
+            blocks = new TreeMap<String, BufferedImage>();
             verticalOffset = 0;
-            for (char c = 33; c <= 125; c++) {
+            for (char c : fitting) {
                 rightSize = true;
                 if (!willFit(c, whiteSpace)) {//found one that doesn't work, skip to the next step
                     rightSize = false;
@@ -191,11 +198,13 @@ public class TextBlockFactory {
      * to not fit by definition.
      *
      * @param c
-     * @param whiteSpace if true then there must be room for the character c and
-     * at least one empty pixel on all sides of the character
      * @return
      */
-    public boolean willFit(char c, boolean whiteSpace) {
+    public boolean willFit(char c) {
+        return willFit(c, whiteSpace);
+    }
+
+    private boolean willFit(char c, boolean whiteSpace) {
         if (Character.isISOControl(c)) {//make sure it's a printable character
             return false;
         }
@@ -211,14 +220,14 @@ public class TextBlockFactory {
         }
 
         //the test image has to be slightly larger to ensure outside is clear
-        BufferedImage testImage = new BufferedImage(width + 2 + 2 * testWidth, height + 2 + 2 * testHeight, BufferedImage.TYPE_4BYTE_ABGR);
+        BufferedImage testImage = new BufferedImage(cellWidth + 2 + 2 * testWidth, cellHeight + 2 + 2 * testHeight, BufferedImage.TYPE_4BYTE_ABGR);
         Graphics2D g = testImage.createGraphics();
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, testImage.getWidth(), testImage.getHeight());
         g.setColor(Color.BLACK);
         g.setFont(font);
 
-        g.drawString("" + c, (width + 1 + 2 * testWidth - g.getFontMetrics().charWidth(c)) / 2, g.getFontMetrics().getMaxAscent() - verticalOffset + 1 + testHeight);
+        g.drawString("" + c, (cellWidth + 1 + 2 * testWidth - g.getFontMetrics().charWidth(c)) / 2, g.getFontMetrics().getMaxAscent() - verticalOffset + 1 + testHeight);
 
         //check for font drawn at the edge
         for (int y = 0; y < testImage.getHeight(); y++) {
@@ -249,14 +258,13 @@ public class TextBlockFactory {
      */
     public BufferedImage getImageFor(char c, Color foreground, Color background) {
         String search = getStringRepresentationOf(c, foreground, background);
-        SGImageBlock block = blocks.get(search);
+        BufferedImage block = blocks.get(search);
 
         if (block == null) {
-            block = new SingleImageSGBlock();
-            block.setImage(makeImage(c, foreground, background));
+            block = makeImage(c, foreground, background);
             blocks.put(search, block);
         }
-        return block.getImage();
+        return block;
     }
 
     /**
@@ -277,14 +285,14 @@ public class TextBlockFactory {
      * Creates an image based on the input.
      */
     private BufferedImage makeImage(char c, Color foreground, Color background) {
-        BufferedImage i = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
+        BufferedImage i = new BufferedImage(cellWidth, cellHeight, BufferedImage.TYPE_4BYTE_ABGR);
         Graphics2D g = i.createGraphics();
         g.setColor(background);
-        g.fillRect(0, 0, width, height);
+        g.fillRect(0, 0, cellWidth, cellHeight);
         g.setColor(foreground);
         g.setFont(font);
 
-        g.drawString("" + c, (width - g.getFontMetrics().charWidth(c)) / 2, g.getFontMetrics().getMaxAscent() - verticalOffset);
+        g.drawString("" + c, (cellWidth - g.getFontMetrics().charWidth(c)) / 2, g.getFontMetrics().getMaxAscent() - verticalOffset);
         return i;
     }
 }
