5db6ba0d8ae2e0827aee928a57adcbb8757ba1db
Eben Howard <eben.howard@gmail.com>
2013-01-13 22:49:41 -0700

    Reworked FOVTranslator

--- src/squidpony/squidgrid/fov/FOVTranslator.java
+++ src/squidpony/squidgrid/fov/FOVTranslator.java
@@ -4,15 +4,8 @@ import java.util.HashMap;
 import squidpony.squidutility.Pair;
 
 /**
- * An Adapter which bridges FOVSolvers and allows the input and output of
- * multiple types, as well as the compositing of multiple FOV algorithms into a
- * single result.
- *
- * When combining the results of multiple FOV runs, an weighted average is taken
- * for each cell and used as the result. The methods that add a solver without a
- * weight assign a weight of 1f.
- *
- * If no solvers have been added, then calculations return null;
+ * An Adapter which wraps a FOVSolver and allows the input and output of
+ * multiple types.
  *
  * The FOV methods that don't require an explicit resistance map will only
  * return a valid result if one of the constructors passing one in or one of the
@@ -39,211 +32,145 @@ import squidpony.squidutility.Pair;
  */
 public class FOVTranslator implements FOVSolver {
 
-    private HashMap<Pair<? extends FOVSolver, ? extends RadiusStrategy>, Float> solvers = new HashMap<>();
-    private float[][] lightMap, resistanceMap;//backing map for results
-    private float force, decay, totalWeight = 0;
-    private int width, height;
+    private FOVSolver solver;
+    private float[][] lightMap;//backing map for results
 
     /**
      * Creates an empty instance. At least one FOVSolver must be added through
      * an add method before FOV calculations can be performed with this object.
      */
-    public FOVTranslator() {
-    }
-
-    /**
-     * Creates an adapter with the elements needed to run FOV calculations. More
-     * solvers can be added if desired later.
-     *
-     * @param resistanceMap
-     * @param force
-     * @param decay
-     * @param solver
-     * @param strategy
-     */
-    public FOVTranslator(float[][] resistanceMap, float force, float decay, FOVSolver solver, RadiusStrategy strategy) {
-        this(resistanceMap, force, decay);
-        add(solver, strategy);
-    }
-
-    /**
-     * Creates an adapter with the elements for FOV calculations.
-     *
-     * At least one FOVSolver must be added before calculations can be
-     * performed.
-     *
-     * @param resistanceMap
-     * @param force
-     * @param decay
-     */
-    public FOVTranslator(float[][] resistanceMap, float force, float decay) {
-        width = resistanceMap.length;
-        height = resistanceMap[0].length;
-        this.resistanceMap = resistanceMap;
-        this.force = force;
-        this.decay = decay;
-    }
-
-    /**
-     * Creates an adapter with the elements needed to run FOV calculations. More
-     * solvers can be added if desired later.
-     *
-     * @param resistanceMap
-     * @param scale
-     * @param force
-     * @param decay
-     * @param solver
-     * @param strategy
-     */
-    public FOVTranslator(int[][] resistanceMap, int scale, int force, int decay, FOVSolver solver, RadiusStrategy strategy) {
-        this(resistanceMap, scale, force, decay);
-        add(solver, strategy);
-    }
-
-    /**
-     * Creates an adapter with the elements needed to run FOV calculations.
-     *
-     * At least one FOVSolver must be added before calculations can be
-     * performed.
-     *
-     * @param resistanceMap
-     * @param scale
-     * @param force
-     * @param decay
-     */
-    public FOVTranslator(int[][] resistanceMap, int scale, int force, int decay) {
-        width = resistanceMap.length;
-        height = resistanceMap[0].length;
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                this.resistanceMap[x][y] = (float) scale / resistanceMap[x][y];
-            }
-        }
-        this.force = (float) scale / force;
-        this.decay = (float) scale / decay;
-    }
-
-    /**
-     * Creates an adapter with the elements needed to run FOV calculations. More
-     * solvers can be added if desired later.
-     *
-     * @param resistanceMap
-     * @param force
-     * @param decay
-     * @param solver
-     * @param strategy
-     */
-    public FOVTranslator(boolean[][] resistanceMap, float force, float decay, FOVSolver solver, RadiusStrategy strategy) {
-        this(resistanceMap, force, decay);
-        add(solver, strategy);
-    }
-
-    /**
-     * Creates an adapter with the elements needed to run FOV calculations. More
-     * solvers can be added if desired later.
-     *
-     * @param resistanceMap
-     * @param force
-     * @param decay
-     */
-    public FOVTranslator(boolean[][] resistanceMap, float force, float decay) {
-        width = resistanceMap.length;
-        height = resistanceMap[0].length;
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                this.resistanceMap[x][y] = resistanceMap[x][y] ? 1f : 0f;
-            }
-        }
-        this.force = force;
-        this.decay = decay;
-    }
-
-    /**
-     * Setting the force overwrites any previously set radius value.
-     *
-     * @param force
-     */
-    public void setForce(float force) {
-        this.force = force;
-    }
-
-    /**
-     * Setting the decay overwrites any previously set radius value.
-     *
-     * @param decay
-     */
-    public void setDecay(float decay) {
-        this.decay = decay;
-    }
-
-    /**
-     * Setting the radius overwrites any previously set force and decay values.
-     *
-     * @param radius
-     */
-    public void setRadius(float radius) {
-        force = 1f;
-        decay = force / radius;
-    }
-
-    /**
-     * Setting the radius overwrites any previously set force and decay values.
-     *
-     * @param radius
-     */
-    public void setRadius(int radius) {
-        force = radius;
-        decay = force / radius;
+    public FOVTranslator(FOVSolver solver) {
+        this.solver = solver;
     }
 
     @Override
     public float[][] calculateFOV(float[][] map, int startx, int starty, float force, float decay, RadiusStrategy radiusStrategy) {
-        resistanceMap = map;
-        this.force = force;
-        this.decay = decay;
-        width = map.length;
-        height = map[0].length;
-        if (solvers.isEmpty()) {
-            return null;//no solvers loaded
-        } else if (solvers.size() == 1) {//just one solver so run only it
-            Pair<? extends FOVSolver, ? extends RadiusStrategy> p = (Pair<? extends FOVSolver, ? extends RadiusStrategy>) solvers.keySet().toArray()[0];
-            lightMap = p.getFirst().calculateFOV(resistanceMap, startx, starty, force, decay, p.getSecond());
-        } else {//multiple solvers, run them and take the average results for each cell
-            lightMap = new float[width][height];
-            for (Pair<? extends FOVSolver, ? extends RadiusStrategy> p : solvers.keySet()) {
-                float[][] tempMap = p.getFirst().calculateFOV(resistanceMap, startx, starty, force, decay, p.getSecond());
-                for (int x = 0; x < width; x++) {
-                    for (int y = 0; y < height; y++) {
-                        float weight = solvers.get(p) / totalWeight;
-                        float bright = lightMap[x][y];
-                        float tempbright = tempMap[x][y];
-                        lightMap[x][y] += tempMap[x][y] * weight;//add portion to running average
-                    }
-                }
-            }
-        }
-
+        lightMap = solver.calculateFOV(map, startx, starty, force, decay, radiusStrategy);
         return lightMap;
     }
 
     @Override
     public float[][] calculateFOV(float[][] map, int startx, int starty, float radius) {
-        return calculateFOV(map, startx, starty, 1, 1f / radius, null);//don't need a radius strategy as it uses ones added from pairs
+        lightMap = calculateFOV(map, startx, starty, 1, 1f / radius, BasicRadiusStrategy.CIRCLE);//don't need a radius strategy as it uses ones added from pairs
+        return lightMap;
     }
 
     /**
-     * Runs an FOV calculation with the previously provided elements, with the
-     * provided location as the start point.
+     * Calculates the FOV using an integer array.
      *
+     * @param map
      * @param startx
      * @param starty
+     * @param force
+     * @param decay
+     * @param radiusStrategy
+     * @param scale
      * @return
      */
-    public float[][] calculateFOV(int startx, int starty) {
-        if (resistanceMap == null) {//make sure there's a map to work with
-            return null;
+    public int[][] calculateFOV(int[][] map, int startx, int starty, float force, float decay, RadiusStrategy radiusStrategy, float scale) {
+        int width = map.length;
+        int height = map[0].length;
+        float[][] tempMap = new float[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                tempMap[x][y] = map[x][y] / scale;
+            }
         }
-        return calculateFOV(lightMap, startx, starty, force, decay, null);//run FOV from the provided start location with preloaded variables
+        lightMap = solver.calculateFOV(tempMap, startx, starty, force, decay, radiusStrategy);
+        int[][] resultMap = new int[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                resultMap[x][y] = (int) (lightMap[x][y] * scale);
+            }
+        }
+        return resultMap;
+    }
+
+    /**
+     * Calculates the FOV using an integer array.
+     *
+     * @param map
+     * @param startx
+     * @param starty
+     * @param radius
+     * @param scale
+     * @return
+     */
+    public int[][] calculateFOV(int[][] map, int startx, int starty, float radius, float scale) {
+        int width = map.length;
+        int height = map[0].length;
+        float[][] tempMap = new float[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                tempMap[x][y] = map[x][y] / scale;
+            }
+        }
+        lightMap = solver.calculateFOV(tempMap, startx, starty, radius);
+        int[][] resultMap = new int[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                resultMap[x][y] = (int) (lightMap[x][y] / scale);
+            }
+        }
+        return resultMap;
+    }
+
+    /**
+     * Calculates the FOV using a boolean array.
+     *
+     * @param map
+     * @param startx
+     * @param starty
+     * @param force
+     * @param decay
+     * @param radiusStrategy
+     * @return
+     */
+    public boolean[][] calculateFOV(boolean[][] map, int startx, int starty, float force, float decay, RadiusStrategy radiusStrategy) {
+        int width = map.length;
+        int height = map[0].length;
+        float[][] tempMap = new float[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                tempMap[x][y] = map[x][y] ? 1f : 0f;
+            }
+        }
+        lightMap = solver.calculateFOV(tempMap, startx, starty, force, decay, radiusStrategy);
+        boolean[][] resultMap = new boolean[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                resultMap[x][y] = lightMap[x][y] > 0;
+            }
+        }
+        return resultMap;
+    }
+
+    /**
+     * Calculates the FOV using a boolean array.
+     *
+     * @param map
+     * @param startx
+     * @param starty
+     * @param radius
+     * @return
+     */
+    public boolean[][] calculateFOV(boolean[][] map, int startx, int starty, float radius) {
+        int width = map.length;
+        int height = map[0].length;
+        float[][] tempMap = new float[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                tempMap[x][y] = map[x][y] ? 1f : 0f;
+            }
+        }
+        lightMap = solver.calculateFOV(tempMap, startx, starty, radius);
+        boolean[][] resultMap = new boolean[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                resultMap[x][y] = lightMap[x][y] > 0;
+            }
+        }
+        return resultMap;
     }
 
     /**
@@ -258,6 +185,8 @@ public class FOVTranslator implements FOVSolver {
             return null;
         }
 
+        int width = lightMap.length;
+        int height = lightMap[0].length;
         boolean[][] retMap = new boolean[width][height];
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
@@ -275,15 +204,17 @@ public class FOVTranslator implements FOVSolver {
      * @param multiplyer
      * @return
      */
-    public int[][] getIntArray(float multiplyer) {
+    public int[][] getIntArray(float scale) {
         if (lightMap == null) {//make sure there's something to work with
             return null;
         }
 
+        int width = lightMap.length;
+        int height = lightMap[0].length;
         int[][] retMap = new int[width][height];
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
-                retMap[x][y] = (int) (lightMap[x][y] * multiplyer);
+                retMap[x][y] = (int) (lightMap[x][y] * scale);
             }
         }
 
@@ -311,40 +242,5 @@ public class FOVTranslator implements FOVSolver {
      */
     public int getInt(int x, int y, int scale) {
         return (int) (lightMap[x][y] * scale);
-    }
-
-    public void add(Pair<FOVSolver, RadiusStrategy> pair, float weight) {
-        totalWeight += weight;
-        solvers.put(pair, weight);
-    }
-
-    /**
-     * Adds the provided pair to the list of solvers that will be run during
-     * calculations.
-     *
-     * @param pair
-     */
-    public void add(Pair<FOVSolver, RadiusStrategy> pair) {
-        add(pair, 1f);
-    }
-
-    /**
-     * Adds the provided solver and strategy as a pair to the list of solvers
-     * that will be run during calculations.
-     *
-     * @param solver
-     * @param strategy
-     */
-    public void add(FOVSolver solver, RadiusStrategy strategy) {
-        add(new Pair<>(solver, strategy));
-    }
-
-    /**
-     * Adds the provided solver with a default radius strategy.
-     *
-     * @param solver
-     */
-    public void add(FOVSolver solver) {
-        add(new Pair<FOVSolver, RadiusStrategy>(solver, BasicRadiusStrategy.CIRCLE));
     }
 }
