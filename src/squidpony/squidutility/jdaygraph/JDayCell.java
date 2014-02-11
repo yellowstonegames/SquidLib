5102b42e93649cb412cd631d0a532cb050d4bcc3
Eben Howard <eben.howard@gmail.com>
2014-02-11 04:23:37 -0700

    started work on JDay's topology algorithms

---
+++ src/squidpony/squidutility/jdaygraph/JDayCell.java
@@ -0,0 +1,12 @@
+package squidpony.squidutility.jdaygraph;
+
+import squidpony.annotation.Beta;
+
+/**
+ *
+ * @author Eben Howard - http://squidpony.com - howard@squidpony.com
+ */
+@Beta
+public interface JDayCell {
+
+}
---
+++ src/squidpony/squidutility/jdaygraph/Overlay.java
@@ -0,0 +1,21 @@
+package squidpony.squidutility.jdaygraph;
+
+import squidpony.annotation.Beta;
+
+/**
+ * Utility class that stores a graph and performs calculations on it.
+ *
+ * Adapted from posts at http://paleoludic.com/
+ *
+ * @author Eben Howard - http://squidpony.com - howard@squidpony.com
+ */
+@Beta
+public class Overlay {
+
+    private final JDayCell cells[];
+
+    public Overlay (Topology topology, JDayCell cellType) {
+        cells = new JDayCell[topology.size()];
+    }
+
+}
