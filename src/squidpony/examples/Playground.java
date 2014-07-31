414486f6a86e0ec122679714d3bda4a424bcd0c3
Eben Howard <eben.howard@gmail.com>
2014-07-31 02:28:10 -0600

    Now Java 7 instead of 8
    Dropping JavaFX for sound (some time ago actually) meant 8 was no longer
    needed

---
+++ src/squidpony/examples/Playground.java
@@ -0,0 +1,14 @@
+package squidpony.examples;
+
+import squidpony.squidcolor.SColor;
+
+/**
+ * This class is a scratchpad area to test things out quickly.
+ *
+ * @author Eben Howard - http://squidpony.com - howard@squidpony.com
+ */
+public class Playground {
+    public static void main(String... args){
+        System.out.println("" + SColor.FULL_PALLET.length);
+    }
+}
