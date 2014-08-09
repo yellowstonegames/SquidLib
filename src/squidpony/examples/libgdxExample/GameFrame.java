089d3fc549bd2343cfa0b79f718afcd789891a35
Eben Howard <eben.howard@gmail.com>
2014-08-09 16:56:13 -0600

    libgdx support testing

---
+++ src/squidpony/examples/libgdxExample/GameFrame.java
@@ -0,0 +1,50 @@
+package squidpony.examples.libgdxExample;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Pixmap;
+
+/**
+ * The primary frame for the game.
+ *
+ * @author Eben Howard - http://squidpony.com - eben@squidpony.com
+ */
+public class GameFrame extends Game {
+    
+    Pixmap[] icons = new Pixmap[3];
+
+    @Override
+    public void create() {
+        System.out.println("Creating new game.");
+        System.out.println("Working in folder: " + System.getProperty("user.dir"));
+        super.setScreen(new DisplayMaster());
+    }
+    
+    @Override
+    public void resize(int width, int height) {
+        System.out.println("New size: " + width + ", " + height);
+    }
+
+    @Override
+    public void render() {
+        //fpsLogger.log();
+        super.render();
+    }
+
+    @Override
+    public void pause() {
+        System.out.println("Pausing game.");
+        super.pause();
+    }
+
+    @Override
+    public void resume() {
+        System.out.println("Resuming game.");
+        super.resume();
+    }
+
+    @Override
+    public void dispose() {
+        System.out.println("Disposing game.");
+        super.dispose();
+    }
+}
