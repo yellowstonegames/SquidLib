4086a07325bd0e09b3c8e2f1aa3904edc04d8736
Tommy Ettinger <tommy.ettinger@gmail.com>
2015-07-13 12:57:24 -0700

    New approach to AOE effects for path/target-finding

---
+++ src/main/java/squidpony/squidai/BurstAOE.java
@@ -0,0 +1,62 @@
+package squidpony.squidai;
+
+import squidpony.squidgrid.FOV;
+import squidpony.squidgrid.Radius;
+import squidpony.squidgrid.mapping.DungeonUtility;
+
+import java.awt.*;
+import java.util.HashMap;
+
+/**
+ * An AOE type that has a center and a radius, and uses shadowcasting to create a burst of rays from the center, out to
+ * the distance specified by radius. You can specify the RadiusType to Radius.DIAMOND for Manhattan distance,
+ * RADIUS.SQUARE for Chebyshev, or RADIUS.CIRCLE for Euclidean.
+ * Created by Tommy Ettinger on 7/13/2015.
+ */
+public class BurstAOE implements AOE {
+    private FOV fov;
+    private Point center;
+    private int radius;
+    private double[][] map;
+    private Radius radiusType;
+    public BurstAOE(Point center, int radius, Radius radiusType)
+    {
+        fov = new FOV(FOV.SHADOW);
+        this.center = center;
+        this.radius = radius;
+        this.radiusType = radiusType;
+    }
+    private BurstAOE()
+    {
+        fov = new FOV(FOV.SHADOW);
+        this.center = new Point(1, 1);
+        this.radius = 1;
+        this.radiusType = Radius.DIAMOND;
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void setMap(char[][] map) {
+        this.map = DungeonUtility.generateResistances(map);
+    }
+
+    @Override
+    public HashMap<Point, Double> findArea() {
+        return AreaUtils.arrayToHashMap(fov.calculateFOV(map, center.x, center.y, radius, radiusType));
+    }
+}
---
+++ src/main/java/squidpony/squidai/CloudAOE.java
@@ -0,0 +1,98 @@
+package squidpony.squidai;
+
+import squidpony.squidgrid.Radius;
+import squidpony.squidgrid.Spill;
+import squidpony.squidgrid.mapping.DungeonUtility;
+import squidpony.squidmath.LightRNG;
+import squidpony.squidmath.RNG;
+
+import java.awt.*;
+import java.util.HashMap;
+
+/**
+ * An AOE type that has a center and a volume, and will randomly expand in all directions until it reaches volume or
+ * cannot expand further. Specify the RadiusType as Radius.DIAMOND for Manhattan distance (and the best results),
+ * RADIUS.SQUARE for Chebyshev, or RADIUS.CIRCLE for Euclidean. You can specify a seed for the RNG and a fresh RNG will
+ * be used for all random expansion; the RNG will reset to the specified seed after each generation so the same
+ * CloudAOE can be used in different places by just changing the center.
+ * Created by Tommy Ettinger on 7/13/2015.
+ */
+public class CloudAOE implements AOE {
+    private Spill spill;
+    private Point center;
+    private int volume;
+    private Spill.Measurement measurement;
+    private long seed;
+    public CloudAOE(Point center, int volume, Radius radiusType)
+    {
+        LightRNG l = new LightRNG();
+        this.seed = l.getState();
+        this.spill = new Spill(new RNG(l));
+        this.center = center;
+        this.volume = this.volume;
+        switch (radiusType)
+        {
+            case CIRCLE: this.measurement = Spill.Measurement.EUCLIDEAN;
+                break;
+            case SQUARE: this.measurement = Spill.Measurement.CHEBYSHEV;
+                break;
+            default: this.measurement = Spill.Measurement.MANHATTAN;
+                break;
+        }
+    }
+    public CloudAOE(Point center, int volume, Radius radiusType, long rngSeed)
+    {
+        this.seed = rngSeed;
+        this.spill = new Spill(new RNG(new LightRNG(rngSeed)));
+        this.center = center;
+        this.volume = this.volume;
+        switch (radiusType)
+        {
+            case CIRCLE: this.measurement = Spill.Measurement.EUCLIDEAN;
+                break;
+            case SQUARE: this.measurement = Spill.Measurement.CHEBYSHEV;
+                break;
+            default: this.measurement = Spill.Measurement.MANHATTAN;
+                break;
+        }
+    }
+    private CloudAOE()
+    {
+        LightRNG l = new LightRNG();
+        this.seed = l.getState();
+        this.spill = new Spill(new RNG(l));
+        this.center = new Point(1, 1);
+        this.volume = 1;
+        this.measurement = Spill.Measurement.MANHATTAN;
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    @Override
+    public void setMap(char[][] map) {
+        spill.initialize(map);
+    }
+
+    @Override
+    public HashMap<Point, Double> findArea() {
+        spill.start(center, volume, null);
+        HashMap<Point, Double> r = AreaUtils.arrayToHashMap(spill.spillMap);
+        spill.reset();
+        spill.rng.setRandomness(new LightRNG(this.seed));
+        return r;
+    }
+}
