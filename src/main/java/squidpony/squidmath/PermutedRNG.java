8d16e297772e796776574cd5b58a95c6cf649cd3
Tommy Ettinger <tommy.ettinger@gmail.com>
2015-07-17 00:24:49 -0700

    Improvements to LightRNG, add PermutedRNG

--- src/main/java/squidpony/squidmath/PermutedRNG.java
+++ src/main/java/squidpony/squidmath/PermutedRNG.java
@@ -10,13 +10,16 @@ package squidpony.squidmath;
 
 /**
  * This is a SplittableRandom-style generator, meant to have a tiny state
- * that permits storing many different generators with low overhead.
- * It should be rather fast, though no guarantees can be made.
+ * that can be efficiently stored and passed, that also performs pseudo-
+ * random modifications to the output based on the techniques from the
+ * Permuted Congruential Generators created by M.E. O'Neill.
+ * It should be rather fast, though LightRNG is probably slightly faster,
+ * but the quality of this generator should be better.
  * Written in 2015 by Sebastiano Vigna (vigna@acm.org)
  * @author Sebastiano Vigna
  * @author Tommy Ettinger
  */
-public class LightRNG implements RandomnessSource
+public class PermutedRNG implements RandomnessSource
 {
     private static final long serialVersionUID = 3L;
     /** 2 raised to the 53, - 1. */
@@ -27,32 +30,23 @@ public class LightRNG implements RandomnessSource
     private static final long FLOAT_MASK = ( 1L << 24 ) - 1;
     /** 2 raised to the -24. */
     private static final double NORM_24 = 1. / ( 1L << 24 );
-
-    public long state; /* The state can be seeded with any value. */
+    /**
+     * The state can be seeded with any value.
+     */
+    public long state;
 
     /** Creates a new generator seeded using Math.random. */
-    public LightRNG() {
+    public PermutedRNG() {
         this((long)Math.floor(Math.random() * Long.MAX_VALUE));
     }
 
-    public LightRNG( final long seed ) {
-        setSeed(seed);
+    public PermutedRNG(final long seed) {
+        this.state = seed;
     }
 
     @Override
     public int next( int bits ) {
-        return (int)( nextLong() & ( 1L << bits ) - 1 );
-    }
-
-    /**
-     * Can return any long, positive or negative, of any size permissible in a 64-bit signed integer.
-     * @return
-     */
-    public long nextLong() {
-        long z = ( state += 0x9E3779B97F4A7C15l );
-        z = (z ^ (z >> 30)) * 0xBF58476D1CE4E5B9l;
-        z = (z ^ (z >> 27)) * 0x94D049BB133111EBl;
-        return z ^ (z >> 31);
+        return (int)( nextInt() & ( 1L << bits ) - 1 );
     }
 
     /**
@@ -60,11 +54,27 @@ public class LightRNG implements RandomnessSource
      * @return
      */
     public int nextInt() {
-        return (int)nextLong();
+        long z = ( state += 0x9E3779B97F4A7C15l );
+        z = (z ^ (z >> 30)) * 0xBF58476D1CE4E5B9l;
+        z = (z ^ (z >> 27)) * 0x94D049BB133111EBl;
+        z = z ^ (z >> 31);
+        return Integer.rotateRight((int)((z ^ (z >> 18)) >> 27), (int)(z >> 59));
+    }
+    /**
+     * Can return any long, positive or negative, of any size permissible in a 64-bit signed integer.
+     *
+     * Generates two 32-bit integers using two calls to nextInt() and combines them into one long.
+     *
+     * @return
+     */
+    public long nextLong() {
+        return ((long)(nextInt()) << 32) | nextInt();
     }
 
     /**
      * Exclusive on the upper bound n.  The lower bound is 0.
+     *
+     * May call nextInt() multiple times, but usually will not.
      * @param n
      * @return
      */
@@ -79,6 +89,8 @@ public class LightRNG implements RandomnessSource
 
     /**
      * Inclusive lower, exclusive upper.
+     *
+     * May call nextInt() multiple times, but usually will not.
      * @param lower
      * @param upper
      * @return
@@ -90,6 +102,8 @@ public class LightRNG implements RandomnessSource
 
     /**
      * Exclusive on the upper bound n. The lower bound is 0.
+     *
+     * Will call nextInt() at least 2 times, possibly more.
      * @param n
      * @return
      */
@@ -103,7 +117,9 @@ public class LightRNG implements RandomnessSource
     }
 
     /**
-     * Inclusive lower, exclusive upper.
+     * Exclusive on the upper bound n. The lower bound is 0.
+     *
+     * Will call nextInt() at least 2 times, possibly more.
      * @param lower
      * @param upper
      * @return
@@ -112,8 +128,12 @@ public class LightRNG implements RandomnessSource
         if ( upper - lower <= 0 ) throw new IllegalArgumentException();
         return lower + nextLong(upper - lower);
     }
+
     /**
      * Gets a uniform random double in the range [0.0,1.0)
+     *
+     * Calls nextInt() exactly two times.
+     *
      * @return
      */
     public double nextDouble() {
@@ -122,6 +142,9 @@ public class LightRNG implements RandomnessSource
 
     /**
      * Gets a uniform random double in the range [0.0,upper) given the parameter upper.
+     *
+     * Calls nextInt() exactly two times.
+     *
      * @param upper
      * @return
      */
@@ -131,21 +154,24 @@ public class LightRNG implements RandomnessSource
 
     /**
      * Gets a uniform random float in the range [0.0,1.0)
+     *
+     * Calls nextInt() exactly one time.
+     *
      * @return
      */
     public float nextFloat() {
-        return (float)( ( nextLong() & FLOAT_MASK ) * NORM_24 );
+        return (float)( ( nextInt() & FLOAT_MASK ) * NORM_24 );
     }
 
     public boolean nextBoolean() {
-        return ( nextLong() & 1 ) != 0;
+        return ( nextInt() & 1 ) != 0;
     }
 
     public void nextBytes( final byte[] bytes ) {
         int i = bytes.length, n = 0;
         while( i != 0 ) {
-            n = Math.min( i, 8 );
-            for ( long bits = nextLong(); n-- != 0; bits >>= 8 ) bytes[ --i ] = (byte)bits;
+            n = Math.min(i, 4 );
+            for ( int bits = nextInt(); n-- != 0; bits >>= 8 ) bytes[ --i ] = (byte)bits;
         }
     }
 
