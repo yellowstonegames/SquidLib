cfa72831a4a761bb59c5b9f5b58b0f31045c52ab
David Becker <dasmau89@googlemail.com>
2015-09-20 09:16:22 +0200

    convert test into multithreaded one

--- squidlib-util/src/test/squidpony/examples/PerformaneTest.java
+++ squidlib-util/src/test/squidpony/examples/PerformaneTest.java
@@ -1,5 +1,13 @@
 package squidpony.examples;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import squidpony.squidgrid.FOV;
 import squidpony.squidgrid.LOS;
 import squidpony.squidgrid.mapping.DungeonGenerator;
@@ -21,61 +29,99 @@ import squidpony.squidmath.RandomnessSource;
  * @author David Becker
  *
  */
-public class PerformaneTest {
+public final class PerformaneTest {
 	// we want predictable outcome for our test
-	private static final RandomnessSource source = new LightRNG(0x1337BEEF);
-	private static final RNG rng = new RNG(source);
+	private static final RandomnessSource SOURCE = new LightRNG(0x1337BEEF);
+	private static final RNG RNG = new RNG(SOURCE);
 
 	// a 30 * 30 map should be enough
-	private static final int dimension = 30;
+	private static final int DIMENSION = 30;
 
-	public static void main(String[] args) {
-		final long timerStart = System.currentTimeMillis();
-		final DungeonGenerator generator = new DungeonGenerator(dimension, dimension, rng);
+	private static final int NUM_THREADS = 4;
+	private static final int NUM_TASKS = 10;
+
+	private PerformaneTest() {
+	}
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		final DungeonGenerator generator = new DungeonGenerator(DIMENSION, DIMENSION, RNG);
 		final char[][] map = generator.generate();
 		final double[][] res = DungeonUtility.generateResistances(map);
-		final FOV fovRipple = new FOV(FOV.RIPPLE);
-		final FOV fovRippleL = new FOV(FOV.RIPPLE_LOOSE);
-		final FOV fovRippleT = new FOV(FOV.RIPPLE_TIGHT);
-		final FOV fovRippleV = new FOV(FOV.RIPPLE_VERY_LOOSE);
-		final FOV fovShadow = new FOV(FOV.SHADOW);
-		final LOS losBresenham = new LOS(LOS.BRESENHAM);
-		final LOS losElias = new LOS(LOS.ELIAS);
-		final LOS losRay = new LOS(LOS.RAY);
-		final int end = dimension - 1;
 
-		final long timerPreparation = System.currentTimeMillis();
-
-		for (int x = 0; x < dimension; x++) {
-			for (int y = 0; y < dimension; y++) {
-				fovRipple.calculateFOV(res, x, y);
-				fovRippleL.calculateFOV(res, x, y);
-				fovRippleT.calculateFOV(res, x, y);
-				// FIXME causes exception !
-				// fovRippleV.calculateFOV(res, x, y);
-				fovShadow.calculateFOV(res, x, y);
-
-				losBresenham.isReachable(map, x, y, 0, 0);
-				losBresenham.isReachable(map, x, y, 0, end);
-				losBresenham.isReachable(map, x, y, end, 0);
-				losBresenham.isReachable(map, x, y, end, end);
-
-				losElias.isReachable(map, x, y, 0, 0);
-				losElias.isReachable(map, x, y, 0, end);
-				losElias.isReachable(map, x, y, end, 0);
-				losElias.isReachable(map, x, y, end, end);
-
-				losRay.isReachable(map, x, y, 0, 0);
-				losRay.isReachable(map, x, y, 0, end);
-				losRay.isReachable(map, x, y, end, 0);
-				losRay.isReachable(map, x, y, end, end);
-			}
+		List<Callable<Long>> tasks = new ArrayList<>();
+		for (int i = 0; i < NUM_TASKS; i++) {
+			tasks.add(new Test(map, res));
 		}
-		final long timerEnd = System.currentTimeMillis();
+		ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
+		System.out.println("invoking " + NUM_TASKS + " tasks on " + NUM_THREADS + " threads");
+		final List<Future<Long>> invoke = executor.invokeAll(tasks);
 
-		System.out.println("Prepatation duration: " + (timerPreparation - timerStart) + "(ms)");
-		System.out.println("Calculation duration: " + (timerEnd - timerPreparation) + "(ms)");
-		System.out.println("Total duration: " + (timerEnd - timerStart) + "(ms)");
+		final int size = invoke.size();
+		long total = 0L;
+		for (Future<Long> future : invoke) {
+			total += future.get();
+		}
+		System.out.println("arithmatic mean: " + total / size);
+		System.exit(0);
+	}
+
+	/**
+	 * separate thread that does the real test
+	 * 
+	 * @author David Becker
+	 *
+	 */
+	private static final class Test implements Callable<Long> {
+
+		private char[][] map;
+		private double[][] res;
+
+		public Test(char[][] m, double[][] r) {
+			map = m;
+			res = r;
+		}
+
+		@Override
+		public Long call() throws Exception {
+			final long timerStart = System.currentTimeMillis();
+			final FOV fovRipple = new FOV(FOV.RIPPLE);
+			final FOV fovRippleL = new FOV(FOV.RIPPLE_LOOSE);
+			final FOV fovRippleT = new FOV(FOV.RIPPLE_TIGHT);
+			final FOV fovRippleV = new FOV(FOV.RIPPLE_VERY_LOOSE);
+			final FOV fovShadow = new FOV(FOV.SHADOW);
+			final LOS losBresenham = new LOS(LOS.BRESENHAM);
+			final LOS losElias = new LOS(LOS.ELIAS);
+			final LOS losRay = new LOS(LOS.RAY);
+			final int end = DIMENSION - 1;
+
+			for (int x = 0; x < DIMENSION; x++) {
+				for (int y = 0; y < DIMENSION; y++) {
+					fovRipple.calculateFOV(res, x, y);
+					fovRippleL.calculateFOV(res, x, y);
+					fovRippleT.calculateFOV(res, x, y);
+					// FIXME causes exception !
+					// fovRippleV.calculateFOV(res, x, y);
+					fovShadow.calculateFOV(res, x, y);
+
+					losBresenham.isReachable(map, x, y, 0, 0);
+					losBresenham.isReachable(map, x, y, 0, end);
+					losBresenham.isReachable(map, x, y, end, 0);
+					losBresenham.isReachable(map, x, y, end, end);
+
+					losElias.isReachable(map, x, y, 0, 0);
+					losElias.isReachable(map, x, y, 0, end);
+					losElias.isReachable(map, x, y, end, 0);
+					losElias.isReachable(map, x, y, end, end);
+
+					losRay.isReachable(map, x, y, 0, 0);
+					losRay.isReachable(map, x, y, 0, end);
+					losRay.isReachable(map, x, y, end, 0);
+					losRay.isReachable(map, x, y, end, end);
+				}
+			}
+			final long timerEnd = System.currentTimeMillis();
+			return timerEnd - timerStart;
+		}
 
 	}
 }
