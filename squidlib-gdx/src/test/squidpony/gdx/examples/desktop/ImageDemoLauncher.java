2d3f180cc0e23f43ccaf7f93aef83752927f59cd
Tommy Ettinger <tommy.ettinger@gmail.com>
2015-09-21 18:11:21 -0700

    Squid-themed "default" icon for GDX

--- squidlib-gdx/src/test/squidpony/gdx/examples/desktop/ImageDemoLauncher.java
+++ squidlib-gdx/src/test/squidpony/gdx/examples/desktop/ImageDemoLauncher.java
@@ -1,5 +1,6 @@
 package squidpony.gdx.examples.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import squidpony.gdx.examples.ImageDemo;
@@ -10,6 +11,9 @@ public class ImageDemoLauncher {
 		config.title = "SquidLib GDX Image Demo";
 		config.width = 30 * 2 * 18;
 		config.height = 20 * 36;
+		config.addIcon("Tentacle-16.png", Files.FileType.Classpath);
+		config.addIcon("Tentacle-32.png", Files.FileType.Classpath);
+		config.addIcon("Tentacle-128.png", Files.FileType.Classpath);
 		new LwjglApplication(new ImageDemo(), config);
 	}
 }
--- squidlib-gdx/src/test/squidpony/gdx/examples/desktop/SquidAIDemoLauncher.java
+++ squidlib-gdx/src/test/squidpony/gdx/examples/desktop/SquidAIDemoLauncher.java
@@ -1,8 +1,8 @@
 package squidpony.gdx.examples.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import squidpony.gdx.examples.EverythingDemo;
 import squidpony.gdx.examples.SquidAIDemo;
 
 public class SquidAIDemoLauncher {
@@ -11,6 +11,9 @@ public class SquidAIDemoLauncher {
 		config.title = "SquidLib GDX AI Demo";
 		config.width = 40 * 2 * 6;
 		config.height = 40 * 12;
+		config.addIcon("Tentacle-16.png", Files.FileType.Classpath);
+		config.addIcon("Tentacle-32.png", Files.FileType.Classpath);
+		config.addIcon("Tentacle-128.png", Files.FileType.Classpath);
 		new LwjglApplication(new SquidAIDemo(), config);
 	}
 }
