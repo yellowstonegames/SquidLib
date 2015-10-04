e60db502436d3eabf814c6f1f3e2990729a6f603
David Becker <dasmau89@googlemail.com>
2015-10-04 10:06:27 +0200

    extract wrapper class for chunks of work

--- squidlib-util/src/test/java/squidpony/performance/DijkstraPerformanceTest.java
+++ squidlib-util/src/test/java/squidpony/performance/DijkstraPerformanceTest.java
@@ -18,11 +18,15 @@ import java.util.concurrent.*;
  * <ul>
  * <li>generate dungeon</li>
  * <li>for every walkable cell <b>W</b> in the dungeon:</li>
- * <li><ul>
- *     <li>generate a random walkable cell <b>R</b> using DungeonUtility.randomFloor</li>
- *     <li>compute DijkstraMap, spanning the entire dungeon level, with the goals being <b>W</b> and <b>R</b></li>
- *     <li>compute path using findPath from <b>W</b> to <b>R</b></li>
- * </ul></li>
+ * <li>
+ * <ul>
+ * <li>generate a random walkable cell <b>R</b> using DungeonUtility.randomFloor
+ * </li>
+ * <li>compute DijkstraMap, spanning the entire dungeon level, with the goals
+ * being <b>W</b> and <b>R</b></li>
+ * <li>compute path using findPath from <b>W</b> to <b>R</b></li>
+ * </ul>
+ * </li>
  * </ul>
  * 
  * @author David Becker
@@ -35,7 +39,7 @@ public final class DijkstraPerformanceTest {
 	private static final RNG RNG = new RNG(SOURCE);
 
 	// a 60 * 60 map should be more taxing
-    private static final int DIMENSION = 60, PATH_LENGTH = (DIMENSION - 2) * (DIMENSION - 2);
+	private static final int DIMENSION = 60, PATH_LENGTH = (DIMENSION - 2) * (DIMENSION - 2);
 	private static final int NUM_THREADS = 8;
 	private static final int NUM_TASKS = 100;
 
@@ -45,7 +49,7 @@ public final class DijkstraPerformanceTest {
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		final DungeonGenerator generator = new DungeonGenerator(DIMENSION, DIMENSION, RNG);
 		final char[][] map = generator.generate();
-        System.out.println(generator.toString());
+		System.out.println(generator.toString());
 		List<Callable<Long>> tasks = new ArrayList<>();
 		for (int i = 0; i < NUM_TASKS; i++) {
 			tasks.add(new Test(map));
@@ -64,41 +68,39 @@ public final class DijkstraPerformanceTest {
 	 * separate thread that does the real test
 	 * 
 	 * @author David Becker
-     * @author Tommy Ettinger
+	 * @author Tommy Ettinger
 	 */
-	private static final class Test implements Callable<Long> {
+	private static final class Test extends AbstractPerformanceUnit {
 
-        private char[][] map;
+		private char[][] map;
 		private DijkstraMap dijkstra;
-        private DungeonUtility utility;
+		private DungeonUtility utility;
+
 		public Test(char[][] m) {
-            map = m;
+			map = m;
 			dijkstra = new DijkstraMap(map, new StatefulRNG(new LightRNG(0x1337BEEF)));
-            utility = new DungeonUtility(new StatefulRNG(new LightRNG(0x1337BEEF)));
+			utility = new DungeonUtility(new StatefulRNG(new LightRNG(0x1337BEEF)));
 		}
 
 		@Override
-		public Long call() throws Exception {
-			final long timerStart = System.currentTimeMillis();
-            Coord r;
+		protected void doWork() {
+			Coord r;
 
 			for (int x = 1; x < DIMENSION - 1; x++) {
 				for (int y = 1; y < DIMENSION - 1; y++) {
-                    if(map[x][y] == '#')
-                        continue;
-                    // this should ensure no blatant correlation between R and W
-                    utility.rng.setState((x << 22) | (y << 16) | (x * y));
-                    ((StatefulRNG)dijkstra.rng).setState((x << 20) | (y << 14) | (x * y));
-                    r = utility.randomFloor(map);
-                    dijkstra.setGoal(x, y);
-                    dijkstra.setGoal(r);
-                    dijkstra.scan(null);
-                    dijkstra.clearGoals();
-                    dijkstra.findPath(PATH_LENGTH, null, null, Coord.get(x, y), r);
-                }
+					if (map[x][y] == '#')
+						continue;
+					// this should ensure no blatant correlation between R and W
+					utility.rng.setState((x << 22) | (y << 16) | (x * y));
+					((StatefulRNG) dijkstra.rng).setState((x << 20) | (y << 14) | (x * y));
+					r = utility.randomFloor(map);
+					dijkstra.setGoal(x, y);
+					dijkstra.setGoal(r);
+					dijkstra.scan(null);
+					dijkstra.clearGoals();
+					dijkstra.findPath(PATH_LENGTH, null, null, Coord.get(x, y), r);
+				}
 			}
-			final long timerEnd = System.currentTimeMillis();
-			return timerEnd - timerStart;
 		}
 
 	}
