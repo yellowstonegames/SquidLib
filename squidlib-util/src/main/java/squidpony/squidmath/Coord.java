dae33543a9a8ea026d2150e073d5b8609b0f14f2
Tommy Ettinger <tommy.ettinger@gmail.com>
2015-10-21 00:16:10 -0700

    Poisson Disk sampling class, more Coord methods

--- squidlib-util/src/main/java/squidpony/squidmath/Coord.java
+++ squidlib-util/src/main/java/squidpony/squidmath/Coord.java
@@ -34,15 +34,182 @@ public class Coord implements java.io.Serializable {
         else return new Coord(x, y);
     }
 
+    /**
+     * Provided for compatibility with earlier code that used the AWT Point API.
+     * @return this Coord, without changes
+     */
     public Coord getLocation()
     {
         return this;
     }
+
+    /**
+     * Takes this Coord, adds x to its x and y to its y, and returns the Coord at that position.
+     * @param x the amount of x distance to move
+     * @param y the amount of y distance to move
+     * @return a Coord (usually cached and not a new instance) that has been moved the specified distance
+     */
     public Coord translate(int x, int y)
     {
         return get(this.x + x, this.y + y);
     }
+    /**
+     * Takes this Coord, adds x to its x and y to its y, limiting x from 0 to width and limiting y from 0 to height,
+     * and returns the Coord at that position.
+     * @param x the amount of x distance to move
+     * @param y the amount of y distance to move
+     * @param width one higher than the maximum x value this can use; typically the length of an array
+     * @param height one higher than the maximum y value this can use; typically the length of an array
+     * @return a Coord (usually cached and not a new instance) that has been moved the specified distance
+     */
+    public Coord translateCapped(int x, int y, int width, int height)
+    {
+        return get(Math.min(Math.max(0, this.x + x), width - 1), Math.min(Math.max(0, this.y + y), height - 1));
+    }
 
+    /**
+     * Separately combines the x and y positions of this Coord and other, producing a different Coord as their "sum."
+     * @param other another Coord
+     * @return a Coord (usually cached and not a new instance) with {@code x = this.x + other.x; y = this.y + other.y}
+     */
+    public Coord add(Coord other)
+    {
+        return get(this.x + other.x, this.y + other.y);
+    }
+
+    /**
+     * Separately adds the x and y positions of this Coord to operand, producing a different Coord as their
+     * "sum."
+     * @param operand a value to add each of x and y to
+     * @return a Coord (usually cached and not a new instance) with {@code x = this.x + operand; y = this.y + operand}
+     */
+    public Coord add(int operand)
+    {
+        return get(this.x + operand, this.y + operand);
+    }
+
+    /**
+     * Separately adds the x and y positions of this Coord to operand, rounding to the nearest int for each of x
+     * and y and producing a different Coord as their "sum."
+     * @param operand a value to add each of x and y to
+     * @return a Coord (usually cached and not a new instance) with {@code x = this.x + operand; y = this.y +
+     *          operand}, with both x and y rounded accordingly
+     */
+    public Coord add(double operand)
+    {
+        return get((int)Math.round(this.x + operand), (int)Math.round(this.y + operand));
+    }
+
+    /**
+     * Separately subtracts the x and y positions of other from this Coord, producing a different Coord as their
+     * "difference."
+     * @param other another Coord
+     * @return a Coord (usually cached and not a new instance) with {@code x = this.x - other.x; y = this.y - other.y}
+     */
+    public Coord subtract(Coord other)
+    {
+        return get(this.x - other.x, this.y - other.y);
+    }
+
+    /**
+     * Separately subtracts operand from the x and y positions of this Coord, producing a different Coord as their
+     * "difference."
+     * @param operand a value to subtract from each of x and y
+     * @return a Coord (usually cached and not a new instance) with {@code x = this.x - operand; y = this.y - operand}
+     */
+    public Coord subtract(int operand)
+    {
+        return get(this.x - operand, this.y - operand);
+    }
+
+    /**
+     * Separately subtracts operand from the x and y positions of this Coord, rounding to the nearest int for each of x
+     * and y and producing a different Coord as their "difference."
+     * @param operand a value to subtract from each of x and y
+     * @return a Coord (usually cached and not a new instance) with {@code x = this.x - operand; y = this.y -
+     *          operand}, with both x and y rounded accordingly
+     */
+    public Coord subtract(double operand)
+    {
+        return get((int)Math.round(this.x - operand), (int)Math.round(this.y - operand));
+    }
+    /**
+     * Separately multiplies the x and y positions of other from this Coord, producing a different Coord as their
+     * "product."
+     * @param other another Coord
+     * @return a Coord (usually cached and not a new instance) with {@code x = this.x * other.x; y = this.y * other.y}
+     */
+    public Coord multiply(Coord other)
+    {
+        return get(this.x * other.x, this.y * other.y);
+    }
+    /**
+     * Separately multiplies the x and y positions of this Coord by operand, producing a different Coord as their
+     * "product."
+     * @param operand a value to multiply each of x and y by
+     * @return a Coord (usually cached and not a new instance) with {@code x = this.x * operand; y = this.y * operand}
+     */
+    public Coord multiply(int operand)
+    {
+        return get(this.x * operand, this.y * operand);
+    }
+
+    /**
+     * Separately multiplies the x and y positions of this Coord by operand, rounding to the nearest int for each of x
+     * and y and producing a different Coord as their "product."
+     * @param operand a value to multiply each of x and y by
+     * @return a Coord (usually cached and not a new instance) with {@code x = this.x * operand; y = this.y *
+     *          operand}, with both x and y rounded accordingly
+     */
+    public Coord multiply(double operand)
+    {
+        return get((int)Math.round(this.x * operand), (int)Math.round(this.y * operand));
+    }
+
+    /**
+     * Separately divides the x and y positions of this Coord by other, producing a different Coord as their
+     * "quotient." If other has 0 for x or y, this will throw an exception, as dividing by 0 is expected to do.
+     * @param other another Coord
+     * @return a Coord (usually cached and not a new instance) with {@code x = this.x / other.x; y = this.y / other.y}
+     */
+    public Coord divide(Coord other)
+    {
+        return get(this.x / other.x, this.y / other.y);
+    }
+    /**
+     * Separately divides the x and y positions of this Coord by operand, producing a different Coord as their
+     * "quotient." If operand is 0, this will throw an exception, as dividing by 0 is expected to do.
+     * @param operand a value to divide each of x and y by
+     * @return a Coord (usually cached and not a new instance) with {@code x = this.x / operand; y = this.y / operand}
+     */
+    public Coord divide(int operand)
+    {
+        return get(this.x / operand, this.y / operand);
+    }
+
+    /**
+     * Separately divides the x and y positions of this Coord by operand, rounding to the nearest int for each of x and
+     * y and producing a different Coord as their "quotient." If operand is 0.0, expect strange results (infinity and
+     * NaN are both possibilities).
+     * @param operand a value to divide each of x and y by
+     * @return a Coord (usually cached and not a new instance) with {@code x = this.x / operand; y = this.y /
+     *          operand}, with both x and y rounded accordingly
+     */
+    public Coord divide(double operand)
+    {
+        return get((int)Math.round(this.x / operand), (int)Math.round(this.y / operand));
+    }
+
+    /**
+     * Separately averages the x and y positions of this Coord with other, producing a different Coord as their
+     * "midpoint."
+     * @param other another Coord
+     * @return a Coord (usually cached and not a new instance) halfway between this and other, rounded nearest.
+     */
+    public Coord average(Coord other)
+    {
+        return get(Math.round((this.x + other.x) / 2.0f), Math.round((this.y + other.y) / 2.0f));
+    }
 	/**
 	 * @param d
 	 *            A non-{@code null} direction.
