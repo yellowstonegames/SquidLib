8665389866dc0280c738c659db39bfa9a25c17ab
Tommy Ettinger <tommy.ettinger@gmail.com>
2015-11-07 16:36:07 -0800

    Starting work on better AI.

---
+++ squidlib-util/src/main/java/squidpony/squidai/ActionEconomy.java
@@ -0,0 +1,12 @@
+package squidpony.squidai;
+
+/**
+ * Handles turn scheduling and specific properties of how actions and turns work for a game as a whole.
+ * For certain AI decisions to be made, the AI may need to be aware of how much it can do without being interrupted,
+ * and the classes that implement this can be important in that decision-making process.
+ *
+ * Created by Tommy Ettinger on 11/7/2015.
+ */
+public interface ActionEconomy {
+
+}
