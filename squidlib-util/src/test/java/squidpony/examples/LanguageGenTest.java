800ae27ba8ed68be0e0b12dcd8e5daace64e4cf4
Tommy Ettinger <tommy.ettinger@gmail.com>
2015-12-01 00:38:05 -0800

    Zut alors! We now have fake French-like generation
    Ebbœu esau extanc glaïbeu siçouche ba baglors.
    That shouldn't mean anything. But in a medieval game it could be fun to
    wander too far from home and have everyone speak nonsense (to the PC)
    that looks like it could be French or Greek!

--- squidlib-util/src/test/java/squidpony/examples/LanguageGenTest.java
+++ squidlib-util/src/test/java/squidpony/examples/LanguageGenTest.java
@@ -10,23 +10,29 @@ import squidpony.squidmath.RNG;
 public class LanguageGenTest {
     public static void main(String[] args)
     {
-        RNG rng = new RNG(new LightRNG(0xdeadbeefca77L));
+        RNG rng = new RNG(new LightRNG(0xf00df00L));
         FakeLanguageGen flg = FakeLanguageGen.LOVECRAFT;
         for (int i = 0; i < 50; i++) {
             System.out.println(flg.sentence(rng, 3, 9, new String[]{",", ",", ";"},
                     new String[]{".", ".", "!", "!", "?", "...", "..."}, 0.15));
         }
-        rng = new RNG(new LightRNG(0xdeadbeefca77L));
+        rng = new RNG(new LightRNG(0xf00df00L));
         flg = FakeLanguageGen.GREEK_ROMANIZED;
         for (int i = 0; i < 50; i++) {
             System.out.println(flg.sentence(rng, 5, 11, new String[]{",", ",", ";"},
                     new String[]{".", ".", ".", "!", "?", "..."}, 0.2));
         }
-        rng = new RNG(new LightRNG(0xdeadbeefca77L));
+        rng = new RNG(new LightRNG(0xf00df00L));
         flg = FakeLanguageGen.GREEK_AUTHENTIC;
         for (int i = 0; i < 50; i++) {
             System.out.println(flg.sentence(rng, 5, 11, new String[]{",", ",", ";"},
                     new String[]{".", ".", ".", "!", "?", "..."}, 0.2));
+        }
+        rng = new RNG(new LightRNG(0xf00df00L));
+        flg = FakeLanguageGen.FRENCH;
+        for (int i = 0; i < 50; i++) {
+            System.out.println(flg.sentence(rng, 4, 12, new String[]{",", ",", ",", ";", ";"},
+                    new String[]{".", ".", ".", "!", "?", "..."}, 0.17));
         }
     }
 }
