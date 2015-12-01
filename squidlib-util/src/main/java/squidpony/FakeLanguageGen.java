0abca7c526f9490913d1448734b04bf11e88ad59
Tommy Ettinger <tommy.ettinger@gmail.com>
2015-12-01 00:38:05 -0800

    Zut alors! We now have fake French-like generation
    Ebbœu esau extanc glaïbeu siçouche ba baglors.
    That shouldn't mean anything. But in a medieval game it could be fun to
    wander too far from home and have everyone speak nonsense (to the PC)
    that looks like it could be French or Greek!

--- squidlib-util/src/main/java/squidpony/FakeLanguageGen.java
+++ squidlib-util/src/main/java/squidpony/FakeLanguageGen.java
@@ -10,7 +10,7 @@ import java.util.Map;
  * Created by Tommy Ettinger on 11/29/2015.
  */
 public class FakeLanguageGen {
-    protected final String[] vowels, openingConsonants, midConsonants, closingConsonants, vowelSplitters,
+    protected final String[] openingVowels, midVowels, openingConsonants, midConsonants, closingConsonants, vowelSplitters,
             closingSyllables;
     protected final LinkedHashMap<Integer, Double> syllableFrequencies;
     protected double totalSyllableFrequency = 0.0;
@@ -20,17 +20,19 @@ public class FakeLanguageGen {
      * Ia! Ia! Cthulhu Rl'yeh ftaghn! Useful for generating cultist ramblings or unreadable occult texts.
      */
     public static final FakeLanguageGen LOVECRAFT = new FakeLanguageGen(
-            new String[]{"a", "i", "o", "e", "u", "a", "i", "o", "e", "u", "ia", "ai", "ei"},
+            new String[]{"a", "i", "o", "e", "u", "a", "i", "o", "e", "u", "ia", "ai", "aa", "ei"},
+            new String[]{},
             new String[]{"s", "t", "k", "n", "y", "p", "k", "l", "g", "gl", "th", "sh", "ny", "ft", "hm", "zvr", "cth"},
             new String[]{"h", "gl", "gr", "nd", "mr", "vr", "kr"},
             new String[]{"l", "p", "s", "t", "n", "k", "g", "x", "rl", "th", "gg", "gh", "ts", "lt", "rk", "kh", "sh", "ng", "shk"},
-            new String[]{"aghn", "ulhu", "urath", "oigor", "alos", "'yeh", "achtal"},
+            new String[]{"aghn", "ulhu", "urath", "oigor", "alos", "'yeh", "achtal", "urath", "ikhet", "adzek"},
             new String[]{"'", "-"}, new int[]{1, 2, 3}, new double[]{6, 7, 2}, 0.4, 0.31, 0.07, 0.04);
     /**
      * Imitation ancient Greek, romanized to use the Latin alphabet. Likely to seem pretty fake to many readers.
      */
     public static final FakeLanguageGen GREEK_ROMANIZED = new FakeLanguageGen(
-            new String[]{"a", "a", "a", "o", "o", "o", "e", "e", "i", "i", "i", "au", "ai", "ai", "oi", "oi", "ia", "ou", "ou", "eo", "ei", "ei", "ui"},
+            new String[]{"a", "a", "a", "o", "o", "o", "e", "e", "i", "i", "i", "au", "ai", "ai", "oi", "oi", "ia", "io", "ou", "ou", "eo", "ei"},
+            new String[]{"ui", "ei"},
             new String[]{"rh", "s", "z", "t", "t", "k", "ch", "n", "th", "kth", "m", "p", "ps", "b", "l", "kr", "g", "phth"},
             new String[]{"lph", "pl", "l", "l", "kr", "nch", "nx", "ps"},
             new String[]{"s", "p", "t", "ch", "n", "m", "s", "p", "t", "ch", "n", "m", "b", "g", "st", "rst", "rt", "sp", "rk", "ph", "x", "z", "nk", "ng", "th"},
@@ -42,7 +44,8 @@ public class FakeLanguageGen {
      * fonts support all the Greek you need for this.
      */
     public static final FakeLanguageGen GREEK_AUTHENTIC = new FakeLanguageGen(
-            new String[]{"α", "α", "α", "ο", "ο", "ο", "ε", "ε", "ι", "ι", "ι", "αυ", "αι", "αι", "οι", "οι", "ια", "ου", "ου", "εο", "ει", "ει", "υι"},
+            new String[]{"α", "α", "α", "ο", "ο", "ο", "ε", "ε", "ι", "ι", "ι", "αυ", "αι", "αι", "οι", "οι", "ια", "ιο", "ου", "ου", "εο", "ει"},
+            new String[]{"υι", "ει"},
             new String[]{"ρ", "σ", "ζ", "τ", "τ", "κ", "χ", "ν", "θ", "κθ", "μ", "π", "ψ", "β", "λ", "κρ", "γ", "ϕθ"},
             new String[]{"λϕ", "πλ", "λ", "λ", "κρ", "γχ", "γξ", "ψ"},
             new String[]{"σ", "π", "τ", "χ", "ν", "μ", "σ", "π", "τ", "χ", "ν", "μ", "β", "γ", "στ", "ρστ", "ρτ", "σπ", "ρκ", "ϕ", "ξ", "ζ", "γκ", "γγ", "θ"},
@@ -50,15 +53,51 @@ public class FakeLanguageGen {
             new String[]{}, new int[]{1, 2, 3}, new double[]{5, 7, 4}, 0.45, 0.45, 0.0, 0.3);
 
     /**
+     *
+     */
+    public static final FakeLanguageGen FRENCH = new FakeLanguageGen(
+            new String[]{"a", "a", "a", "e", "e", "e", "i", "i", "o", "u", "a", "a", "a", "e", "e", "e", "i", "i", "o",
+                    "a", "a", "a", "e", "e", "e", "i", "i", "o", "u", "a", "a", "a", "e", "e", "e", "i", "i", "o",
+                    "a", "a", "e", "e", "i", "o", "a", "a", "a", "e", "e", "e", "i", "i", "o",
+                    "ai", "oi", "oui", "au", "œu", "ou"
+            },
+            new String[]{
+                    "ai", "aie", "aou", "eau", "oi", "oui", "oie", "eu", "eu",
+                    "à", "â", "ai", "aî", "aï", "aie", "aou", "aoû", "au", "ay", "e", "é", "ée", "è",
+                    "ê", "eau", "ei", "eî", "eu", "eû", "i", "î", "ï", "o", "ô", "oe", "oê", "oë", "œu",
+                    "oi", "oie", "oï", "ou", "oû", "oy", "u", "û", "ue"
+            },
+            new String[]{"tr", "ch", "m", "b", "b", "br", "j", "j", "j", "j", "g", "t", "t", "t", "c", "d", "f", "f", "h", "n", "l", "l",
+                     "s", "s", "s", "r", "r", "r", "v", "v", "p", "pl", "pr", "bl", "br", "dr", "gl", "gr"},
+            new String[]{"cqu", "gu", "qu", "rqu", "nt", "ng", "ngu", "mb", "ll", "nd", "ndr", "nct", "st",
+                         "xt", "mbr", "pl", "g", "gg", "ggr", "gl",
+                         "m", "m", "mm", "v", "v", "f", "f", "f", "ff", "b", "b", "bb", "d", "d", "dd", "s", "s", "s", "ss",  "ss", "ss",
+                         "cl", "cr", "ng", "ç", "ç", "rç"},
+            new String[]{},
+            new String[]{"e", "e", "e", "e", "e", "é", "é", "er", "er", "er", "er", "er", "es", "es", "es", "es", "es", "es",
+                    "e", "e", "e", "e", "e", "é", "é", "er", "er", "er", "er", "er", "er", "es", "es", "es", "es", "es",
+                    "e", "e", "e", "e", "e", "é", "é", "é", "er", "er", "er", "er", "er", "es", "es", "es", "es", "es",
+                    "ent", "em", "en", "en", "aim", "ain", "an", "oin", "ien", "iere", "ors", "anse",
+                    "ombs", "ommes", "ancs", "ends", "œufs", "erfs", "ongs", "aps", "ats", "ives", "ui", "illes",
+                    "aen", "aon", "am", "an", "eun", "ein", "age", "age", "uile", "uin", "um", "un", "un", "un",
+                    "aille", "ouille", "eille", "ille", "eur", "it", "ot", "oi", "oi", "oi", "aire", "om", "on", "on",
+                    "im", "in", "in", "ien", "ien", "ion", "il", "eil", "oin", "oint", "iguïté", "ience", "incte",
+                    "ang", "ong", "acré", "eau", "ouche", "oux", "oux", "ect", "ecri", "agne", "uer", "aix", "eth", "ut", "ant",
+                    "anc", "anc", "anche", "ioche", "eaux", "ive", "eur", "ancois", "ecois"},
+            new String[]{}, new int[]{1, 2, 3}, new double[]{18, 7, 2}, 0.35, 1.0, 0.0, 0.55);
+    /**
      * This is a very complicated constructor! Maybe look at the calls to this to initialize static members of this
      * class, LOVECRAFT and GREEK_ROMANIZED.
-     * @param vowels String array where each element is a vowel or group of vowels; elements may be repeated to make
-     *               them more common
+     * @param openingVowels String array where each element is a vowel or group of vowels that may appear at the start
+     *                      of a word or in the middle; elements may be repeated to make them more common
+     * @param midVowels String array where each element is a vowel or group of vowels that may appear in the
+     *                      middle of the word; all openingVowels are automatically copied into this internally.
+     *                      Elements may be repeated to make them more common
      * @param openingConsonants String array where each element is a consonant or consonant cluster that can appear
      *                          at the start of a word; elements may be repeated to make them more common
      * @param midConsonants String array where each element is a consonant or consonant cluster than can appear
-     *                      between vowels, and all closingConsonants are automatically copied into this internally;
-     *                      elements may be repeated to make them more common
+     *                      between vowels; all closingConsonants are automatically copied into this internally.
+     *                      Elements may be repeated to make them more common
      * @param closingConsonants String array where each element is a consonant or consonant cluster than can appear
      *                          at the end of a word; elements may be repeated to make them more common
      * @param closingSyllables String array where each element is a syllable starting with a vowel and ending in
@@ -79,11 +118,14 @@ public class FakeLanguageGen {
      * @param syllableEndFrequency a double between 0.0 and 1.0 that determines how often an element of
      *                             closingSyllables is used instead of ending normally
      */
-    public FakeLanguageGen(String[] vowels, String[] openingConsonants, String[] midConsonants, String[] closingConsonants,
-                           String[] closingSyllables, String[] vowelSplitters,
+    public FakeLanguageGen(String[] openingVowels, String[] midVowels, String[] openingConsonants,
+                           String[] midConsonants, String[] closingConsonants, String[] closingSyllables, String[] vowelSplitters,
                            int[] syllableLengths, double[] syllableFrequencies, double vowelStartFrequency,
                            double vowelEndFrequency, double vowelSplitFrequency, double syllableEndFrequency) {
-        this.vowels = vowels;
+        this.openingVowels = openingVowels;
+        this.midVowels = new String[openingVowels.length + midVowels.length];
+        System.arraycopy(midVowels, 0, this.midVowels, 0, midVowels.length);
+        System.arraycopy(openingVowels, 0, this.midVowels, midVowels.length, openingVowels.length);
         this.openingConsonants = openingConsonants;
         this.midConsonants = new String[midConsonants.length + closingConsonants.length];
         System.arraycopy(midConsonants, 0, this.midConsonants, 0, midConsonants.length);
@@ -130,9 +172,13 @@ public class FakeLanguageGen {
     public String word(RNG rng, boolean capitalize) {
         StringBuilder sb = new StringBuilder(20);
         if (rng.nextDouble() < vowelStartFrequency) {
-            sb.append(rng.getRandomElement(vowels));
+            sb.append(rng.getRandomElement(openingVowels));
+            sb.append(rng.getRandomElement(midConsonants));
         }
-        sb.append(rng.getRandomElement(openingConsonants));
+        else
+        {
+            sb.append(rng.getRandomElement(openingConsonants));
+        }
 
         double syllableChance = rng.nextDouble(totalSyllableFrequency);
         int syllables = 1;
@@ -144,20 +190,20 @@ public class FakeLanguageGen {
                 syllableChance -= kv.getValue();
         }
         for (int i = 0; i < syllables - 1; i++) {
-            sb.append(rng.getRandomElement(vowels));
+            sb.append(rng.getRandomElement(midVowels));
             if (rng.nextDouble() < vowelSplitFrequency) {
                 sb.append(rng.getRandomElement(vowelSplitters));
-                sb.append(rng.getRandomElement(vowels));
+                sb.append(rng.getRandomElement(midVowels));
             }
             sb.append(rng.getRandomElement(midConsonants));
         }
         if (rng.nextDouble() < syllableEndFrequency) {
             sb.append(rng.getRandomElement(closingSyllables));
         } else {
-            sb.append(rng.getRandomElement(vowels));
+            sb.append(rng.getRandomElement(midVowels));
             if (rng.nextDouble() < vowelSplitFrequency) {
                 sb.append(rng.getRandomElement(vowelSplitters));
-                sb.append(rng.getRandomElement(vowels));
+                sb.append(rng.getRandomElement(midVowels));
             }
             if (rng.nextDouble() >= vowelEndFrequency) {
                 sb.append(rng.getRandomElement(closingConsonants));
