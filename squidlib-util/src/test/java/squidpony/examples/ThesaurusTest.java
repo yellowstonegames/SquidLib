96bcaa19349c51cde561b445bec0fa00f6281bd8
Tommy Ettinger <tommy.ettinger@gmail.com>
2016-05-24 17:57:32 -0700

    Start making large list of Thesaurus categories
    This is the type of thing a library can be really good for, since this
    would be tedious to do on your own. There are already keywords for some
    medieval-political words like "dynasties" and "barony", and many
    pre-made, hand-picked imitation-language words drawn from
    "FakeLanguageGen's Greatest Hits", pretty much. There's a small example
    in ThesaurusTest, generating phrases like, "The small state of
    Dabrisazetsky in the Hegemony of Devive, ruled by Shogun Jélebi."

--- squidlib-util/src/test/java/squidpony/examples/ThesaurusTest.java
+++ squidlib-util/src/test/java/squidpony/examples/ThesaurusTest.java
@@ -1,7 +1,6 @@
 package squidpony.examples;
 
 import squidpony.Thesaurus;
-import squidpony.squidmath.StatefulRNG;
 
 import static squidpony.Maker.makeList;
 
@@ -10,12 +9,15 @@ import static squidpony.Maker.makeList;
  */
 public class ThesaurusTest {
     public static void main(String[] args) {
-        Thesaurus thesaurus = new Thesaurus(new StatefulRNG("SquidLib!"));
+        Thesaurus thesaurus = new Thesaurus("SquidLib!"), thesaurus2  = new Thesaurus("SquidLib!");
         thesaurus.addSynonyms(makeList("devil", "fiend", "demon", "horror", "abomination", "terror", "hellspawn"));
         thesaurus.addSynonyms(makeList("despoiler", "defiler", "blighter", "poisoner"));
+        thesaurus2.addKnownCategories().addFakeWords();
         for (int i = 0; i < 12; i++) {
+            //System.out.println(
+            //        thesaurus.process("You fiend! You demon! You despoiler of creation; devil made flesh!"));
             System.out.println(
-                    thesaurus.process("You fiend! You demon! You despoiler of creation; devil made flesh!"));
+                    thesaurus2.process("The small state of Ru`gen` in the Empire`noun` of Fr`gen`, ruled by Duke`noun` So`mod`gen`."));
         }
     }
 }
