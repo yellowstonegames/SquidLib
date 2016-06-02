531d997556da45d856a1931ef95765f3d92229b1
Tommy Ettinger <tommy.ettinger@gmail.com>
2016-06-02 00:39:28 -0700

    3D arrays, also float arrays, encoding/decoding

--- squidlib-util/src/test/java/squidpony/examples/EncodingTest.java
+++ squidlib-util/src/test/java/squidpony/examples/EncodingTest.java
@@ -1,12 +1,14 @@
 package squidpony.examples;
 
+import org.junit.Test;
 import squidpony.StringKit;
 
 /**
  * Created by Tommy Ettinger on 6/1/2016.
  */
 public class EncodingTest {
-    public static void main(String[] args)
+    @Test
+    public void test2D()
     {
         CharSequence cs = StringKit.encode(new byte[][]{
                 new byte[]{1,2, 3},
@@ -16,7 +18,34 @@ public class EncodingTest {
         byte[][] bytes = StringKit.decodeBytes2D(cs);
         for (int x = 0; x < bytes.length; x++) {
             for (int y = 0; y < bytes[x].length; y++) {
-                System.out.print(bytes[x][y]);
+                System.out.print(bytes[x][y] + " ");
+            }
+            System.out.println();
+        }
+    }
+    @Test
+    public void test3D()
+    {
+        CharSequence cs = StringKit.encode(new byte[][][]{
+                {
+                        new byte[]{1, 2, 3},
+                        new byte[]{4, 5, 6},
+                        new byte[]{7, 8, 9}
+                },
+                {
+                        new byte[]{11, 12, 13},
+                        new byte[]{14, 15, 16},
+                        new byte[]{17, 18, 19}
+                },
+        });
+        System.out.println(cs);
+        byte[][][] bytes = StringKit.decodeBytes3D(cs);
+        for (int x = 0; x < bytes.length; x++) {
+            for (int y = 0; y < bytes[x].length; y++) {
+                for (int z = 0; z < bytes[x][y].length; z++) {
+                    System.out.print(bytes[x][y][z] + " ");
+                }
+                System.out.println(',');
             }
             System.out.println();
         }
