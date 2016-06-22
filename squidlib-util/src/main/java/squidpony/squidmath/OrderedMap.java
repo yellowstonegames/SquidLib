f40ec8bca3366aabfd21d0a89b6e93acf77bf9ac
Tommy Ettinger <tommy.ettinger@gmail.com>
2016-06-21 22:34:32 -0700

    Important bugfix for OrderedMap, fixes demos
    This was fixed already in OrderedSet and I thought it didn't show up in
    OrderedMap, but there is a KeySet in OrderedMap that has the same issue.
    Really it affected anything that iterated through an OrderedMap, not
    just the keys, but it seems fixed now. All the demos work as before.

--- squidlib-util/src/main/java/squidpony/squidmath/OrderedMap.java
+++ squidlib-util/src/main/java/squidpony/squidmath/OrderedMap.java
@@ -1064,10 +1064,10 @@ public class OrderedMap<K, V> implements SortedMap<K, V>, java.io.Serializable, 
             if (!hasNext())
                 throw new NoSuchElementException();
             curr = next;
-            if(++index >= order.size - 1)
+            if(index >= order.size - 1)
                 next = -1;
             else
-                next = order.get(index + 1);//(int) link[curr];
+                next = order.get(++index);//(int) link[curr];
             prev = curr;
             return curr;
         }
