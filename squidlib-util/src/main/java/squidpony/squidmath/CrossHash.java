44258717d0252946209780811a6ec69fa60a856f
Tommy Ettinger <tommy.ettinger@gmail.com>
2016-07-03 01:21:48 -0700

    Better support for arrays as OrderedMap/Set Keys
    Before, the default `.hashCode()` method was called on arrays used as
    keys in OrderedMap and OrderedSet (generally on primitive arrays), the
    same as LinkedHashMap/Set. That unfortunately is an identity hash code,
    and two different arrays with identical contents would hash differently
    every time. Now, CrossHash can be used (and is in CoordPacker) to define
    a hashing strategy that can be used in place of the default
    `.hashCode()`. This is simply an extra arg to the constructor that is
    usually a constant in CrossHash that defines a simple hashing strategy,
    but on Java 8 and higher can even be a lambda, though I haven't tested
    that yet. You can create a `DefaultHasher` that will just call
    `.hashCode()` on its input if you want identity hash codes for arrays or
    the normal `.hashCode()` behavior for non-arrays, and one of these is
    used if no IHasher is specified in OrderedMap/Set.

--- squidlib-util/src/main/java/squidpony/squidmath/CrossHash.java
+++ squidlib-util/src/main/java/squidpony/squidmath/CrossHash.java
@@ -1,5 +1,7 @@
 package squidpony.squidmath;
 
+import java.io.Serializable;
+
 /**
  * Simple hashing functions that we can rely on staying the same cross-platform.
  * These use the Fowler/Noll/Vo Hash (FNV-1a) algorithm, which is public domain.
@@ -169,6 +171,25 @@ public class CrossHash {
     }
 
     public static int hash(String[] data)
+    {
+        if(data == null)
+            return 0;
+        int h = -2128831035, len = data.length, t;
+        for (int i = 0; i < len; i++) {
+            t = hash(data[i]);
+            h ^= t & 0xff;
+            h *= 16777619;
+            h ^= (t >>> 8) & 0xff;
+            h *= 16777619;
+            h ^= (t >>> 16) & 0xff;
+            h *= 16777619;
+            h ^= t >>> 24;
+            h *= 16777619;
+        }
+        return h;
+    }
+
+    public static int hash(char[][] data)
     {
         if(data == null)
             return 0;
@@ -375,6 +396,32 @@ public class CrossHash {
         }
         return h;
     }
+    public static long hash64(char[][] data)
+    {
+        if(data == null)
+            return 0;
+        long h = -3750763034362895579L, len = data.length, t;
+        for (int i = 0; i < len; i++) {
+            t = hash64(data[i]);
+            h ^= (t & 0xff);
+            h *= 1099511628211L;
+            h ^= ((t >>> 8) & 0xff);
+            h *= 1099511628211L;
+            h ^= ((t >>> 16) & 0xff);
+            h *= 1099511628211L;
+            h ^= ((t >>> 24) & 0xff);
+            h *= 1099511628211L;
+            h ^= ((t >>> 32) & 0xff);
+            h *= 1099511628211L;
+            h ^= ((t >>> 40) & 0xff);
+            h *= 1099511628211L;
+            h ^= ((t >>> 48) & 0xff);
+            h *= 1099511628211L;
+            h ^= (t >>> 56);
+            h *= 1099511628211L;
+        }
+        return h;
+    }
     public static long hash64(Iterable<String> data)
     {
         if(data == null)
@@ -452,4 +499,106 @@ public class CrossHash {
         }
         return h;
     }
+
+    public interface IHasher extends Serializable
+    {
+        int hash(Object data);
+    }
+    private static class BooleanHasher implements IHasher {
+        BooleanHasher(){};
+        public int hash(Object data)
+        {
+            return (data instanceof boolean[]) ? CrossHash.hash((boolean[]) data) : data.hashCode();
+        }
+    }
+    public static final IHasher booleanHasher = new BooleanHasher();
+
+    private static class ByteHasher implements IHasher {
+        ByteHasher(){};
+        public int hash(Object data)
+        {
+            return (data instanceof byte[]) ? CrossHash.hash((byte[]) data) : data.hashCode();
+        }
+    }
+    public static final IHasher byteHasher = new ByteHasher();
+    private static class ShortHasher implements IHasher {
+        ShortHasher(){};
+        public int hash(Object data)
+        {
+            return (data instanceof short[]) ? CrossHash.hash((short[]) data) : data.hashCode();
+        }
+    }
+    public static final IHasher shortHasher = new ShortHasher();
+    private static class CharHasher implements IHasher {
+        CharHasher(){};
+        public int hash(Object data)
+        {
+            return (data instanceof char[]) ? CrossHash.hash((char[]) data) : data.hashCode();
+        }
+    }
+    public static final IHasher charHasher = new CharHasher();
+    private static class IntHasher implements IHasher {
+        IntHasher(){};
+        public int hash(Object data)
+        {
+            return (data instanceof int[]) ? CrossHash.hash((int[]) data) : data.hashCode();
+        }
+    }
+    public static final IHasher intHasher = new IntHasher();
+    private static class LongHasher implements IHasher {
+        LongHasher(){};
+        public int hash(Object data)
+        {
+            return (data instanceof long[]) ? CrossHash.hash((long[]) data) : data.hashCode();
+        }
+    }
+    public static final IHasher longHasher = new LongHasher();
+    private static class FloatHasher implements IHasher {
+        FloatHasher(){};
+        public int hash(Object data)
+        {
+            return (data instanceof float[]) ? CrossHash.hash((float[]) data) : data.hashCode();
+        }
+    }
+    public static final IHasher floatHasher = new FloatHasher();
+    private static class DoubleHasher implements IHasher {
+        DoubleHasher(){};
+        public int hash(Object data)
+        {
+            return (data instanceof double[]) ? CrossHash.hash((double[]) data) : data.hashCode();
+        }
+    }
+    public static final IHasher doubleHasher = new DoubleHasher();
+    private static class Char2DHasher implements IHasher {
+        Char2DHasher(){};
+        public int hash(Object data)
+        {
+            return (data instanceof char[][]) ? CrossHash.hash((char[][]) data) : data.hashCode();
+        }
+    }
+    public static final IHasher char2DHasher = new Char2DHasher();
+    private static class StringHasher implements IHasher {
+        StringHasher(){};
+        public int hash(Object data)
+        {
+            return (data instanceof String) ? CrossHash.hash((String) data) : data.hashCode();
+        }
+    }
+    public static final IHasher stringHasher = new StringHasher();
+    private static class StringArrayHasher implements IHasher {
+        StringArrayHasher(){};
+        public int hash(Object data)
+        {
+            return (data instanceof String[]) ? CrossHash.hash((String[]) data) : data.hashCode();
+        }
+    }
+    public static final IHasher stringArrayHasher = new StringArrayHasher();
+    public static class DefaultHasher implements IHasher {
+        public DefaultHasher(){};
+        public int hash(Object data)
+        {
+            return data.hashCode();
+        }
+    }
+
 }
