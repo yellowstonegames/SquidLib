57364ae4ec6180bbc83c40d28da33ee57f7cf367
Tommy Ettinger <tommy.ettinger@gmail.com>
2016-08-28 02:14:24 -0700

    Add Storm hash family, tweaks to Lightning
    Lightning had a slight flaw, possibly, where the least significant bit
    was significantly less likely to be set, though a bitwise rotation after
    the flaw occured distributed it across all 64 bits.
    
    Storm is based on Lightning and is about half the speed, but allows a
    64-bit salt to modify the hashing function in a wide amount of ways
    (halfway on its way to Sip, which allows 128 bits of salt and is about a
    quarter the speed of Lightning). The idea is to maybe replace Sip at
    some point with something that has similar quality, and Storm seems only
    slightly worse on visual quality and significantly better on collision
    rates. Sip is mainly a lot of code for such a similar result to
    Lightning and Storm, and it is just not fast enough to warrant its
    continued inclusion, in my opinion.
    
    String hashing functions in Lightning and Storm now don't create a
    temporary char[] and just read chars from the String, to avoid creating
    garbage.

--- squidlib-performance/src/main/java/squidpony/performance/HashBenchmark.java
+++ squidlib-performance/src/main/java/squidpony/performance/HashBenchmark.java
@@ -202,6 +202,43 @@ public class HashBenchmark {
         doJVMInt();
     }
 
+    public long doStorm()
+    {
+        LongPeriodRNG rng = new LongPeriodRNG(seed);
+        CrossHash.Storm storm = new CrossHash.Storm();
+        for (int i = 0; i < 1000000; i++) {
+            rng.nextLong();
+            seed += storm.hash64(rng.state);
+        }
+        return seed;
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void measureStorm() throws InterruptedException {
+        seed = 9000;
+        doStorm();
+    }
+
+    public long doStormInt()
+    {
+        LongPeriodRNG rng = new LongPeriodRNG(iseed);
+        CrossHash.Storm storm = new CrossHash.Storm();
+        for (int i = 0; i < 1000000; i++) {
+            rng.nextLong();
+            iseed += storm.hash(rng.state);
+        }
+        return iseed;
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void measureStormInt() throws InterruptedException {
+        iseed = 9000;
+        doStormInt();
+    }
     /*
      * ============================== HOW TO RUN THIS TEST: ====================================
      *
