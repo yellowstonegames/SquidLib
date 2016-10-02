efa08a889282bb1c2572eb9cedc3767e807ba231
Tommy Ettinger <tommy.ettinger@gmail.com>
2016-10-02 15:26:48 -0700

    Many small changes
    Removed encoding methods in StringKit (they didn't help reduce the size
    of textual representations of numbers). Improvements to speed for
    MerlinNoise, especially in 3D. Re-added some methods that slipped by me
    when I added them to the stand-alone RNG project, Sarong, but are also
    useful here (in GwtCompatibility, including invertOrdering for the
    orderings that RNG can produce, and reorder for ArrayLists as well as
    the existing OrderedMap/Set stuff). RNG now has a shuffleInPlace method
    that is GWT-compatible.
    
    This commit seems huge because it removes timestamps on all the docs,
    but future commits shouldn't change every javadoc HTML page just to
    update timestamps, since I set some configuration in Maven to avoid that
    from now on.

--- squidlib-util/src/main/java/squidpony/GwtCompatibility.java
+++ squidlib-util/src/main/java/squidpony/GwtCompatibility.java
@@ -2,6 +2,7 @@ package squidpony;
 
 import squidpony.squidmath.Coord;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 
@@ -340,4 +341,67 @@ public class GwtCompatibility {
         }
         return next;
     }
+    /**
+     * Rearranges an ArrayList to use the given ordering, returning a copy; random orderings can be produced with
+     * {@link squidpony.squidmath.RNG#randomOrdering(int)} or
+     * {@link squidpony.squidmath.RNG#randomOrdering(int, int[])}. These orderings will never repeat an earlier element,
+     * and the returned ArrayList may be shorter than the original if {@code ordering} isn't as long as {@code list}.
+     * Using a random ordering is like shuffling, but allows you to repeat the shuffle exactly on other collections of
+     * the same size. A reordering can also be inverted with {@link #invertOrdering(int[])} or
+     * {@link #invertOrdering(int[], int[])}, getting the change that will undo another ordering.
+     * @param list an ArrayList that you want a reordered version of; will not be modified.
+     * @param ordering an ordering, typically produced by one of RNG's randomOrdering methods.
+     * @param <T> any generic type
+     * @return a modified copy of {@code list} with its ordering changed to match {@code ordering}.
+     */
+    public static <T> ArrayList<T> reorder (ArrayList<T> list, int... ordering) {
+        int ol;
+        if (list == null || ordering == null || (ol = Math.min(list.size(), ordering.length)) == 0)
+            return list;
+        ArrayList<T> alt = new ArrayList<T>(ol);
+        for (int i = 0; i < ol; i++) {
+            alt.add(list.get((ordering[i] % ol + ol) % ol));
+        }
+        return alt;
+    }
+
+    /**
+     * Given an ordering such as one produced by {@link squidpony.squidmath.RNG#randomOrdering(int, int[])}, this finds
+     * its inverse, able to reverse the reordering and vice versa.
+     * @param ordering the ordering to find the inverse for
+     * @return the inverse of ordering
+     */
+    public static int[] invertOrdering(int[] ordering)
+    {
+        int ol = 0;
+        if(ordering == null || (ol = ordering.length) == 0) return ordering;
+        int[] next = new int[ol];
+        for (int i = 0; i < ol; i++) {
+            if(ordering[i] < 0 || ordering[i] >= ol) return next;
+            next[ordering[i]] = i;
+        }
+        return next;
+    }
+
+    /**
+     * Given an ordering such as one produced by {@link squidpony.squidmath.RNG#randomOrdering(int, int[])}, this finds
+     * its inverse, able to reverse the reordering and vice versa. This overload doesn't allocate a new int
+     * array, and instead relies on having an int array of the same size as ordering passed to it as an
+     * additional argument.
+     * @param ordering the ordering to find the inverse for
+     * @param dest the int array to put the inverse reordering into; should have the same length as ordering
+     * @return the inverse of ordering; will have the same value as dest
+     */
+    public static int[] invertOrdering(int[] ordering, int[] dest)
+    {
+        int ol = 0;
+        if(ordering == null || dest == null || (ol = Math.min(ordering.length, dest.length)) == 0)
+            return ordering;
+        for (int i = 0; i < ol; i++) {
+            if(ordering[i] < 0 || ordering[i] >= ol) return dest;
+            dest[ordering[i]] = i;
+        }
+        return dest;
+    }
+
 }
