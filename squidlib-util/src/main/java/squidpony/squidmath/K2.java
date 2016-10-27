403eec239dc3d87bac3c23e35cb8ddcae449944a
Tommy Ettinger <tommy.ettinger@gmail.com>
2016-10-27 14:10:52 -0700

    Rename TwoKey to K2, add K2V1 (keys AND values)
    K2V1 has more of a Map-like role here, allowing 2 kinds of key (plus an
    int index) associated together with a single value, where K2 is more of
    a Set since it only allows unique A and B keys (plus an int index). The
    APIs are similar and may have an interface made that will make going
    between them easier. SquidStorage can (de-)serialize K2 and K2V1 values.

--- squidlib-util/src/main/java/squidpony/squidmath/K2.java
+++ squidlib-util/src/main/java/squidpony/squidmath/K2.java
@@ -6,58 +6,60 @@ import java.util.Iterator;
 import java.util.SortedSet;
 
 /**
- * An ordered bidirectional map-like data structure, with unique A items and unique B items updated together like a map
+ * An ordered bidirectional map-like data structure, with unique A keys and unique B keys updated together like a map
  * that can be queried by A keys, B keys, or int indices. Does not implement any interfaces that you would expect for a
  * data structure, because almost every method it has needs to specify whether it applies to A or B items, but you can
- * get collections that implement SortedSet of its A or B items.
+ * get collections that implement SortedSet of its A or B keys.
+ * <br>
+ * Called K2 because it has 2 key sets; other collections can have other keys or have values, like K2V1.
  * Created by Tommy Ettinger on 10/25/2016.
  */
 @Beta
-public class TwoKey<A, B>
+public class K2<A, B>
 {
-    protected Arrangement<A> keysA;
-    protected Arrangement<B> keysB;
+    Arrangement<A> keysA;
+    Arrangement<B> keysB;
 
     /**
-     * Constructs an empty TwoKey.
+     * Constructs an empty K2.
      */
-    public TwoKey()
+    public K2()
     {
         keysA = new Arrangement<>();
         keysB = new Arrangement<>();
     }
 
     /**
-     * Constructs a TwoKey with the expected number of indices to hold (the number of A and number of B items is always
+     * Constructs a K2 with the expected number of indices to hold (the number of A and number of B items is always
      * the same, and this will be more efficient if expected is greater than that number).
      * @param expected
      */
-    public TwoKey(int expected)
+    public K2(int expected)
     {
         keysA = new Arrangement<>(expected);
         keysB = new Arrangement<>(expected);
     }
 
     /**
-     * Constructs a TwoKey with the expected number of indices to hold (the number of A and number of B items is always
+     * Constructs a K2 with the expected number of indices to hold (the number of A and number of B items is always
      * the same, and this will be more efficient if expected is greater than that number) and the load factor to use,
      * between 0.1f and 0.8f usually (using load factors higher than 0.8f can cause problems).
      * @param expected the amount of indices (the count of A items is the same as the count of B items) this should hold
      * @param f the load factor, probably between 0.1f and 0.8f
      */
-    public TwoKey(int expected, float f)
+    public K2(int expected, float f)
     {
         keysA = new Arrangement<>(expected, f);
         keysB = new Arrangement<>(expected, f);
     }
 
     /**
-     * Constructs a TwoKey from a pair of Iterables that will be processed in pairs, adding a unique A from aKeys if and
+     * Constructs a K2 from a pair of Iterables that will be processed in pairs, adding a unique A from aKeys if and
      * only if it can also add a unique B from bKeys, otherwise skipping that pair.
      * @param aKeys an Iterable of A that should all be unique
      * @param bKeys an Iterable of B that should all be unique
      */
-    public TwoKey(Iterable<A> aKeys, Iterable<B> bKeys)
+    public K2(Iterable<A> aKeys, Iterable<B> bKeys)
     {
         keysA = new Arrangement<>();
         keysB = new Arrangement<>();
@@ -65,7 +67,7 @@ public class TwoKey<A, B>
             putAll(aKeys, bKeys);
     }
 
-    public TwoKey(TwoKey<A, B> other)
+    public K2(K2<A, B> other)
     {
         if(other == null)
         {
@@ -79,7 +81,7 @@ public class TwoKey<A, B>
         }
     }
 
-    public TwoKey(Arrangement<A> aItems, Arrangement<B> bItems)
+    public K2(Arrangement<A> aItems, Arrangement<B> bItems)
     {
         if(aItems == null || bItems == null)
         {
@@ -109,12 +111,13 @@ public class TwoKey<A, B>
     /**
      * Returns true if index is between 0 (inclusive) and {@link #size()} (exclusive), or false otherwise.
      * @param index the index to check
-     * @return true if index is a valid index in the ordering of this TwoKey
+     * @return true if index is a valid index in the ordering of this K2
      */
     public boolean containsIndex(int index) { return keysA.containsValue(index); }
 
     /**
-     * Given an A object key, finds the position in the ordering that A has, or -1 if key is not present
+     * Given an A object key, finds the position in the ordering that A has, or -1 if key is not present.
+     * Unlike {@link java.util.List#indexOf(Object)}, this runs in constant time.
      * @param key the A to find the position of
      * @return the int index of key in the ordering, or -1 if it is not present
      */
@@ -123,7 +126,8 @@ public class TwoKey<A, B>
         return keysA.getInt(key);
     }
     /**
-     * Given a B object key, finds the position in the ordering that B has, or -1 if key is not present
+     * Given a B object key, finds the position in the ordering that B has, or -1 if key is not present.
+     * Unlike {@link java.util.List#indexOf(Object)}, this runs in constant time.
      * @param key the B to find the position of
      * @return the int index of key in the ordering, or -1 if it is not present
      */
@@ -134,7 +138,7 @@ public class TwoKey<A, B>
 
     /**
      * Given an int index, finds the associated A key (using index as a point in the ordering).
-     * @param index an int index into this TwoKey
+     * @param index an int index into this K2
      * @return the A object with index for its position in the ordering, or null if index was invalid
      */
     public A getAAt(int index)
@@ -143,7 +147,7 @@ public class TwoKey<A, B>
     }
     /**
      * Given an int index, finds the associated B key (using index as a point in the ordering).
-     * @param index an int index into this TwoKey
+     * @param index an int index into this K2
      * @return the B object with index for its position in the ordering, or null if index was invalid
      */
     public B getBAt(int index)
@@ -172,7 +176,7 @@ public class TwoKey<A, B>
     }
 
     /**
-     * Gets a random A from this TwoKey using the given RNG.
+     * Gets a random A from this K2 using the given RNG.
      * @param random generates a random index to get an A with
      * @return a randomly chosen A, or null if this is empty
      */
@@ -182,7 +186,7 @@ public class TwoKey<A, B>
     }
 
     /**
-     * Gets a random B from this TwoKey using the given RNG.
+     * Gets a random B from this K2 using the given RNG.
      * @param random generates a random index to get a B with
      * @return a randomly chosen B, or null if this is empty
      */
@@ -191,14 +195,14 @@ public class TwoKey<A, B>
         return keysB.randomKey(random);
     }
     /**
-     * Changes an existing A key, {@code past}, to another A key, {@code future}, if past exists in this TwoKey
-     * and future does not yet exist in this TwoKey. This will retain past's point in the ordering for future, so
+     * Changes an existing A key, {@code past}, to another A key, {@code future}, if past exists in this K2
+     * and future does not yet exist in this K2. This will retain past's point in the ordering for future, so
      * the associated other key(s) will still be associated in the same way.
-     * @param past an A key, that must exist in this TwoKey's A keys, and will be changed
-     * @param future an A key, that cannot currently exist in this TwoKey's A keys, but will if this succeeds
+     * @param past an A key, that must exist in this K2's A keys, and will be changed
+     * @param future an A key, that cannot currently exist in this K2's A keys, but will if this succeeds
      * @return this for chaining
      */
-    public TwoKey<A, B> alterA(A past, A future)
+    public K2<A, B> alterA(A past, A future)
     {
         if(keysA.containsKey(past) && !keysA.containsKey(future))
             keysA.alter(past, future);
@@ -206,14 +210,14 @@ public class TwoKey<A, B>
     }
 
     /**
-     * Changes an existing B key, {@code past}, to another B key, {@code future}, if past exists in this TwoKey
-     * and future does not yet exist in this TwoKey. This will retain past's point in the ordering for future, so
+     * Changes an existing B key, {@code past}, to another B key, {@code future}, if past exists in this K2
+     * and future does not yet exist in this K2. This will retain past's point in the ordering for future, so
      * the associated other key(s) will still be associated in the same way.
-     * @param past a B key, that must exist in this TwoKey's B keys, and will be changed
-     * @param future a B key, that cannot currently exist in this TwoKey's B keys, but will if this succeeds
+     * @param past a B key, that must exist in this K2's B keys, and will be changed
+     * @param future a B key, that cannot currently exist in this K2's B keys, but will if this succeeds
      * @return this for chaining
      */
-    public TwoKey<A, B> alterB(B past, B future)
+    public K2<A, B> alterB(B past, B future)
     {
         if(keysB.containsKey(past) && !keysB.containsKey(future))
             keysB.alter(past, future);
@@ -222,14 +226,14 @@ public class TwoKey<A, B>
 
     /**
      * Changes the A key at {@code index} to another A key, {@code future}, if index is valid and future does not
-     * yet exist in this TwoKey. The position in the ordering for future will be the same as index, and the same
+     * yet exist in this K2. The position in the ordering for future will be the same as index, and the same
      * as the key this replaced, if this succeeds, so the other key(s) at that position will still be associated in
      * the same way.
      * @param index a position in the ordering to change; must be at least 0 and less than {@link #size()}
-     * @param future an A key, that cannot currently exist in this TwoKey's A keys, but will if this succeeds
+     * @param future an A key, that cannot currently exist in this K2's A keys, but will if this succeeds
      * @return this for chaining
      */
-    public TwoKey<A, B> alterAAt(int index, A future)
+    public K2<A, B> alterAAt(int index, A future)
     {
         if(!keysA.containsKey(future) && index >= 0 && index < keysA.size)
             keysA.alter(keysA.keyAt(index), future);
@@ -239,94 +243,115 @@ public class TwoKey<A, B>
 
     /**
      * Changes the B key at {@code index} to another B key, {@code future}, if index is valid and future does not
-     * yet exist in this TwoKey. The position in the ordering for future will be the same as index, and the same
+     * yet exist in this K2. The position in the ordering for future will be the same as index, and the same
      * as the key this replaced, if this succeeds, so the other key(s) at that position will still be associated in
      * the same way.
      * @param index a position in the ordering to change; must be at least 0 and less than {@link #size()}
-     * @param future a B key, that cannot currently exist in this TwoKey's B keys, but will if this succeeds
+     * @param future a B key, that cannot currently exist in this K2's B keys, but will if this succeeds
      * @return this for chaining
      */
-    public TwoKey<A, B> alterBAt(int index, B future)
+    public K2<A, B> alterBAt(int index, B future)
     {
         if(!keysB.containsKey(future) && index >= 0 && index < keysB.size)
             keysB.alter(keysB.keyAt(index), future);
         return this;
     }
     /**
-     * Adds an A key and a B key at the same point in the ordering to this TwoKey. Neither parameter can be
+     * Adds an A key and a B key at the same point in the ordering (the end) to this K2. Neither parameter can be
      * present in this collection before this is called. If you want to change or update an existing key, use
      * {@link #alterA(Object, Object)} or {@link #alterB(Object, Object)}.
-     * @param a an A value to add; cannot already be present
-     * @param b a B value to add; cannot already be present
-     * @return this for chaining
+     * @param a an A key to add; cannot already be present
+     * @param b a B key to add; cannot already be present
+     * @return true if this collection changed as a result of this call
      */
-    public TwoKey<A, B> put(A a, B b)
+    public boolean put(A a, B b)
     {
         if(!keysA.containsKey(a) && !keysB.containsKey(b))
         {
             keysA.add(a);
             keysB.add(b);
+            return true;
         }
-        return this;
+        return false;
     }
 
     /**
-     * Puts all unique A and B keys in {@code aKeys} and {@code bKeys} into this TwoKey. If an A in aKeys or a B
+     * Puts all unique A and B keys in {@code aKeys} and {@code bKeys} into this K2 at the end. If an A in aKeys or a B
      * in bKeys is already present when this would add one, this will not put the A and B keys at that point in the
      * iteration order, and will place the next unique A and B it finds in the arguments at that position instead.
      * @param aKeys an Iterable or Collection of A keys to add; should all be unique (like a Set)
      * @param bKeys an Iterable or Collection of B keys to add; should all be unique (like a Set)
-     * @return this for chaining
+     * @return true if this collection changed as a result of this call
      */
-    public TwoKey<A, B> putAll(Iterable<A> aKeys, Iterable<B> bKeys)
+    public boolean putAll(Iterable<A> aKeys, Iterable<B> bKeys)
     {
+        if(aKeys == null || bKeys == null) return false;
         Iterator<A> aIt = aKeys.iterator();
         Iterator<B> bIt = bKeys.iterator();
+        boolean changed = false;
         while (aIt.hasNext() && bIt.hasNext())
         {
-            put(aIt.next(), bIt.next());
+            changed = put(aIt.next(), bIt.next()) || changed;
         }
-        return this;
+        return changed;
+    }
+    /**
+     * Puts all unique A and B keys in {@code other} into this K2, respecting other's ordering. If an A or a B in other
+     * is already present when this would add one, this will not put the A and B keys at that point in the iteration
+     * order, and will place the next unique A and B it finds in the arguments at that position instead.
+     * @param other another K2 collection with the same A and B types
+     * @return true if this collection changed as a result of this call
+     */
+    public boolean putAll(K2<A, B> other)
+    {
+        if(other == null) return false;
+        boolean changed = false;
+        int sz = other.size();
+        for (int i = 0; i < sz; i++) {
+            changed = put(other.getAAt(i), other.getBAt(i)) || changed;
+        }
+        return changed;
     }
 
     /**
-     * Adds an A key and a B key at the given index in the ordering to this TwoKey. Neither a nor b can be
+     * Adds an A key and a B key at the given index in the ordering to this K2. Neither a nor b can be
      * present in this collection before this is called. If you want to change or update an existing key, use
      * {@link #alterA(Object, Object)} or {@link #alterB(Object, Object)}. The index this is given should be at
      * least 0 and no greater than {@link #size()}.
      * @param index the point in the ordering to place a and b into; later entries will be shifted forward
-     * @param a an A value to add; cannot already be present
-     * @param b a B value to add; cannot already be present
-     * @return this for chaining
+     * @param a an A key to add; cannot already be present
+     * @param b a B key to add; cannot already be present
+     * @return true if this collection changed as a result of this call
      */
-    public TwoKey<A, B> putAt(int index, A a, B b)
+    public boolean putAt(int index, A a, B b)
     {
         if(!keysA.containsKey(a) && !keysB.containsKey(b))
         {
             keysA.addAt(index, a);
             keysB.addAt(index, b);
+            return true;
         }
-        return this;
+        return false;
     }
 
     /**
-     * Removes a given A key, if {@code removing} exists in this TwoKey's A keys, and also removes any keys
+     * Removes a given A key, if {@code removing} exists in this K2's A keys, and also removes any keys
      * associated with its point in the ordering.
      * @param removing the A key to remove
      * @return this for chaining
      */
-    public TwoKey<A, B> removeA(A removing)
+    public K2<A, B> removeA(A removing)
     {
         keysB.removeAt(keysA.removeInt(removing));
         return this;
     }
     /**
-     * Removes a given B key, if {@code removing} exists in this TwoKey's B keys, and also removes any keys
+     * Removes a given B key, if {@code removing} exists in this K2's B keys, and also removes any keys
      * associated with its point in the ordering.
      * @param removing the B key to remove
      * @return this for chaining
      */
-    public TwoKey<A, B> removeB(B removing)
+    public K2<A, B> removeB(B removing)
     {
         keysA.removeAt(keysB.removeInt(removing));
         return this;
@@ -336,7 +361,7 @@ public class TwoKey<A, B>
      * @param index the position in the ordering to remove
      * @return this for chaining
      */
-    public TwoKey<A, B> removeAt(int index)
+    public K2<A, B> removeAt(int index)
     {
         keysA.removeAt(index);
         keysB.removeAt(index);
@@ -344,7 +369,7 @@ public class TwoKey<A, B>
     }
 
     /**
-     * Reorders this TwoKey using {@code ordering}, which have the same length as this TwoKey's {@link #size()}
+     * Reorders this K2 using {@code ordering}, which have the same length as this K2's {@link #size()}
      * and can be generated with {@link squidpony.GwtCompatibility#range(int)} (which, if applied, would produce no
      * change to the current ordering), {@link RNG#randomOrdering(int)} (which gives a random ordering, and if
      * applied immediately would be the same as calling {@link #shuffle(RNG)}), or made in some other way. If you
@@ -353,7 +378,7 @@ public class TwoKey<A, B>
      * @param ordering an int array or vararg that should contain each int from 0 to {@link #size()} (or less)
      * @return this for chaining
      */
-    public TwoKey<A, B> reorder(int... ordering)
+    public K2<A, B> reorder(int... ordering)
     {
         keysA.reorder(ordering);
         keysB.reorder(ordering);
@@ -366,7 +391,7 @@ public class TwoKey<A, B>
      * @param rng an RNG to produce the random ordering this will use
      * @return this for chaining
      */
-    public TwoKey<A, B> shuffle(RNG rng)
+    public K2<A, B> shuffle(RNG rng)
     {
         int[] ordering = rng.randomOrdering(keysA.size);
         keysA.reorder(ordering);
@@ -378,7 +403,7 @@ public class TwoKey<A, B>
      * Creates a new iterator over the A keys this holds. This can be problematic for garbage collection if called very
      * frequently; it may be better to access items by index (which also lets you access other keys associated with that
      * index) using {@link #getAAt(int)} in a for(int i...) loop.
-     * @return a newly-created iterator over this TwoKey's A keys
+     * @return a newly-created iterator over this K2's A keys
      */
     public Iterator<A> iteratorA()
     {
@@ -388,7 +413,7 @@ public class TwoKey<A, B>
      * Creates a new iterator over the B keys this holds. This can be problematic for garbage collection if called very
      * frequently; it may be better to access items by index (which also lets you access other keys associated with that
      * index) using {@link #getBAt(int)} in a for(int i...) loop.
-     * @return a newly-created iterator over this TwoKey's B keys
+     * @return a newly-created iterator over this K2's B keys
      */
     public Iterator<B> iteratorB()
     {
@@ -428,6 +453,9 @@ public class TwoKey<A, B>
 
     public int keyCount() {
         return 2;
+    }
+    public int valueCount() {
+        return 0;
     }
 
     public int size() {
--- squidlib-util/src/main/java/squidpony/squidmath/MultiKey.java
+++ squidlib-util/src/main/java/squidpony/squidmath/MultiKey.java
@@ -3,8 +3,8 @@ package squidpony.squidmath;
 import squidpony.annotation.Beta;
 
 /**
- * Currently, you should see {@link TwoKey} for an ordered bidirectional map; this class, MultiKey, should eventually
- * allow more than two keys to be used.
+ * Currently, you should see {@link K2} for an ordered bidirectional map; this class, MultiKey, should eventually
+ * allow more than two keys to be used, although without generic type parameters.
  * Created by Tommy Ettinger on 10/23/2016.
  */
 @Beta
