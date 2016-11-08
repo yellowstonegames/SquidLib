7d9775f0864eb264944ac6dfe34c191647389071
Tommy Ettinger <tommy.ettinger@gmail.com>
2016-11-08 02:09:06 -0800

    Bring in Messaging from related Gabber project
    This is a handy class for writing messages that the user should see, and
    upholding the DRY principle while doing so. It handles conjugation of
    English verbs and pronouns in a fairly robust way, adjusts nouns for
    pluralization, and is extensible to handle irregular words. It can
    handle just about any gender preference need, possibly by delegating to
    lib-using code that can post-process its output, but usually doesn't
    need any hard work. It currently is limited to one "user" and one
    "target", both of which can be singular or plural, but can't deal with
    the ambiguity of multiple targets. I hope it's useful! There's examples
    in the javadocs.

---
+++ squidlib-util/src/test/java/squidpony/examples/MessagingTest.java
@@ -0,0 +1,48 @@
+package squidpony.examples;
+import squidpony.Messaging;
+import static squidpony.Messaging.NounTrait.*;
+
+/**
+ * Created by Tommy Ettinger on 11/2/2016.
+ */
+public class MessagingTest {
+    public static void main(String[] args)
+    {
+        String message = "@Name hit$ ^ for ~ ~ damage and ~ ~ damage!";
+        System.out.println(Messaging.transform(message, "Heero Supra", SECOND_PERSON_SINGULAR, "the goblin", MALE_GENDER, "10", "bludgeoning", "4", "lightning"));
+        System.out.println(Messaging.transform(message, "the goblin", MALE_GENDER, "Heero Supra", SECOND_PERSON_SINGULAR, "10", "poison", "3", "piercing"));
+
+        message = "@Name spit$ in ^name_s face^s!";
+        System.out.println(Messaging.transform(message, "Heero Supra", SECOND_PERSON_SINGULAR, "the goblin", MALE_GENDER));
+        System.out.println(Messaging.transform(message, "Heero Supra", SECOND_PERSON_SINGULAR, "the goblins", GROUP));
+        System.out.println(Messaging.transform(message, "the goblin", MALE_GENDER, "Heero Supra", SECOND_PERSON_SINGULAR));
+        System.out.println(Messaging.transform(message, "the goblins", GROUP, "Heero Supra", SECOND_PERSON_SINGULAR));
+
+        message = "@Name @don_t care what ^name think^$, @i'll get @myself onto that dancefloor!";
+        System.out.println(Messaging.transform(message, "Heero Supra", SECOND_PERSON_SINGULAR, "the goblin", MALE_GENDER));
+        System.out.println(Messaging.transform(message, "Heero Supra", SECOND_PERSON_SINGULAR, "the goblins", GROUP));
+        System.out.println(Messaging.transform(message, "the goblin", MALE_GENDER, "Heero Supra", SECOND_PERSON_SINGULAR));
+        System.out.println(Messaging.transform(message, "the goblins", GROUP, "Heero Supra", SECOND_PERSON_SINGULAR));
+
+        message = "@Name@m gonna try out the escapes! \\@, \\^ \\$\\~!";
+        System.out.println(Messaging.transform(message, "Heero Supra", FIRST_PERSON_SINGULAR));
+        System.out.println(Messaging.transform(message, "Heero Supra", SECOND_PERSON_SINGULAR));
+        System.out.println(Messaging.transform(message, "the goblin", MALE_GENDER, "Heero Supra", SECOND_PERSON_SINGULAR));
+        System.out.println(Messaging.transform(message, "the goblins", GROUP, "Heero Supra", SECOND_PERSON_SINGULAR));
+
+        message = "@I @am @my own boss@ss.";
+        System.out.println(Messaging.transform(message, "Captain Spectacular", FIRST_PERSON_SINGULAR));
+        System.out.println(Messaging.transform(message, "Captain Spectacular", FIRST_PERSON_PLURAL));
+        System.out.println(Messaging.transform(message, "Captain Spectacular", SECOND_PERSON_SINGULAR));
+        System.out.println(Messaging.transform(message, "Captain Spectacular", SECOND_PERSON_PLURAL));
+        System.out.println(Messaging.transform(message, "Captain Spectacular", NO_GENDER));
+        System.out.println(Messaging.transform(message, "Captain Spectacular", MALE_GENDER));
+        System.out.println(Messaging.transform(message, "Captain Spectacular", FEMALE_GENDER));
+        System.out.println(Messaging.transform(message, "Captain Spectacular", UNSPECIFIED_GENDER));
+        System.out.println(Messaging.transform(message, "Captain Spectacular", ADDITIONAL_GENDER));
+        System.out.println(Messaging.transform(message, "Captain Spectacular", SPECIAL_CASE_GENDER));
+        System.out.println(Messaging.transform(message, "Captain Spectacular", GROUP));
+
+
+    }
+}
