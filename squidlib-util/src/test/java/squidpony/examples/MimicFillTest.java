2d7427a21859a4123119af9bdf43f8cd846a4c88
Tommy Ettinger <tommy.ettinger@gmail.com>
2016-12-18 02:27:56 -0800

    Optimize MimicFill more, varied SColor preset...
    ... and GreasedRegion.flip() to change just one bit. A nice little grab
    bag.

--- squidlib-util/src/test/java/squidpony/examples/MimicFillTest.java
+++ squidlib-util/src/test/java/squidpony/examples/MimicFillTest.java
@@ -2,11 +2,7 @@ package squidpony.examples;
 
 import squidpony.squidgrid.MimicFill;
 import squidpony.squidgrid.mapping.DungeonUtility;
-import squidpony.squidgrid.mapping.SerpentMapGenerator;
-import squidpony.squidmath.Coord;
 import squidpony.squidmath.StatefulRNG;
-
-import java.util.ArrayList;
 
 /**
  * Created by Tommy Ettinger on 5/14/2016.
@@ -17,10 +13,10 @@ public class MimicFillTest {
         //seed is, in base 36, the number SQUIDLIB
         StatefulRNG rng = new StatefulRNG(2252637788195L);
         boolean[][] result;
-
+        long time = System.currentTimeMillis();
         for(boolean[][] sample : MimicFill.samples)
         {
-            result = MimicFill.fill(sample, 32, 0.2, 3, rng);
+            result = MimicFill.fill(sample, 200, 0.2, 3, rng);
 
             DungeonUtility.debugPrint(DungeonUtility.hashesToLines(
                     DungeonUtility.wallWrap(MimicFill.sampleToMap(result, '.', '#')),
@@ -28,14 +24,15 @@ public class MimicFillTest {
             System.out.println();
             //rng.setState(2252637788195L);
         }
-
+        System.out.println(System.currentTimeMillis() - time);
+/*
         //char[][] dungeon = new DungeonBoneGen(rng).generate(TilesetType.DEFAULT_DUNGEON, 47, 47);
         SerpentMapGenerator serpent = new SerpentMapGenerator(35, 35, rng, 0.1);
         serpent.putWalledRoundRoomCarvers(3);
         serpent.putWalledBoxRoomCarvers(2);
         char[][] dungeon = serpent.generate();
         //dungeon = new DungeonBoneGen(rng).generate(TilesetType.DEFAULT_DUNGEON, 30, 30);
-        result = MimicFill.fill(MimicFill.mapToSample(dungeon, '.'), 32, 0.25, 5, rng);
+        result = MimicFill.fill(MimicFill.mapToSample(dungeon, '.'), 200, 0.25, 5, rng);
 
         DungeonUtility.debugPrint(DungeonUtility.hashesToLines(
                 DungeonUtility.wallWrap(MimicFill.sampleToMap(result, '.', '#')),
@@ -47,12 +44,12 @@ public class MimicFillTest {
 
         ArrayList<Coord> pts = SerpentMapGenerator.pointPath(60, 60, rng);
         result = MimicFill.markSample(
-                MimicFill.fill(MimicFill.mapToSample(dungeon, '.'), 60, 0.25, 5, rng),
+                MimicFill.fill(MimicFill.mapToSample(dungeon, '.'), 200, 0.25, 5, rng),
                 pts);
         DungeonUtility.debugPrint(DungeonUtility.hashesToLines(
                 DungeonUtility.wallWrap(MimicFill.sampleToMap(result, '.', '#')),
                 true));
-
+*/
 
         /*
         boolean[][] sample2 = new PacMazeGenerator(33, 33, rng).create();
