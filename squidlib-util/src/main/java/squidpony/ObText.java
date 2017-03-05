a5eb0e234102b13cf25ed8bb33fc18468c95af77
Tommy Ettinger <tommy.ettinger@gmail.com>
2017-03-04 16:28:55 -0800

    Significant improvements to ObText
    Docs are better, including a sample of the format. Raw strings allow
    delimiters using a different syntax. Actually using the objects is
    reasonable now because the way you get child elements is more robust
    (returning a new Iterator over the child elements only, instead of
    modifying the same iterator and dead-ending when the children run out).
    And it's pretty small and convenient for a format, too!

--- squidlib-util/src/main/java/squidpony/ObText.java
+++ squidlib-util/src/main/java/squidpony/ObText.java
@@ -2,6 +2,8 @@ package squidpony;
 
 import regexodus.Matcher;
 import regexodus.Pattern;
+import regexodus.REFlags;
+import squidpony.annotation.Beta;
 import squidpony.squidmath.IntVLA;
 
 import java.util.ArrayList;
@@ -12,24 +14,59 @@ import java.util.Iterator;
  * Supports only one type, String, but allows each String to have arbitrary nested levels of
  * String children as if in sub-lists. You can interpret the Strings however you want, and
  * quoting each String isn't necessary if they are just one word ("bare words" are allowed).
+ * The main way of using this is to get an ObText.ItemIterator value using {@link #iterator()},
+ * which acts as a normal Iterator over the top-level Strings (not children of anything), but
+ * to call its {@link ItemIterator#hasChild()} method when you expect potential child elements,
+ * then {@link ItemIterator#children()} to get another ItemIterator over the child elements if
+ * you want to explore deeper.
+ * <br>
+ * Format example:
+ * <pre>
+ * hello world
+ * 'how are you today?' [just great thanks]
+ * hooray!
+ *
+ * complexity?
+ * [it is possible [yes this is a good example]
+ * 'escapes like \[\'\] all work'
+ * ]
+ *
+ * '''
+ * raw strings (heredocs) look like this normally.
+ *     they permit characters without escapes, ]][][[ \/\/\ ,
+ *     except for triple quotes.
+ *     they keep newlines and indentation intact,
+ * except for up to one newline ignored adjacent to each triple quote.
+ * '''
+ *
+ * [[different[
+ * if you may need triple quotes
+ *     in the raw string, use a different syntax that allows delimiters.
+ * here, the delimiter is '''different''', just to be different.]different]]
+ * </pre>
  * <br>
  * Inspired strongly by STOB, http://stobml.org/ , but no code is shared and the format is
  * slightly different. The main differences are that ObText supports nested block comments
- * using the syntax /[delimiter/comment here/delimiter]/, and that it uses Python-like
- * "heredoc" syntax for raw strings surrounded by triple-apostrophes '''like this''' with
- * optional initial and final newlines in the raw string ignored. We use square brackets in
- * place of curly braces to mark children associated with a string.
+ * using the syntax {@code /[delimiter/contents/delimiter]/} where delimiter may be empty
+ * but must match on both sides, and contents is the body of the comment. ObText uses Python-
+ * like "heredoc" syntax for raw strings surrounded by triple-apostrophes '''like so''' with
+ * optional initial and final newlines in the raw string ignored. An alternate raw string
+ * syntax is present that allows delimiters, using the syntax
+ * {@code [[delimiter[contents]delimiter]]}, where again delimiter may be empty and contents
+ * is the body of the raw string. We use square brackets in place of STOB's curly braces to
+ * mark children associated with a string.
  */
+@Beta
 public class ObText implements Iterable<String>{
     public static final Pattern pattern = Pattern.compile(
-            "(?>'''(?:[\n\u000C\f\r\u0085\u2028\u2029]|\r\n)?({=s}[\\d\\D]*?)(?:[\n\u000C\f\r\u0085\u2028\u2029]|\r\n)?''')" +
-            "|(?>'''({=s}[\\d\\D]*?)''')" +
-            "|(?>({=q}[\"'])({=s}[\\d\\D]*?)(?<!\\\\){\\q})" +
-            "|({=comment}(?>//|^#!)(?>\\V*))" +
-            "|({=block}/\\[({=q}\\S*)/(?:[\\d\\D]*?)/{\\q}\\]/)" +
+            "(?>'''(?:[\n\u000C\f\r\u0085\u2028\u2029]|\r\n)?({=s}.*?)(?:[\n\u000C\f\r\u0085\u2028\u2029]|\r\n)?''')" +
+            "|(?>\\[\\[({=q}[^\\[\\]]*)\\[(?:[\n\u000C\f\r\u0085\u2028\u2029]|\r\n)?({=s}.*?)(?:[\n\u000C\f\r\u0085\u2028\u2029]|\r\n)?\\]{\\q}\\]\\])" +
+            "|(?>({=q}[\"'])({=s}.*?)(?<!\\\\){\\q})" +
+            "|(?>(?>//|^#!)(?>\\V*))" +
+            "|(?>/\\[({=q}\\S*)/(?:.*?)/{\\q}\\]/)" +
             "|({=s}[^\\s\\[\\]\"'#\\\\]+)" +
             "|({=o}\\[)" +
-            "|({=c}\\])"
+            "|({=c}\\])", REFlags.DOTALL | REFlags.MULTILINE | REFlags.UNICODE
 
     );
     public static final Matcher m = pattern.matcher();
@@ -80,8 +117,8 @@ public class ObText implements Iterable<String>{
     }
 
     /**
-     * A one-way iterator through this ObText's String items; note that this can be
-     * instructed to descend into child sequences with {@link #child()}, which should
+     * A one-way iterator through this ObText's String items. Note that this can get
+     * an iterator into a child sequence with {@link #children()}, which should
      * only be called after checking that a child exists with {@link #hasChild()}.
      * {@link #remove()} is not supported, but {@link #next()} and {@link #hasNext()}
      * are, of course.
@@ -93,8 +130,8 @@ public class ObText implements Iterable<String>{
         }
         ItemIterator(int i)
         {
-            index = i % neighbors.size;
-            current = index - 1;
+            current = i % neighbors.size;
+            index = current;
         }
         int index = 0, current = -1;
         /**
@@ -110,8 +147,8 @@ public class ObText implements Iterable<String>{
         }
         /**
          * Returns {@code true} if the ObText.Item has any child elements.
-         * (In other words, returns {@code true} if {@link #child} would
-         * return an element rather than throwing an exception.)
+         * (In other words, returns {@code true} if {@link #children} would
+         * return an ItemIterator rather than throwing an exception.)
          *
          * @return {@code true} if this has any children
          */
@@ -135,10 +172,9 @@ public class ObText implements Iterable<String>{
                 index = neighbors.get(0);
                 return strings.get(0);
             }
-            int i;
-            if(index >= neighbors.size || (i = neighbors.get(current)) <= 0)
+            if(index >= neighbors.size || neighbors.get(current) <= 0)
                 throw new java.util.NoSuchElementException("No more sibling items in ObText object");
-            index = i + (current = index);
+            index = neighbors.get(index) + (current = index);
             return strings.get(current);
         }
         /**
@@ -147,19 +183,16 @@ public class ObText implements Iterable<String>{
          * @return the first child of this item
          * @throws java.util.NoSuchElementException if the iteration has no children
          */
-        public String child() {
+        public ItemIterator children() {
             if(current < 0)
             {
                 if(neighbors.size <= 0 || neighbors.get(0) == 1)
-                    throw new java.util.NoSuchElementException("No more sibling items in ObText object");
-                current = 0;
-                index = 1;
-                return strings.get(1);
+                    throw new java.util.NoSuchElementException("No current children in ObText object");
+                return new ItemIterator(1);
             }
             if(current >= neighbors.size - 2 || neighbors.get(current) == 1)
-                throw new java.util.NoSuchElementException("No more sibling items in ObText object");
-            index = ++current + 1;
-            return strings.get(current);
+                throw new java.util.NoSuchElementException("No current children in ObText object");
+            return new ItemIterator(current+1);
         }
 
         @Override
--- squidlib-util/src/test/java/squidpony/examples/ObTextTest.java
+++ squidlib-util/src/test/java/squidpony/examples/ObTextTest.java
@@ -8,32 +8,37 @@ import squidpony.ObText;
 public class ObTextTest {
     public static void main(String[] args){
         ObText ot = new ObText("hello world\n" +
-                //"'how are you today?' [just \"great\" thanks]\n" +
-                "nice\n" +
+                "'how are you today?' [just great thanks]\n" +
+                "hooray!\n" +
                 "\n" +
                 "complexity?\n" +
                 "[it is possible [yes this is a good example]\n" +
-                " 'escapes like \\[\\'\\] all work'\n" +
+                "'escapes like \\[\\'\\] all work'\n" +
                 "]\n" +
                 "\n" +
-                "\n" +
                 "'''\n" +
-                "raw strings! ]][][[ \\/\\/\\\n" +
-                "keeping newlines intact!\n" +
-                "]][][[ \\/\\/\\'''\n" +
-                "\n" +
+                "raw strings (heredocs) look like this normally.\n" +
+                "    they permit characters without escapes, ]][][[ \\/\\/\\ ,\n" +
+                "    except for triple quotes.\n" +
+                "    they keep newlines and indentation intact,\n" +
+                "except for up to one newline ignored adjacent to each triple quote.\n" +
                 "'''\n" +
-                "represented internally as a string array containing all elements\n" +
-                "and an int (or unsigned int) array representing the advancement\n" +
-                "to get to the next non-child string or 0 for the final element\n" +
-                "in a section (either of children or top-level elements).\n" +
-                "'''\n");
-        ObText.ItemIterator ii = ot.iterator();
-        while (ii.hasNext())
+                "\n" +
+                "[[different[\n" +
+                "if you may need triple quotes\n" +
+                "    in the raw string, use a different syntax that allows delimiters.\n" +
+                "here, the delimiter is '''different''', just to be different.]different]]\n");
+        iterate(ot.iterator());
+    }
+    public static void iterate(ObText.ItemIterator it)
+    {
+        while (it.hasNext())
         {
-            System.out.println(ii.next());
-            if(ii.hasChild())
-                System.out.println(ii.child());
+            System.out.print('"');
+            System.out.print(it.next());
+            System.out.println("\",");
+            if(it.hasChild())
+                iterate(it.children());
         }
     }
 }
