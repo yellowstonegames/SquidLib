acbf74c4f16c4456fa1022099de7e7470d76758b
Tommy Ettinger <tommy.ettinger@gmail.com>
2017-03-19 13:52:50 -0700

    Try again!

--- squidlib-util/src/main/java/emu/java.lang/Float.java
+++ squidlib-util/src/main/java/emu/java.lang/Float.java
@@ -22,10 +22,29 @@ import com.google.gwt.typedarrays.shared.Float32Array;
 import com.google.gwt.typedarrays.shared.Int32Array;
 import com.google.gwt.typedarrays.shared.Int8Array;
 
+/**
+ * Wraps a primitive <code>float</code> as an object.
+ */
 public final class Float extends Number implements Comparable<Float> {
+    public static final float MAX_VALUE = 3.4028235e+38f;
+    public static final float MIN_VALUE = 1.4e-45f;
+    public static final int MAX_EXPONENT = 127;
+    public static final int MIN_EXPONENT = -126;
+    public static final float MIN_NORMAL = 1.1754943508222875E-38f;
+    public static final float NaN = 0f / 0f;
+    public static final float NEGATIVE_INFINITY = -1f / 0f;
+    public static final float POSITIVE_INFINITY = 1f / 0f;
+    public static final int SIZE = 32;
+    public static final int BYTES = SIZE / Byte.SIZE;
+    public static final Class<Float> TYPE = float.class;
+
     private static final Int8Array wba = Int8ArrayNative.create(4);
     private static final Int32Array wia = Int32ArrayNative.create(wba.buffer(), 0, 1);
     private static final Float32Array wfa = Float32ArrayNative.create(wba.buffer(), 0, 1);
+
+    public static int compare(float x, float y) {
+        return Double.compare(x, y);
+    }
 
     public static int floatToIntBits(float value) {
         wfa.set(0, value);
@@ -50,4 +69,126 @@ public final class Float extends Number implements Comparable<Float> {
         wia.set(0, bits);
         return wfa.get(0);
     }
+
+    public static boolean isFinite(float x) {
+        return Double.isFinite(x);
+    }
+
+    public static boolean isInfinite(float x) {
+        return Double.isInfinite(x);
+    }
+
+    public static boolean isNaN(float x) {
+        return Double.isNaN(x);
+    }
+
+    public static float max(float a, float b) {
+        return Math.max(a, b);
+    }
+
+    public static float min(float a, float b) {
+        return Math.min(a, b);
+    }
+
+    public static float parseFloat(String s) throws NumberFormatException {
+        double doubleValue = __parseAndValidateDouble(s);
+        if (doubleValue > Float.MAX_VALUE) {
+            return Float.POSITIVE_INFINITY;
+        } else if (doubleValue < -Float.MAX_VALUE) {
+            return Float.NEGATIVE_INFINITY;
+        }
+        return (float) doubleValue;
+    }
+
+    public static float sum(float a, float b) {
+        return a + b;
+    }
+
+    public static String toString(float b) {
+        return String.valueOf(b);
+    }
+
+    public static Float valueOf(float f) {
+        return new Float(f);
+    }
+
+    public static Float valueOf(String s) throws NumberFormatException {
+        return new Float(s);
+    }
+
+    private final transient float value;
+
+    public Float(double value) {
+        this.value = (float) value;
+    }
+
+    public Float(float value) {
+        this.value = value;
+    }
+
+    public Float(String s) {
+        value = parseFloat(s);
+    }
+
+    @Override
+    public byte byteValue() {
+        return (byte) value;
+    }
+
+    @Override
+    public int compareTo(Float b) {
+        return compare(value, b.value);
+    }
+
+    @Override
+    public double doubleValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return (o instanceof Float) && (((Float) o).value == value);
+    }
+
+    @Override
+    public float floatValue() {
+        return value;
+    }
+
+    /**
+     * Not as bad as it was before!
+     * @return hash value of this (not truncated; converted to int using a modified {@link #floatToRawIntBits(float)})
+     */
+    public int hashCode() {
+        return hashCode(value);
+    }
+
+    @Override
+    public int intValue() {
+        return (int) value;
+    }
+
+    public boolean isInfinite() {
+        return isInfinite(value);
+    }
+
+    public boolean isNaN() {
+        return isNaN(value);
+    }
+
+    @Override
+    public long longValue() {
+        return (long) value;
+    }
+
+    @Override
+    public short shortValue() {
+        return (short) value;
+    }
+
+    @Override
+    public String toString() {
+        return toString(value);
+    }
+
 }
