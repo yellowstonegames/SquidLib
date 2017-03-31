cadfc7d3ffff6abf4dd492a317404a6ac61828aa
Tommy Ettinger <tommy.ettinger@gmail.com>
2017-03-30 20:40:07 -0700

    Changes and some documentation for XkcdRNG
    Mostly, next() needed a small tweak to allow it to work for inputs of 1
    or 2 for bits. The other return values changed, and (snarky)
    documentation was added as well.

--- squidlib-util/src/main/java/squidpony/squidmath/XkcdRNG.java
+++ squidlib-util/src/main/java/squidpony/squidmath/XkcdRNG.java
@@ -3,73 +3,134 @@ package squidpony.squidmath;
 import java.io.Serializable;
 
 /**
- * This implements the random number function from the "Random Number" XKCD comic. https://xkcd.com/221/
- * See explanation here: https://www.explainxkcd.com/wiki/index.php/221:_Random_Number
+ * This implements the random number function from the "Random Number" XKCD comic, with minor compatibility changes.
+ * https://xkcd.com/221/ See explanation here: https://www.explainxkcd.com/wiki/index.php/221:_Random_Number
+ * The period of this generator is optimal for its state size. Its state size is 0 bits. Thus, its period is 1.
+ * <br>
+ * Perhaps this is a good time to point out that none of our RandomnessSource implementations are truly random, but some
+ * may be closer to random than others... or further from it.
+ * <br>
+ * Primarily useful for benchmarking, when you want to test the absolute minimum work a RandomnessSource can do as a
+ * control group in a larger benchmark.
  */
 public class XkcdRNG implements RandomnessSource, Serializable {
+    private static final long serialVersionUID = 4L;
+    /**
+     * Constructs an XkcdRNG with the default state (0 bits) and corresponding qualities of randomness (period of 1).
+     */
     public XkcdRNG() {
     }
 
+    /**
+     * This takes a seed for some reason, but doesn't use it. I mean, why should XkcdRNG have to concern itself with
+     * deterministic seeding using a user input, when it already has determinism down pat?
+     * @param seed whatever you want it to be
+     */
     public XkcdRNG(final long seed) {
     }
 
+    /**
+     * Gets a random value that fits in the given number of bits. It doesn't actually use the bits parameter, so
+     * technically you could request a negative bit count, or zero bits, and it will still give you "the loneliest
+     * number" every time.
+     * @param bits the number of bits to be returned; should usually be between 1 and 32 but I've stopped caring
+     * @return Ein, uno, un, whatever you want to call it
+     */
     @Override
     public int next(int bits) {
-        return 4;   // chosen by fair dice roll.
+        return 1;   // chosen by fair dice roll.
         // guaranteed to be random.
     }
 
+    /**
+     * Gets a random long. A, singular. The number returned will always be coprime to all of:
+     * <ul>
+     *     <li>the number of fingers the user has, regardless of user (note: this includes non-human users),</li>
+     *     <li>the number of Twitter followers the developer writing the code has,</li>
+     *     <li>the number of protons in this universe,</li>
+     *     <li>420,</li>
+     *     <li>the additive identity of the set of all integers,</li>
+     *     <li>the answer to the question of life, the universe, and everything,</li>
+     *     <li>and all integers in the set of all integers, why not.</li>
+     * </ul>
+     * @return the loneliest number, as a long
+     */
     @Override
     public long nextLong() {
-        return 4;   // chosen by fair dice roll.
+        return 1L;   // chosen by fair dice roll.
         // guaranteed to be random.
     }
 
+    /**
+     * State? What state? This returns itself as a copy because it can.
+     * @return this XkcdRNG
+     */
     @Override
     public RandomnessSource copy() {
-        return new XkcdRNG();
+        return this;
     }
 
+    /**
+     * Gets a random int. A, singular. The number returned will always be coprime to all of:
+     * <ul>
+     *     <li>the number of fingers the user has, regardless of user (note: this includes non-human users),</li>
+     *     <li>the number of Twitter followers the developer writing the code has,</li>
+     *     <li>the number of protons in this universe,</li>
+     *     <li>420,</li>
+     *     <li>the additive identity of the set of all integers,</li>
+     *     <li>the answer to the question of life, the universe, and everything,</li>
+     *     <li>and all integers in the set of all integers, why not.</li>
+     * </ul>
+     * @return the loneliest number, as an int
+     */
     public int nextInt() {
-        return 4;   // chosen by fair dice roll.
+        return 1;   // chosen by fair dice roll.
         // guaranteed to be random.
     }
 
     /**
-     * Inclusive lower, exclusive upper.
+     * Normally this would return a random number between lower and upper, but because actually generating a random
+     * number seems kinda like it would require the computer to do extra work, this just returns lower. That's easier.
      *
      * @param lower the lower bound, inclusive, can be positive or negative
-     * @param upper the upper bound, exclusive, should be positive, must be greater than lower + 4
-     * @return a random int at least equal to lower + 4 and less than upper
+     * @param upper the upper bound, exclusive, discarded, unused, we don't care what it wants to restrict us to
+     * @return lower, as-is
      */
     public int nextInt(final int lower, final int upper) {
-        if (upper - lower < 4) throw new IllegalArgumentException("Upper bound must be greater than lower bound + 4");
-        return lower + 4;
-    }
-
-    public double nextDouble() {
-        return 4d / 6d;   // chosen by fair dice roll.
-        // guaranteed to be random.
-    }
-
-    public double nextDouble(final double outer) {
-        return nextDouble() * outer;
+        return lower;
     }
 
     /**
-     * Gets a uniform random float in the range [0.0,1.0)
-     *
-     * @return a random float at least equal to 0.0 and less than 1.0
+     * Returns the probability of three independent, fair coin flips of the same coin all resulting in the same outcome.
+     * @return A quarter, as a double.
+     */
+    public double nextDouble() {
+        return 0x4p-4;   // chosen by fair "dice" roll.
+        // guaranteed to be random.
+    }
+
+    /**
+     * Returns outer times the probability of three independent, fair coin flips of the same coin all resulting in the
+     * same outcome.
+     * @return A quarter of outer, as a double.
+     */
+    public double nextDouble(final double outer) {
+        return 0x4p-4 * outer;
+    }
+
+    /**
+     * Returns the probability of three independent, fair coin flips of the same coin all resulting in the same outcome.
+     * @return A quarter, as a float.
      */
     public float nextFloat() {
-        return 4f / 6f;   // chosen by fair dice roll.
+        return 0x4p-4f;   // chosen by fair "dice" roll.
         // guaranteed to be random.
     }
 
     /**
-     * Gets a random value, true or false.
+     * Gets a boolean answer to this question, when I ask it of my dog: "Do you want to go for a WALK!?!?"
      *
-     * @return a random true or false value.
+     * @return a highly uncertain boolean value. I mean, cosmic rays could hit your computer as you run this.
      */
     public boolean nextBoolean() {
         return true;   // chosen by fair coin flip.
@@ -84,11 +145,7 @@ public class XkcdRNG implements RandomnessSource, Serializable {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        XkcdRNG xkcdRNG = (XkcdRNG) o;
-
-        return hashCode() == xkcdRNG.hashCode();
+        return  (o != null && getClass() == o.getClass());
     }
 
     @Override
