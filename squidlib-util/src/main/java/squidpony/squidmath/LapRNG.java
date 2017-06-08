142a4a80b1d0175eb84c51bfd4af0b7df3fbdd6a
Tommy Ettinger <tommy.ettinger@gmail.com>
2017-06-07 23:56:19 -0700

    Adjust ZapRNG to improve speed

--- squidlib-util/src/main/java/squidpony/squidmath/LapRNG.java
+++ squidlib-util/src/main/java/squidpony/squidmath/LapRNG.java
@@ -89,18 +89,19 @@ public class LapRNG implements RandomnessSource, Serializable {
      * @param seed2 any int, will not be used verbatim
      */
     public LapRNG(final int seed0, final int seed1, final int seed2) {
-        state0 = (seed0 * 0xBFL + seed1 * seed2 << 24) ^ 0x8329C6EB9E6AD3E3L;
+        state0 = (seed0 * 0xBFL + seed1 * 0x1FL * seed2 << 24) ^ 0x8329C6EB9E6AD3E3L;
         state1 = (seed1 * 0x8329C6EB9E6AD3E3L ^ seed2 - 0xC6BC279692B5C483L) + seed0 * 0x9E3779B97F4A7C15L;
     }
 
     /**
-     * This constructor gets a 64-bit hash code from the given String or other CharSequence and gives it to the
-     * constructor that takes one long, {@link #LapRNG(long)}.
+     * This constructor gets three differently-calculated 32-bit hash codes from the given String or other
+     * CharSequence and gives them to the constructor that takes three ints, {@link #LapRNG(int, int, int)}. You can
+     * pass a null seed and this will still work.
      * @param seed any CharSequence, such as a String
      */
     public LapRNG(final CharSequence seed)
     {
-        this(CrossHash.Wisp.hash64(seed));
+        this(CrossHash.Mist.alpha.hash(seed), CrossHash.Mist.beta.hash(seed), CrossHash.Mist.gamma.hash(seed));
     }
 
     private long state0, state1;
