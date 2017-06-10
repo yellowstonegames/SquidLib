361cdc7ddc9927c2b624850385554221bfa3209d
Tommy Ettinger <tommy.ettinger@gmail.com>
2017-06-10 16:25:03 -0700

    Add docs for IMarkup to close #153

--- squidlib/src/main/java/squidpony/squidgrid/gui/gdx/GDXMarkup.java
+++ squidlib/src/main/java/squidpony/squidgrid/gui/gdx/GDXMarkup.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.math.MathUtils;
 import squidpony.panel.IMarkup;
 
 /**
+ * GDXMarkup implements IMarkup for libGDX Color objects, and can start blocks of markup that libGDX understands that
+ * display text in a given Color. Typically, this class needs only to be instantiated and passed to a class that uses
+ * IMarkup of Color, and then anything else will be handled internally as Colors are given to the using class.
  * Created by Tommy Ettinger on 1/23/2016.
  */
 public class GDXMarkup implements IMarkup<Color>{
