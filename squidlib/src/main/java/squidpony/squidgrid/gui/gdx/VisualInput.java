1b522db71e39992ae0bfc80ff538ffc1e6a07b49
Tommy Ettinger <tommy.ettinger@gmail.com>
2017-07-23 21:22:47 -0700

    Give some long-overdue attention to VisualInput

--- squidlib/src/main/java/squidpony/squidgrid/gui/gdx/VisualInput.java
+++ squidlib/src/main/java/squidpony/squidgrid/gui/gdx/VisualInput.java
@@ -16,6 +16,16 @@ import java.util.TreeMap;
  * Acts like SquidInput but displays available keys as visible buttons if the device has no physical keyboard.
  * Only relevant if you want keyboard input to always be available, so this doesn't implement the SquidInput
  * constructors that don't handle any keys (the buttons this shows will act like keys even on Android and iOS).
+ * <br>
+ * This doesn't currently work well when used with a SquidPanel or SquidLayers that pans a camera over an area
+ * that is larger than one screen (you only need to worry about this in "standard" SquidLib usage if you use
+ * the {@link SquidPanel#SquidPanel(int, int, TextCellFactory, squidpony.IColorCenter, float, float, char[][])}) or
+ * {@link SquidLayers#SquidLayers(int, int, int, int, TextCellFactory, SquidColorCenter, SquidColorCenter, char[][])}
+ * constructors with a larger 2D char array than the given gridWidth and/or gridHeight). It works fine if the
+ * area that can be seen on one screen is as big as it gets, such as in the normal text-based mode of games like
+ * Rogue and Brogue. It can also work if you manually handle movement over a larger area without using libGDX
+ * Viewport and Camera code to accomplish that, since this class needs to handle the Viewport to some extent.
+ * <br>
  * Created by Tommy Ettinger on 4/15/2016.
  */
 public class VisualInput extends SquidInput {
@@ -32,14 +42,14 @@ public class VisualInput extends SquidInput {
     public boolean eightWay = true, forceButtons = false;
     public Stage stage;
     protected ShrinkPartViewport spv;
-    private void init()
+    private void ready(TextCellFactory font)
     {
         initialized = true;
         sectionWidth = Gdx.graphics.getWidth() / 8;
         sectionHeight = Gdx.graphics.getHeight();
 
-        tcfLeft = DefaultResources.getStretchableFont().width(sectionWidth / 4).height(sectionHeight / 16).initBySize();
-        tcfRight = DefaultResources.getStretchableFont().width(sectionWidth / 12).height(sectionHeight / 24).initBySize();
+        tcfLeft = font.copy().width(sectionWidth / 4).height(sectionHeight / 16).initBySize();
+        tcfRight = font.copy().width(sectionWidth / 12).height(sectionHeight / 24).initBySize();
 
         left = new SquidPanel(4, 16, tcfLeft);
         if(eightWay) {
@@ -156,14 +166,35 @@ public class VisualInput extends SquidInput {
      * This also initializes the displayed buttons if there is no hardware keyboard available. This uses the color and
      * eightWay fields to determine what color the buttons will be drawn with and if directions should be 8-way or
      * 4-way, respectively. These fields should be set before calling init() if you don't want the defaults, which are
-     * white buttons and 8-way directions.
+     * white buttons and 8-way directions. The font with this overload of init is set to Inconsolata-LGC-Custom, which
+     * is normally available via {@link DefaultResources#getStretchableFont()} and requires the .fnt file and .png file
+     * listed in the documentation for that method. If you don't have those files, but have some other font (preferably
+     * some kind of distance field font, since this resizes the font significantly), you can use
+     * {@link #init(TextCellFactory, String...)} or {@link #init(TextCellFactory, Map)} to have this use your font.
      * @param enabled an array or vararg of Strings that name actions; the first char of each String should be unique
      */
-    public void init(String... enabled)
+    public void init(String... enabled) {
+        init(DefaultResources.getStretchableFont(), enabled);
+    }
+    /**
+     * Convenience method that does essentially the same thing as init(Map&lt;Character, String&gt;).
+     * This assumes that each String passed to this is an action, and the first letter of a String is the character that
+     * a keypress would generate to perform the action named by the String. For example, calling this with
+     * {@code init("fire", "Fortify")} would register "fire" under 'f' and "Fortify" under 'F', displaying the Strings
+     * instead of the characters if possible. The first char of each String should be unique in the arguments.
+     * <br>
+     * This also initializes the displayed buttons if there is no hardware keyboard available. This uses the color and
+     * eightWay fields to determine what color the buttons will be drawn with and if directions should be 8-way or
+     * 4-way, respectively. These fields should be set before calling init() if you don't want the defaults, which are
+     * white buttons and 8-way directions.
+     * @param font the TextCellFactory to use for the buttons, usually a distance field font from DefaultResources
+     * @param enabled an array or vararg of Strings that name actions; the first char of each String should be unique
+     */
+    public void init(TextCellFactory font, String... enabled)
     {
         if(!forceButtons && Gdx.input.isPeripheralAvailable(Input.Peripheral.HardwareKeyboard))
             return;
-        init();
+        ready(font);
         availableKeys = new TreeMap<>();
         if(enabled == null)
             return;
@@ -179,16 +210,37 @@ public class VisualInput extends SquidInput {
      * available as buttons on the right side of the screen if on a device with no hardware keyboard. Arrows will be
      * provided on the left side of the screen for directions.
      * <br>
-     * This uses the color and eightWay fields to determine what color the buttons will be drawn with and if directions
-     * should be 8-way or 4-way, respectively. These fields should be set before calling init() if you don't want the
-     * defaults, which are white buttons and 8-way directions.
+     * This also initializes the displayed buttons if there is no hardware keyboard available. This uses the color and
+     * eightWay fields to determine what color the buttons will be drawn with and if directions should be 8-way or
+     * 4-way, respectively. These fields should be set before calling init() if you don't want the defaults, which are
+     * white buttons and 8-way directions. The font with this overload of init is set to Inconsolata-LGC-Custom, which
+     * is normally available via {@link DefaultResources#getStretchableFont()} and requires the .fnt file and .png file
+     * listed in the documentation for that method. If you don't have those files, but have some other font (preferably
+     * some kind of distance field font, since this resizes the font significantly), you can use
+     * {@link #init(TextCellFactory, String...)} or {@link #init(TextCellFactory, Map)} to have this use your font.
      * @param available a Map of Character keys representing keyboard keys and Strings for the actions they trigger
      */
     public void init(Map<Character, String> available)
     {
+        init(DefaultResources.getStretchableFont(), available);
+    }
+    /**
+     * For each char and String in available, registers each keyboard key (as a char, such as 'A' or
+     * SquidInput.LEFT_ARROW) with a String that names the action that key is used to perform, and makes these Strings
+     * available as buttons on the right side of the screen if on a device with no hardware keyboard. Arrows will be
+     * provided on the left side of the screen for directions.
+     * <br>
+     * This uses the color and eightWay fields to determine what color the buttons will be drawn with and if directions
+     * should be 8-way or 4-way, respectively. These fields should be set before calling init() if you don't want the
+     * defaults, which are white buttons and 8-way directions.
+     * @param font the TextCellFactory to use for the buttons, usually a distance field font from DefaultResources
+     * @param available a Map of Character keys representing keyboard keys and Strings for the actions they trigger
+     */
+    public void init(TextCellFactory font, Map<Character, String> available)
+    {
         if(!forceButtons && Gdx.input.isPeripheralAvailable(Input.Peripheral.HardwareKeyboard))
             return;
-        init();
+        ready(font);
         if(available != null) {
             availableKeys = new TreeMap<>(available);
             fillActions();
