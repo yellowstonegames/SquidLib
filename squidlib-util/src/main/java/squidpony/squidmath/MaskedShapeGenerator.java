67c6f756fd16c5837507ea599ce079e75d7ea7df
Tommy Ettinger <tommy.ettinger@gmail.com>
2017-10-12 18:36:01 -0700

    Add MaskedShapeGenerator, test.
    Preview looks like https://i.imgur.com/O4q1a2I.png . It is very fast and
    can generate approximately 50,000 ships per second on this laptop (that
    includes the overhead of displaying them). MaskedShapeGenerator makes
    heavy usage of GreasedRegion.

---
+++ squidlib-util/src/main/java/squidpony/squidmath/MaskedShapeGenerator.java
@@ -0,0 +1,162 @@
+package squidpony.squidmath;
+
+/**
+ * Generates procedural shapes based on a mask that determines what values can exist at a location.
+ * Currently, this generates spaceship-like shapes, which <a href="https://i.imgur.com/O4q1a2I.png">look like this</a>.
+ * The technique used here is derived from <a href="https://github.com/zfedoran/pixel-sprite-generator">this repo</a>,
+ * which is an adaptation of
+ * <a href="http://web.archive.org/web/20080228054410/http://www.davebollinger.com/works/pixelspaceships/"> Dave
+ * Bollinger's work</a>.
+ * Created by Tommy Ettinger on 10/12/2017.
+ */
+public class MaskedShapeGenerator {
+    public static final int[][] spaceship = {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0},
+            {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0},
+            {0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            {0, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 0},
+            {0, 1, 3, 3, 3, 3, 2, 2, 2, 3, 1, 0}
+    };
+    public static final GreasedRegion potentialBody = new GreasedRegion(spaceship, 1), // can be body or empty
+            potentialSolid = new GreasedRegion(spaceship, 2, 4), // can be solid or body, never empty
+            alwaysSolid = new GreasedRegion(spaceship, 3); // must be solid
+    public ThrustRNG randomness = new ThrustRNG(123456789L);
+    public RNG rng = new RNG(randomness);
+    public final GreasedRegion randomRegion = new GreasedRegion(randomness, 6, 12);
+    private final GreasedRegion workingBody = new GreasedRegion(6, 12),
+            workingSolid = new GreasedRegion(6, 12),
+            workingShade = new GreasedRegion(6, 12),
+            workingShine = new GreasedRegion(6, 12);
+
+    public MaskedShapeGenerator()
+    {
+    }
+
+    /**
+     * Returns a modified version of changing where 0 represents empty space, 1 represents border, and 2 represents
+     * "body." Only a 12x12 area will be changed by this call, with its minimum x and y determined by xPos and yPos.
+     * The seed will change each time this runs, producing different shapes each time.
+     * The technique used here is derived from https://github.com/zfedoran/pixel-sprite-generator .
+     * @param changing an int array that will be altered if possible
+     * @param xPos the minimum x to modify; the maximum will be xPos + 12, exclusive
+     * @param yPos the minimum y to modify; the maximum will be yPos + 12, exclusive
+     * @return changing, after modifications
+     */
+    public int[][] generateInto(int[][] changing, int xPos, int yPos)
+    {
+        int w = workingBody.width, h = workingBody.height, ys = workingBody.ySections;
+        if(changing.length < w * 2 || changing[0].length < h
+                || xPos + w * 2 >= changing.length || yPos + h >= changing[0].length)
+            return changing;
+        randomRegion.refill(rng, 0.78, w, h);
+        workingSolid.remake(potentialSolid);
+        workingBody.remake(potentialBody).or(potentialSolid).andNot(alwaysSolid).and(randomRegion);
+        workingSolid.andNot(workingBody).or(randomRegion.remake(workingBody).fringe());
+        for (int x = 0, o = w*2-1; x < w; x++, o--) {
+            for (int y = 0; y < h; y++) {
+                changing[xPos + x][yPos + y] = ((workingBody.data[x * ys + (y >> 6)] & (1L << (y & 63))) != 0 ? 2 : 0)
+                        | ((workingSolid.data[x * ys + (y >> 6)] & (1L << (y & 63))) != 0 ? 1 : 0);
+            }
+            System.arraycopy(changing[xPos + x], yPos, changing[xPos + o], yPos, h);
+        }
+        return changing;
+    }
+
+    /**
+     * Returns an int array (12x12) where 0 represents empty space, 1 represents border, and 2 represents "body."
+     * The seed will change each time this runs, producing different shapes each time.
+     * The technique used here is derived from https://github.com/zfedoran/pixel-sprite-generator .
+     * @return an int array with the randomly generated shape.
+     */
+    public int[][] generate()
+    {
+        return generateInto(new int[workingSolid.width][workingSolid.height], 0, 0);
+    }
+    /**
+     * Returns an int array (12x12) where 0 represents empty space, 1 represents border, and 2 represents "body."
+     * Will use the specified seed for this generation.
+     * The technique used here is derived from https://github.com/zfedoran/pixel-sprite-generator .
+     * @param seed a long to use as the seed for this random shape.
+     * @return an int array with the randomly generated shape.
+     */
+    public int[][] generate(long seed)
+    {
+        randomness.setState(seed);
+        return generateInto(new int[workingSolid.width][workingSolid.height], 0, 0);
+    }
+
+    /**
+     * Returns a modified version of changing where 0 represents empty space, 1 represents border, 2 represents shaded
+     * "body,", 3 represents normal body, and 4 represents lit body. Only a 12x12 area will be changed by this call,
+     * with its minimum x and y determined by xPos and yPos.
+     * The seed will change each time this runs, producing different shapes each time.
+     * The technique used here is derived from https://github.com/zfedoran/pixel-sprite-generator .
+     * @param changing an int array that will be altered if possible
+     * @param xPos the minimum x to modify; the maximum will be xPos + 12, exclusive
+     * @param yPos the minimum y to modify; the maximum will be yPos + 12, exclusive
+     * @return changing, after modifications
+     */
+    public int[][] generateIntoShaded(int[][] changing, int xPos, int yPos)
+    {
+        int w = workingBody.width, h = workingBody.height, ys = workingBody.ySections;
+        if(changing.length < w * 2 || changing[0].length < h
+                || xPos + w * 2 >= changing.length || yPos + h >= changing[0].length)
+            return changing;
+        randomRegion.refill(rng, 0.78, w, h);
+        workingSolid.remake(potentialSolid);
+        workingBody.remake(potentialBody).or(potentialSolid).andNot(alwaysSolid).and(randomRegion);
+        workingSolid.andNot(workingBody).or(randomRegion.remake(workingBody).fringe());
+        workingShade.remake(workingBody).neighborDown().not().and(workingBody);
+        workingShine.remake(workingBody).neighborUp().not().and(workingBody).andNot(workingShade);
+        workingBody.andNot(workingShade).andNot(workingShine);
+        for (int x = 0, o = w*2-1; x < w; x++, o--) {
+            for (int y = 0; y < h; y++) {
+                changing[xPos + x][yPos + y] = ((workingShine.data[x * ys + (y >> 6)] & (1L << (y & 63))) != 0 ? 4 : 0)
+                        | ((workingBody.data[x * ys + (y >> 6)] & (1L << (y & 63))) != 0 ? 3 : 0)
+                        | ((workingShade.data[x * ys + (y >> 6)] & (1L << (y & 63))) != 0 ? 2 : 0)
+                        | ((workingSolid.data[x * ys + (y >> 6)] & (1L << (y & 63))) != 0 ? 1 : 0);
+            }
+            System.arraycopy(changing[xPos + x], yPos, changing[xPos + o], yPos, h);
+        }
+        return changing;
+    }
+
+    /**
+     * Returns a modified version of changing where 0 represents empty space, 1 represents border, 2 represents shaded
+     * "body,", 3 represents normal body, and 4 represents lit body. Only a 12x12 area will be changed by this call,
+     * with its minimum x and y determined by xPos and yPos. Ensures that borders drawn around the shape cover all cells
+     * that are 8-way adjacent to any cells in the shape.
+     * The seed will change each time this runs, producing different shapes each time.
+     * The technique used here is derived from https://github.com/zfedoran/pixel-sprite-generator .
+     * @param changing an int array that will be altered if possible
+     * @param xPos the minimum x to modify; the maximum will be xPos + 12, exclusive
+     * @param yPos the minimum y to modify; the maximum will be yPos + 12, exclusive
+     * @return changing, after modifications
+     */
+    public int[][] generateIntoShaded8way(int[][] changing, int xPos, int yPos)
+    {
+        int w = workingBody.width, h = workingBody.height, ys = workingBody.ySections;
+        if(changing.length < w * 2 || changing[0].length < h
+                || xPos + w * 2 >= changing.length || yPos + h >= changing[0].length)
+            return changing;
+        randomRegion.refill(rng, 0.78, w, h);
+        workingSolid.remake(potentialSolid);
+        workingBody.remake(potentialBody).or(potentialSolid).andNot(alwaysSolid).and(randomRegion);
+        workingSolid.andNot(workingBody).or(randomRegion.remake(workingBody).fringe8way());
+        workingShade.remake(workingBody).neighborDown().not().and(workingBody);
+        workingShine.remake(workingBody).neighborUp().not().and(workingBody).andNot(workingShade);
+        workingBody.andNot(workingShade).andNot(workingShine);
+        for (int x = 0, o = w*2-1; x < w; x++, o--) {
+            for (int y = 0; y < h; y++) {
+                changing[xPos + x][yPos + y] = ((workingShine.data[x * ys + (y >> 6)] & (1L << (y & 63))) != 0 ? 4 : 0)
+                        | ((workingBody.data[x * ys + (y >> 6)] & (1L << (y & 63))) != 0 ? 3 : 0)
+                        | ((workingShade.data[x * ys + (y >> 6)] & (1L << (y & 63))) != 0 ? 2 : 0)
+                        | ((workingSolid.data[x * ys + (y >> 6)] & (1L << (y & 63))) != 0 ? 1 : 0);
+            }
+            System.arraycopy(changing[xPos + x], yPos, changing[xPos + o], yPos, h);
+        }
+        return changing;
+    }
+
+}
---
+++ squidlib/src/test/java/squidpony/gdx/tests/ShapeTest.java
@@ -0,0 +1,141 @@
+package squidpony.gdx.tests;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import squidpony.ArrayTools;
+import squidpony.squidgrid.gui.gdx.DefaultResources;
+import squidpony.squidgrid.gui.gdx.SColor;
+import squidpony.squidgrid.gui.gdx.SparseLayers;
+import squidpony.squidgrid.gui.gdx.SquidInput;
+import squidpony.squidmath.MaskedShapeGenerator;
+import squidpony.squidmath.ThrustRNG;
+
+/**
+ * Created by Tommy Ettinger on 10/12/2017.
+ */
+public class ShapeTest extends ApplicationAdapter {
+    private SpriteBatch batch;
+    private SparseLayers display;//, overlay;
+    private static final int width = 96, height = 96;
+    private static final int cellWidth = 6, cellHeight = 6;
+    private SquidInput input;
+    private Stage stage;
+    private Viewport view;
+    private MaskedShapeGenerator gen;
+    private int[][] data, colorChoices;
+    private int counter = 0;
+    @Override
+    public void create() {
+        gen = new MaskedShapeGenerator();
+        batch = new SpriteBatch();
+        data = new int[width][height];
+        colorChoices = new int[6][6];
+        display = new SparseLayers(width, height, cellWidth, cellHeight, DefaultResources.getStretchableHeavySquareFont());
+        view = new StretchViewport(width * cellWidth, height * cellHeight);
+        stage = new Stage(view, batch);
+        input = new SquidInput(new SquidInput.KeyHandler() {
+            @Override
+            public void handle(char key, boolean alt, boolean ctrl, boolean shift) {
+                switch (key)
+                {
+                    case 'Q':
+                    case 'q':
+                    case SquidInput.ESCAPE: {
+                        Gdx.app.exit();
+                    }
+                }
+            }
+        });
+        // ABSOLUTELY NEEDED TO HANDLE INPUT
+        Gdx.input.setInputProcessor(input);
+        // and then add display, our one visual component, to the list of things that act in Stage.
+        display.setPosition(0, 0);
+        //overlay.setPosition(0, 0);
+        //Stack stk = new Stack(display, overlay);
+        //stage.addActor(stk);
+        //stk.layout();
+        stage.addActor(display);
+        putMap();
+    }
+    public void putMap()
+    {
+        ArrayTools.fill(data, 0);
+        for (int y = 0; y < 6; y++) {
+            for (int x = 0; x < 6; x++) {
+                gen.generateIntoShaded8way(data, x * 16 + 2, y * 16 + 2);
+                colorChoices[x][y] = ((counter + x + 36 * y) * 13 % 21) * 9
+                        + ThrustRNG.determineBounded(counter + x + 36 * y, 3);
+            }
+        }
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                switch (data[x][y])
+                {
+                    case 0:
+                        display.backgrounds[x][y] = 0f;
+                        break;
+                    case 1:
+                        display.backgrounds[x][y] = SColor.FLOAT_BLACK;
+                        break;
+                    case 2:
+                        display.backgrounds[x][y] = SColor.COLOR_WHEEL_PALETTE[colorChoices[x >> 4][y >> 4] + 6].toFloatBits();
+                        break;
+                    case 3:
+                        display.backgrounds[x][y] = SColor.COLOR_WHEEL_PALETTE[colorChoices[x >> 4][y >> 4]].toFloatBits();
+                        break;
+                    case 4:
+                        display.backgrounds[x][y] = SColor.COLOR_WHEEL_PALETTE[colorChoices[x >> 4][y >> 4] + 3].toFloatBits();
+                        break;
+                }
+            }
+        }
+        //Gdx.graphics.setTitle("SquidLib Test: Procedural Spaceships at " + Gdx.graphics.getFramesPerSecond() + " FPS");
+    }
+    @Override
+    public void render() {
+        // standard clear the background routine for libGDX
+        Gdx.gl.glClearColor(0.75f, 0.75f, 0.75f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        counter = gen.randomness.next(26);
+        putMap();
+
+        // if we are waiting for the player's input and get input, process it.
+        if (input.hasNext()) {
+            input.next();
+        }
+        stage.act();
+        // stage has its own batch and must be explicitly told to draw().
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        view.update(width, height, true);
+        view.apply(true);
+        //display = new SquidPanel(this.width, this.height, cellWidth, cellHeight);
+        //Gdx.graphics.requestRendering();
+    }
+
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "SquidLib Test: Procedural Spaceships";
+        config.width = width * cellHeight;
+        config.height = height * cellHeight;
+        config.foregroundFPS = 1;
+        //config.vSyncEnabled = false;
+        config.addIcon("Tentacle-16.png", Files.FileType.Internal);
+        config.addIcon("Tentacle-32.png", Files.FileType.Internal);
+        config.addIcon("Tentacle-128.png", Files.FileType.Internal);
+        new LwjglApplication(new ShapeTest(), config);
+    }
+}
+
