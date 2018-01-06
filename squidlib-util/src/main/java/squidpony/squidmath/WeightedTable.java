e0dea62142f4376e47d1788de0a3aa4e1b3a31d1
Tommy Ettinger <tommy.ettinger@gmail.com>
2018-01-05 19:59:37 -0800

    Add WeightedTable (lighter ProbabilityTable)
    WeightedTable uses the more advanced Alias Method than the simpler
    methods used in ProbabilityTable, and intentionally doesn't store the
    items it would normally randomly return. (Almost everything we use for
    data structures allows access by index anyway, so you could store an
    ArrayList next to this and just get() the index this returns. This
    allows some extra flexibility, as well.)

---
+++ squidlib-util/src/main/java/squidpony/squidmath/WeightedTable.java
@@ -0,0 +1,133 @@
+package squidpony.squidmath;
+
+import java.io.Serializable;
+
+/**
+ * A different approach to the same task {@link ProbabilityTable} solves, though this only looks up an appropriate index
+ * instead of also storing items it can choose; allows positive doubles for weights but does not allow nested tables for
+ * simplicity. This doesn't store an RNG (or RandomnessSource) in this class, and instead expects a long to be given for
+ * each random draw from the table (these long parameters can be random, sequential, or in some other way different
+ * every time). Uses <a href="http://www.keithschwarz.com/darts-dice-coins/">Vose's Alias Method</a>, and is based
+ * fairly-closely on the code given by Keith Schwarz at that link. Because Vose's Alias Method is remarkably fast (O(1)
+ * generation time in use, and O(n) time to construct a WeightedTable instance), this may be useful to consider if you
+ * don't need all the features of ProbabilityTable or if you want deeper control over the random aspects of it.
+ * <br>
+ * Internally, this uses ThrustAltRNG's algorithm as found in {@link ThrustAltRNG#determineBounded(long, int)} and
+ * {@link ThrustAltRNG#determineDouble(long)} to generate both an int and mid-precision double (roughly 32 of the 52
+ * bits of the double's significand are random, more than a float has) from one randomized 64-bit value, allowing the
+ * state to be just one long.
+ * <br>
+ * Created by Tommy Ettinger on 1/5/2018.
+ */
+public class WeightedTable implements Serializable {
+    private static final long serialVersionUID = 100L;
+    protected final int[] alias;
+    protected final double[] probability;
+    public final int size;
+
+    /**
+     * Constructs a useless WeightedTable that always returns the index 0.
+     */
+    public WeightedTable()
+    {
+        this(1);
+    }
+
+    /**
+     * Constructs a WeightedTable with the given array of weights for each index. The array can also be a varargs for
+     * convenience. The weights can be any positive non-zero doubles, but should usually not be so large or small that
+     * precision loss is risked. Each weight will be used to determine the likelihood of that weight's index being
+     * returned by {@link #random(long)}.
+     * @param prob an array or varargs of positive doubles representing the weights for their own indices
+     */
+    public WeightedTable(double... prob) {
+        /* Begin by doing basic structural checks on the inputs. */
+        if (prob == null)
+            throw new NullPointerException("Array 'probabilities' given to WeightedTable cannot be null");
+        if ((size = prob.length) == 0)
+            throw new IllegalArgumentException("Array 'probabilities' given to WeightedTable must be nonempty.");
+
+        /* Allocate space for the probability and alias tables. */
+        probability = new double[size];
+        alias = new int[size];
+
+        /* Compute the average probability and cache it for later use. */
+        double sum = 0.0;
+
+        /* Make a copy of the probabilities list, since we will be making
+         * changes to it.
+         */
+        double[] probabilities = new double[size];
+        for (int i = 0; i < size; ++i) {
+            if(prob[i] <= 0) continue;
+            sum += (probabilities[i] = prob[i]);
+        }
+        if(sum <= 0)
+            throw new IllegalArgumentException("At least one probability must be positive");
+        final double average = sum / size;
+
+        /* Create two stacks to act as worklists as we populate the tables. */
+        IntVLA small = new IntVLA(size);
+        IntVLA large = new IntVLA(size);
+
+        /* Populate the stacks with the input probabilities. */
+        for (int i = 0; i < size; ++i) {
+            /* If the probability is below the average probability, then we add
+             * it to the small list; otherwise we add it to the large list.
+             */
+            if (probabilities[i] >= average)
+                large.add(i);
+            else
+                small.add(i);
+        }
+
+        /* As a note: in the mathematical specification of the algorithm, we
+         * will always exhaust the small list before the big list.  However,
+         * due to floating point inaccuracies, this is not necessarily true.
+         * Consequently, this inner loop (which tries to pair small and large
+         * elements) will have to check that both lists aren't empty.
+         */
+        while (!small.isEmpty() && !large.isEmpty()) {
+            /* Get the index of the small and the large probabilities. */
+            int less = small.pop();
+            int more = large.pop();
+
+            /* These probabilities have not yet been scaled up to be such that
+             * sum/n is given weight 1.0.  We do this here instead.
+             */
+            probability[less] = probabilities[less] / average;
+            alias[less] = more;
+
+            probabilities[more] += probabilities[less] - average;
+
+            if (probabilities[more] >= average)
+                large.add(more);
+            else
+                small.add(more);
+        }
+
+        while (!small.isEmpty())
+            probability[small.pop()] = 1.0;
+        while (!large.isEmpty())
+            probability[large.pop()] = 1.0;
+    }
+
+    /**
+     * Gets an index of one of the weights in this WeightedTable, with the choice determined deterministically by the
+     * given long, but higher weights will be returned by more possible inputs than lower weights. The state parameter
+     * can be from a random source, but this will randomize it again anyway, so it is also fine to just give sequential
+     * longs. The important thing is that each state input this is given will produce the same result for this
+     * WeightedTable every time, so you should give different state values when you want random-seeming results. You may
+     * want to call this like {@code weightedTable.random(++state)}, where state is a long, to ensure the inputs change.
+     * This will always return an int between 0 (inclusive) and {@link #size} (exclusive).
+     * @param state a long that should be different every time; consider calling with {@code ++state}
+     * @return a random-seeming index from 0 to {@link #size} - 1, determined by weights and the given state
+     */
+    public int random(long state)
+    {
+        state = ((state = ((state *= 0x6C8E9CF570932BD5L) ^ (state >>> 25)) * (state | 0xA529L)) ^ (state >>> 22));
+        int column = (int)((size * (state & 0xFFFFFFFFL)) >> 32);
+        return ((state >>> 32) * 0x1p-32 < probability[column]) ? column : alias[column];
+    }
+
+}
--- squidlib-util/src/test/java/squidpony/examples/ProbabilityTableTest.java
+++ squidlib-util/src/test/java/squidpony/examples/ProbabilityTableTest.java
@@ -1,13 +1,18 @@
 package squidpony.examples;
 
+import org.junit.Test;
 import squidpony.squidmath.OrderedMap;
 import squidpony.squidmath.ProbabilityTable;
+import squidpony.squidmath.WeightedTable;
+
+import static squidpony.examples.LanguageGenTest.PRINTING;
 
 /**
  * Created by Tommy Ettinger on 2/21/2017.
  */
 public class ProbabilityTableTest {
-    public static void main(String[] args) {
+    @Test
+    public void testTable() {
         ProbabilityTable<String> wood = new ProbabilityTable<>("wood"),
                 carpenter = new ProbabilityTable<>("carpenter");
         wood.add("splinter", 10).add("twig", 4).add("branch", 2).add("plank", 1).add("twig", 3);
@@ -23,26 +28,34 @@ public class ProbabilityTableTest {
             for (int i = 0; i < 20; i++) {
                 current = wood.random();
                 woodCounts.put(current, woodCounts.get(current) + 1);
-                System.out.print(current);
-                System.out.print(' ');
+                if (PRINTING) {
+                    System.out.print(current);
+                    System.out.print(' ');
+                }
             }
-            System.out.println();
+            if (PRINTING) System.out.println();
         }
-        System.out.println();
-        System.out.println(woodCounts);
-        System.out.println('\n');
+        if (PRINTING) {
+            System.out.println();
+            System.out.println(woodCounts);
+            System.out.println('\n');
+        }
         for (int l = 0; l < 10; l++) {
             for (int i = 0; i < 20; i++) {
                 current = carpenter.random();
                 carpenterCounts.put(current, carpenterCounts.get(current) + 1);
-                System.out.print(current);
-                System.out.print(' ');
+                if (PRINTING) {
+                    System.out.print(current);
+                    System.out.print(' ');
+                }
             }
-            System.out.println();
+            if (PRINTING) System.out.println();
         }
-        System.out.println();
-        System.out.println(carpenterCounts);
-        System.out.println('\n');
+        if (PRINTING) {
+            System.out.println();
+            System.out.println(carpenterCounts);
+            System.out.println('\n');
+        }
         carpenterCounts = new OrderedMap<>(
                 new String[]{"splinter", "twig", "branch", "plank", "table", "shelf", "chair"},
                 new Integer[]{0, 0, 0, 0, 0, 0, 0});
@@ -52,12 +65,48 @@ public class ProbabilityTableTest {
             for (int i = 0; i < 20; i++) {
                 current = carpenter.random();
                 carpenterCounts.put(current, carpenterCounts.get(current) + 1);
-                System.out.print(current);
-                System.out.print(' ');
+                if (PRINTING) {
+                    System.out.print(current);
+                    System.out.print(' ');
+                }
             }
-            System.out.println();
+            if (PRINTING) System.out.println();
         }
-        System.out.println();
-        System.out.println(carpenterCounts);
+        if (PRINTING) {
+            System.out.println();
+            System.out.println(carpenterCounts);
+        }
+    }
+    @Test
+    public void testWeighted()
+    {
+        double[] weights = {5.777, 4.666, 2.444, 3.222};
+        double sum = weights[0] + weights[1] + weights[2] + weights[3];
+        WeightedTable wood = new WeightedTable(weights);
+        String[] woodStrings = {"splinter", "twig", "plank", "branch"};
+
+        OrderedMap<String, Integer> woodCounts = new OrderedMap<>(
+                new String[]{"splinter", "twig", "plank", "branch"}, new Integer[]{0, 0, 0, 0});
+        String current;
+        long state = (long) (System.nanoTime() / (Math.random() * Math.random() + 0.01));
+        for (int l = 0; l < 100; l++) {
+            for (int i = 0; i < 28; i++) {
+                current = woodStrings[wood.random(++state)];
+                woodCounts.put(current, woodCounts.get(current) + 1);
+                if (PRINTING) {
+                    System.out.print(current);
+                    System.out.print(' ');
+                }
+            }
+            if (PRINTING) System.out.println();
+        }
+        if (PRINTING) {
+            System.out.println();
+            for (int i = 0; i < 4; i++) {
+                System.out.println("There should be about " + (2800 * weights[i] / sum) + " " + woodStrings[i] +
+                        " and there are " + woodCounts.get(woodStrings[i]));
+            }
+            System.out.println('\n');
+        }
     }
 }
