c529df9c592d1819dcdc8f83fa82f02fe3eb5239
Tommy Ettinger <tommy.ettinger@gmail.com>
2018-10-16 19:21:43 -0700

    Improve HastyPointHash' "Golden" algo
    Some of the multipliers were even but shouldn't have been; these have been adjusted to nearby odd numbers, and the quality should further improve as a result.  There's also some small optimizations for the HastyPointHash methods that get a small number of bits.

--- squidlib-util/src/main/java/squidpony/squidmath/Noise.java
+++ squidlib-util/src/main/java/squidpony/squidmath/Noise.java
@@ -935,7 +935,7 @@ public class Noise {
             z += s * 0xDB4F0B9175AE2165L;
             y += z * 0xBBE0563303A4615FL;
             x += y * 0xA0F2EC75A1FE1575L;
-            s += x * 0x89E182857D9ED688L;
+            s += x * 0x89E182857D9ED689L;
             return ((s = (s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25);
         }
 
@@ -962,7 +962,7 @@ public class Noise {
             w += s * 0xE19B01AA9D42C633L;
             z += w * 0xC6D1D6C8ED0C9631L;
             y += z * 0xAF36D01EF7518DBBL;
-            x += y * 0x9A69443F36F710E6L;
+            x += y * 0x9A69443F36F710E7L;
             s += x * 0x881403B9339BD42DL;
             return ((s = (s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25);
         }
@@ -990,11 +990,11 @@ public class Noise {
          */
         public static long hashAll(long x, long y, long z, long w, long u, long v, long s) {
             v += s * 0xE95E1DD17D35800DL;
-            u += v * 0xD4BC74E13F3C782EL;
-            w += u * 0xC1EDBC5B5C68AC24L;
-            z += w * 0xB0C8AC50F0EDEF5CL;
+            u += v * 0xD4BC74E13F3C782FL;
+            w += u * 0xC1EDBC5B5C68AC25L;
+            z += w * 0xB0C8AC50F0EDEF5DL;
             y += z * 0xA127A31C56D1CDB5L;
-            x += y * 0x92E852C80D153DB2L;
+            x += y * 0x92E852C80D153DB3L;
             s += x * 0x85EB75C3024385C3L;
             return ((s = (s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25);
         }
@@ -1019,7 +1019,7 @@ public class Noise {
             y += s * 0xD1B54A32D192ED03L;
             x += y * 0xABC98388FB8FAC03L;
             s += x * 0x8CB92BA72F3D8DD7L;
-            return (int)(((s = (s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25) >>> 56);
+            return (int)(((s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) >>> 56);
         }
 
         /**
@@ -1044,8 +1044,8 @@ public class Noise {
             z += s * 0xDB4F0B9175AE2165L;
             y += z * 0xBBE0563303A4615FL;
             x += y * 0xA0F2EC75A1FE1575L;
-            s += x * 0x89E182857D9ED688L;
-            return (int)(((s = (s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25) >>> 56);
+            s += x * 0x89E182857D9ED689L;
+            return (int)(((s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) >>> 56);
         }
 
         /**
@@ -1071,9 +1071,9 @@ public class Noise {
             w += s * 0xE19B01AA9D42C633L;
             z += w * 0xC6D1D6C8ED0C9631L;
             y += z * 0xAF36D01EF7518DBBL;
-            x += y * 0x9A69443F36F710E6L;
+            x += y * 0x9A69443F36F710E7L;
             s += x * 0x881403B9339BD42DL;
-            return (int)(((s = (s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25) >>> 56);
+            return (int)(((s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) >>> 56);
         }
 
         /**
@@ -1099,13 +1099,13 @@ public class Noise {
          */
         public static int hash256(long x, long y, long z, long w, long u, long v, long s) {
             v += s * 0xE95E1DD17D35800DL;
-            u += v * 0xD4BC74E13F3C782EL;
-            w += u * 0xC1EDBC5B5C68AC24L;
-            z += w * 0xB0C8AC50F0EDEF5CL;
+            u += v * 0xD4BC74E13F3C782FL;
+            w += u * 0xC1EDBC5B5C68AC25L;
+            z += w * 0xB0C8AC50F0EDEF5DL;
             y += z * 0xA127A31C56D1CDB5L;
-            x += y * 0x92E852C80D153DB2L;
+            x += y * 0x92E852C80D153DB3L;
             s += x * 0x85EB75C3024385C3L;
-            return (int)(((s = (s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25) >>> 56);
+            return (int)(((s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) >>> 56);
         }
         /**
          * Gets a 5-bit point hash of a 2D point (x and y are both longs) and a state/seed as a long. This point
@@ -1128,7 +1128,7 @@ public class Noise {
             y += s * 0xD1B54A32D192ED03L;
             x += y * 0xABC98388FB8FAC03L;
             s += x * 0x8CB92BA72F3D8DD7L;
-            return (int)(((s = (s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25) >>> 59);
+            return (int)(((s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) >>> 59);
         }
 
         /**
@@ -1153,8 +1153,8 @@ public class Noise {
             z += s * 0xDB4F0B9175AE2165L;
             y += z * 0xBBE0563303A4615FL;
             x += y * 0xA0F2EC75A1FE1575L;
-            s += x * 0x89E182857D9ED688L;
-            return (int)(((s = (s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25) >>> 59);
+            s += x * 0x89E182857D9ED689L;
+            return (int)(((s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) >>> 59);
         }
 
         /**
@@ -1180,9 +1180,9 @@ public class Noise {
             w += s * 0xE19B01AA9D42C633L;
             z += w * 0xC6D1D6C8ED0C9631L;
             y += z * 0xAF36D01EF7518DBBL;
-            x += y * 0x9A69443F36F710E6L;
+            x += y * 0x9A69443F36F710E7L;
             s += x * 0x881403B9339BD42DL;
-            return (int)(((s = (s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25) >>> 59);
+            return (int)(((s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) >>> 59);
         }
 
         /**
@@ -1208,13 +1208,13 @@ public class Noise {
          */
         public static int hash32(long x, long y, long z, long w, long u, long v, long s) {
             v += s * 0xE95E1DD17D35800DL;
-            u += v * 0xD4BC74E13F3C782EL;
-            w += u * 0xC1EDBC5B5C68AC24L;
-            z += w * 0xB0C8AC50F0EDEF5CL;
+            u += v * 0xD4BC74E13F3C782FL;
+            w += u * 0xC1EDBC5B5C68AC25L;
+            z += w * 0xB0C8AC50F0EDEF5DL;
             y += z * 0xA127A31C56D1CDB5L;
-            x += y * 0x92E852C80D153DB2L;
+            x += y * 0x92E852C80D153DB3L;
             s += x * 0x85EB75C3024385C3L;
-            return (int)(((s = (s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25) >>> 59);
+            return (int)(((s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) >>> 59);
         }
         
         /**
@@ -1238,7 +1238,7 @@ public class Noise {
             y += s * 0xD1B54A32D192ED03L;
             x += y * 0xABC98388FB8FAC03L;
             s += x * 0x8CB92BA72F3D8DD7L;
-            return (int)(((s = (s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25) >>> 58);
+            return (int)(((s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) >>> 58);
         }
 
         /**
@@ -1263,8 +1263,8 @@ public class Noise {
             z += s * 0xDB4F0B9175AE2165L;
             y += z * 0xBBE0563303A4615FL;
             x += y * 0xA0F2EC75A1FE1575L;
-            s += x * 0x89E182857D9ED688L;
-            return (int)(((s = (s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25) >>> 58);
+            s += x * 0x89E182857D9ED689L;
+            return (int)(((s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) >>> 58);
         }
 
         /**
@@ -1290,9 +1290,9 @@ public class Noise {
             w += s * 0xE19B01AA9D42C633L;
             z += w * 0xC6D1D6C8ED0C9631L;
             y += z * 0xAF36D01EF7518DBBL;
-            x += y * 0x9A69443F36F710E6L;
+            x += y * 0x9A69443F36F710E7L;
             s += x * 0x881403B9339BD42DL;
-            return (int)(((s = (s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25) >>> 58);
+            return (int)(((s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) >>> 58);
         }
 
         /**
@@ -1318,13 +1318,13 @@ public class Noise {
          */
         public static int hash64(long x, long y, long z, long w, long u, long v, long s) {
             v += s * 0xE95E1DD17D35800DL;
-            u += v * 0xD4BC74E13F3C782EL;
-            w += u * 0xC1EDBC5B5C68AC24L;
-            z += w * 0xB0C8AC50F0EDEF5CL;
+            u += v * 0xD4BC74E13F3C782FL;
+            w += u * 0xC1EDBC5B5C68AC25L;
+            z += w * 0xB0C8AC50F0EDEF5DL;
             y += z * 0xA127A31C56D1CDB5L;
-            x += y * 0x92E852C80D153DB2L;
+            x += y * 0x92E852C80D153DB3L;
             s += x * 0x85EB75C3024385C3L;
-            return (int)(((s = (s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25) >>> 58);
+            return (int)(((s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) >>> 58);
         }
 
         public static int hash256_alt(final long x, final long y, final long z, final long w, final long seed) {
