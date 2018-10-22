75a0e94ee7ebe1b97fa6d4acf73ff19292c67065
Tommy Ettinger <tommy.ettinger@gmail.com>
2018-10-21 23:13:52 -0700

    WorldBenchmark shows we should try using FastNoise in world gen

---
+++ squidlib-performance/src/main/java/squidpony/performance/WorldBenchmark.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2014, Oracle America, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Oracle nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package squidpony.performance;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+import squidpony.performance.alternate.mapping.WorldMapGenerator;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Benchmarks for WorldMapGenerator.
+ * <br>
+ * HyperellipticalMap using ClassicNoise and using FastNoise, run with Java 11, on October 21, 2018:
+ * <pre>
+ * Benchmark                                Mode  Cnt   Score   Error  Units
+ * WorldBenchmark.measureHyperelliptical    avgt    5  43.047 ± 0.789  ms/op
+ * WorldBenchmark.measureHyperellipticalFN  avgt    5  35.127 ± 0.314  ms/op
+ * </pre>
+ */
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Fork(1)
+@Warmup(iterations = 5, time = 5, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
+public class WorldBenchmark {
+    private WorldMapGenerator.HyperellipticalMap hem = new WorldMapGenerator.HyperellipticalMap();
+    
+    @Benchmark
+    public void measureHyperelliptical(Blackhole blackhole)
+    {
+        hem.generate();
+        blackhole.consume(hem);
+    }
+    
+    
+    private WorldMapGenerator.HyperellipticalFNMap fnm = new WorldMapGenerator.HyperellipticalFNMap();
+
+    @Benchmark
+    public void measureHyperellipticalFN(Blackhole blackhole)
+    {
+        fnm.generate();
+        blackhole.consume(fnm);
+    }
+
+    /*
+     * ============================== HOW TO RUN THIS TEST: ====================================
+     *
+     * You are expected to see the different run modes for the same benchmark.
+     * Note the units are different, scores are consistent with each other.
+     *
+     * You can run this test:
+     *
+     * a) Via the command line from the squidlib-performance module's root folder:
+     *    $ mvn clean install
+     *    $ java -jar target/benchmarks.jar NoiseBenchmark -wi 4 -i 4 -f 1
+     *
+     *    (we requested 5 warmup/measurement iterations, single fork)
+     *
+     * b) Via the Java API:
+     *    (see the JMH homepage for possible caveats when running from IDE:
+     *      http://openjdk.java.net/projects/code-tools/jmh/)
+     */
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(WorldBenchmark.class.getSimpleName())
+                .timeout(TimeValue.seconds(30))
+                .warmupIterations(5)
+                .measurementIterations(5)
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+}
