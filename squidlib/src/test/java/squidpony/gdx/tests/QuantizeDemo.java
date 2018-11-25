9b7a50b1d1df0f22f7880d2cfa767ff57084095e
Tommy Ettinger <tommy.ettinger@gmail.com>
2018-11-24 21:02:48 -0800

    Fix in CoordPacker.posToHilbert3D(). QuantizeDemo shows different colors.
    QuantizeDemo can also be used to test the RINSED palette, which is the default in the WarpWriter project for voxel models. WarpWriter depends on SquidLib, and it has also been a test-bed for some 3D operations that later were brought back into SquidLib. DawnBringer's Aurora palette seems much better-distributed across the colors that can be represented on a monitor, but Rinsed can handle visually-relevant colors, like subtle gradients of skin color to show shading, very well. Both do well with dithering, especially noise-based dithering.
    
    NOTICE.txt has also been updated.

--- squidlib/src/test/java/squidpony/gdx/tests/QuantizeDemo.java
+++ squidlib/src/test/java/squidpony/gdx/tests/QuantizeDemo.java
@@ -14,6 +14,7 @@ import squidpony.squidgrid.gui.gdx.PNG8;
 import squidpony.squidgrid.gui.gdx.PaletteReducer;
 import squidpony.squidgrid.gui.gdx.SColor;
 import squidpony.squidgrid.gui.gdx.SquidInput;
+import squidpony.squidmath.CoordPacker;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -43,6 +44,57 @@ public class QuantizeDemo extends ApplicationAdapter {
     private PNG8 png8;
     private PixmapIO.PNG png;
     private PaletteReducer auroraPalette, monaPalette, bivaPalette, colorsPalette;
+    /**
+     * This palette was given along with the Unseven palette
+     * <a href="https://www.deviantart.com/foguinhos/art/Unseven-Full-541514728">in this set of swatches</a>, but it's
+     * unclear if Unseven made it, or if this palette was published in some other medium. It's a nice palette, with 8
+     * levels of lightness ramp for 30 ramps with different hues. It seems meant for pixel art that includes human
+     * characters, and doesn't lack for skin tones like Unseven does. It has a generally good selection of light brown
+     * colors, and has been adjusted to add some dark brown colors, as well as vividly saturated purple. Many ramps also
+     * become more purple as they go into darker shades.
+     * <br>
+     * This is organized so the colors from index 1 to index 232 inclusive are sorted by hue, from red to orange to
+     * yellow to green to blue to purple, while still being organized in blocks of 8 colors at a time from bright to
+     * dark. Some almost-grayscale blocks are jumbled in the middle, but they do have a hue and it is always at the
+     * point where they are in the sort. A block of colors that are practically true grayscale are at indices 1-8,
+     * inclusive.
+     * <br>
+     * This can be passed in to {@link PaletteReducer#PaletteReducer(int[])} to quantize to this palette.
+     */
+    public static final int[] RINSED = {
+            0x00000000, 
+            0xF8F9FAFF, 0xC4C3C5FF, 0x9C9C9DFF, 0x757676FF, 0x616262FF, 0x4C484AFF, 0x252626FF, 0x090304FF,
+            0xD89789FF, 0xC4877AFF, 0xB47B76FF, 0xA36C72FF, 0x905861FF, 0x76454CFF, 0x5F3234FF, 0x452327FF,
+            0xF9DCB8FF, 0xCEB29AFF, 0xB29891FF, 0x8F797FFF, 0x75636FFF, 0x554B67FF, 0x3E3552FF, 0x272340FF,
+            0xEAA18DFF, 0xCF9180FF, 0xB87C6BFF, 0xA06A60FF, 0x905C59FF, 0x73474BFF, 0x52383EFF, 0x35242AFF,
+            0xBEAE97FF, 0xB0968AFF, 0x89756EFF, 0x6E5A54FF, 0x4F413CFF, 0x413534FF, 0x2F2525FF, 0x1C1415FF,
+            0xEED8A1FF, 0xE7B38CFF, 0xCC967FFF, 0xB6776DFF, 0x995A55FF, 0x803D49FF, 0x662139FF, 0x500328FF,
+            0xFDFE9CFF, 0xFDD7AAFF, 0xE9BBA4FF, 0xC9A09DFF, 0xB7889AFF, 0x957088FF, 0x755B7BFF, 0x514265FF,
+            0xFDF067FF, 0xFDBF60FF, 0xEF995AFF, 0xCC7148FF, 0xB65549FF, 0xA34547FF, 0x7D303FFF, 0x61242FFF,
+            0xDDBBA4FF, 0xC0A68FFF, 0x9F8871FF, 0x7F6B5CFF, 0x6B5755FF, 0x5D464CFF, 0x482F3DFF, 0x30232DFF,
+            0xFEF5E1FF, 0xE9DFD3FF, 0xCFC5BAFF, 0xBAAFABFF, 0xAAA291FF, 0x9A877BFF, 0x816F69FF, 0x615D56FF,
+            0xFEF1A8FF, 0xE4CE85FF, 0xC9AD77FF, 0xB19169FF, 0x957859FF, 0x7B604CFF, 0x60463BFF, 0x472F2AFF,
+            0xFEFC74FF, 0xE8D861FF, 0xCDAD53FF, 0xB2893EFF, 0x91672FFF, 0x7D4F21FF, 0x693C12FF, 0x562810FF,
+            0xFDFCB7FF, 0xFCFA3CFF, 0xFAD725FF, 0xF5B325FF, 0xD7853CFF, 0xB25345FF, 0x8A2B2BFF, 0x67160AFF,
+            0xCBD350FF, 0xB3B24BFF, 0x9A9E3AFF, 0x808B30FF, 0x647717FF, 0x4B6309FF, 0x305413FF, 0x272A07FF,
+            0x8DC655FF, 0x7BA838FF, 0x6C8A37FF, 0x5D733AFF, 0x4F633CFF, 0x3F5244FF, 0x323D4AFF, 0x232A45FF,
+            0xADD54BFF, 0x80B040FF, 0x599135FF, 0x35761AFF, 0x2A621FFF, 0x1E5220FF, 0x063824FF, 0x012B1DFF,
+            0xE8FFEFFF, 0xA9DDC0FF, 0x95C89CFF, 0x91B48EFF, 0x759983FF, 0x627F72FF, 0x4C655CFF, 0x36514AFF,
+            0x91E49DFF, 0x69C085FF, 0x4F8F62FF, 0x4A7855FF, 0x396044FF, 0x385240FF, 0x31413DFF, 0x233631FF,
+            0x09EFD0FF, 0x07CCA2FF, 0x03AA83FF, 0x038D75FF, 0x04726DFF, 0x01585AFF, 0x05454EFF, 0x083142FF,
+            0x97D6F9FF, 0x3EB0CAFF, 0x3C919FFF, 0x0A737CFF, 0x226171FF, 0x0B505FFF, 0x0D3948FF, 0x052935FF,
+            0x91FCFCFF, 0x68DBFEFF, 0x5CB1D5FF, 0x4C8CAAFF, 0x406883FF, 0x2B4965FF, 0x29324DFF, 0x1C1E34FF,
+            0x80D1FBFF, 0x62B2E7FF, 0x4D96DBFF, 0x267DB9FF, 0x195F97FF, 0x114776FF, 0x0B355AFF, 0x031D41FF,
+            0xCEEEFDFF, 0xCDD7FEFF, 0xA1AED7FF, 0x898CAEFF, 0x7C7196FF, 0x5E597CFF, 0x404163FF, 0x26294CFF,
+            0x8391C1FF, 0x7181CAFF, 0x5E71BEFF, 0x555FA2FF, 0x424C84FF, 0x323B6DFF, 0x2B325CFF, 0x292349FF,
+            0xE3D1FDFF, 0xBAABFAFF, 0x9F94E2FF, 0x9588D7FF, 0x7B71B3FF, 0x675E9CFF, 0x4F4D7CFF, 0x333158FF,
+            0xA570FFFF, 0x9462FFFF, 0x814EFFFF, 0x6C39FCFF, 0x582DC1FF, 0x472195FF, 0x412160FF, 0x2E1F38FF,
+            0xF7C1E7FF, 0xD791C6FF, 0xBB6FAAFF, 0xAF6190FF, 0x924B76FF, 0x623155FF, 0x47253FFF, 0x2F0E25FF,
+            0xFDC7FBFF, 0xFC9FC5FF, 0xFB71A9FF, 0xE6497EFF, 0xC33C6BFF, 0x933255FF, 0x68243FFF, 0x3F122AFF,
+            0xFDDDDCFF, 0xD1ABB1FF, 0xB48C9AFF, 0x9D7482FF, 0x8B5D6EFF, 0x705057FF, 0x583C4BFF, 0x421E29FF,
+            0xFCD9FBFF, 0xFDB8C7FF, 0xFD97AAFF, 0xF46E7EFF, 0xC65365FF, 0x9E303CFF, 0x741B28FF, 0x50071AFF,
+    };
+
     @Override
     public void create() {
         batch = new SpriteBatch();
@@ -55,14 +107,21 @@ public class QuantizeDemo extends ApplicationAdapter {
 //        SColor[] full = FULL_PALETTE;
         int idx = 0;
         //int len = full.length;
-        for (int x = 0; x < 256; x++) {
-            for (int y = 0; y < 256; y++) {
-                colors.drawPixel(x, y, idx=(y+256>>>1)<<16|(x+255>>>1)<<8|0x880000FF);
-//                colors.drawPixel(x+256, y, idx|0xAA0000FF);
-//                colors.drawPixel(x, y+256, idx|0xCC0000FF);
-//                colors.drawPixel(x+256, y+256, idx|0xEE0000FF);
-            }
+        CoordPacker.init();
+        for (int h = 0; h < 512; h++) {
+            colors.setColor((CoordPacker.hilbert3X[h] * 73 << 23 & 0xFF000000)
+                    | (CoordPacker.hilbert3Y[h] * 73 << 15 & 0xFF0000)
+                    | (CoordPacker.hilbert3Z[h] * 73 << 7) | 0xFF);
+            colors.fillRectangle(CoordPacker.hilbertX[h] << 5, CoordPacker.hilbertY[h] << 4, 32, 16);
         }
+//        for (int x = 0; x < 256; x++) {
+//            for (int y = 0; y < 256; y++) {
+//                colors.drawPixel(x, y, idx=(y+256>>>1)<<16|(x+255>>>1)<<8|0x880000FF);
+////                colors.drawPixel(x+256, y, idx|0xAA0000FF);
+////                colors.drawPixel(x, y+256, idx|0xCC0000FF);
+////                colors.drawPixel(x+256, y+256, idx|0xEE0000FF);
+//            }
+//        }
         edit = new Pixmap(width, height, Pixmap.Format.RGBA8888);
         edit.setBlending(Pixmap.Blending.None);
         edit.setFilter(Pixmap.Filter.NearestNeighbour);
@@ -306,11 +365,11 @@ public class QuantizeDemo extends ApplicationAdapter {
                 pixels.rewind();
                 break;
             case 18:
-                Gdx.graphics.setTitle("(Original) SColor Full Palette");
+                Gdx.graphics.setTitle("(Original) R6G6B6 Palette");
                 edit.drawPixmap(colors, 0, 0);
                 break;
             case 19:
-                Gdx.graphics.setTitle("(64-value channels) SColor Full Palette");
+                Gdx.graphics.setTitle("(64-value channels) R6G6B6 Palette");
                 edit.drawPixmap(colors, 0, 0);
                 while (pixels.remaining() >= 4)
                 {
@@ -321,37 +380,37 @@ public class QuantizeDemo extends ApplicationAdapter {
                 pixels.rewind();
                 break;
             case 20:
-                Gdx.graphics.setTitle("(Adaptive Palette, Sierra Lite dither) SColor Full Palette");
+                Gdx.graphics.setTitle("(Adaptive Palette, Sierra Lite dither) R6G6B6 Palette");
                 edit.drawPixmap(colors, 0, 0);
                 colorsPalette.reduce(edit);
                 break;
             case 21:
-                Gdx.graphics.setTitle("(Adaptive Palette, Burkes dither) SColor Full Palette");
+                Gdx.graphics.setTitle("(Adaptive Palette, Burkes dither) R6G6B6 Palette");
                 edit.drawPixmap(colors, 0, 0);
                 colorsPalette.reduceBurkes(edit);
                 break;
             case 22:
-                Gdx.graphics.setTitle("(Adaptive Palette, Noise-Based dither) SColor Full Palette");
+                Gdx.graphics.setTitle("(Adaptive Palette, Noise-Based dither) R6G6B6 Palette");
                 edit.drawPixmap(colors, 0, 0);
                 colorsPalette.reduceWithNoise(edit);
                 break;
             case 23:
-                Gdx.graphics.setTitle("(DawnBringer Aurora, Sierra Lite dither)  SColor Full Palette");
+                Gdx.graphics.setTitle("(DawnBringer Aurora, Sierra Lite dither)  R6G6B6 Palette");
                 edit.drawPixmap(colors, 0, 0);
                 auroraPalette.reduce(edit);
                 break;
             case 24:
-                Gdx.graphics.setTitle("(DawnBringer Aurora, Burkes dither) SColor Full Palette");
+                Gdx.graphics.setTitle("(DawnBringer Aurora, Burkes dither) R6G6B6 Palette");
                 edit.drawPixmap(colors, 0, 0);
                 auroraPalette.reduce(edit);
                 break;
             case 25:
-                Gdx.graphics.setTitle("(DawnBringer Aurora, Noise-Based dither)  SColor Full Palette");
+                Gdx.graphics.setTitle("(DawnBringer Aurora, Noise-Based dither)  R6G6B6 Palette");
                 edit.drawPixmap(colors, 0, 0);
                 auroraPalette.reduceWithNoise(edit);
                 break;
             case 26:
-                Gdx.graphics.setTitle("(6-value channels) SColor Full Palette");
+                Gdx.graphics.setTitle("(6-value channels) R6G6B6 Palette");
                 edit.drawPixmap(colors, 0, 0);
                 switch (edit.getFormat()) {
                     case RGBA8888: {
