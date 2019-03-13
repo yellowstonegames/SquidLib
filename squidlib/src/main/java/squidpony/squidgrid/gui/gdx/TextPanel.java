870938779eefc451477ca0e1987f27587a6fa178
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-03-12 22:07:23 -0700

    Optimize bloccus image. Start updating TextPanel (removing key input).
    My goal with TextPanel is to make it more usable in a game without constantly re-creating the object, and I'm not totally sure how it is going to work. The key input did not ever work for me, did not cooperate with custom keybindings, and couldn't be changed from j and k (which are much more often used for movement). I'll check how this works in Epigon.

--- squidlib/src/main/java/squidpony/squidgrid/gui/gdx/TextPanel.java
+++ squidlib/src/main/java/squidpony/squidgrid/gui/gdx/TextPanel.java
@@ -10,14 +10,11 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.utils.Align;
 import squidpony.panel.IColoredString;
 import squidpony.panel.IMarkup;
 import squidpony.squidgrid.gui.gdx.UIUtil.CornerStyle;
-import squidpony.squidgrid.gui.gdx.UIUtil.YMoveKind;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -58,9 +55,7 @@ import java.util.List;
  * </p>
  * 
  * <p>
- * In addition to what {@link ScrollPane} does (knobs, handling of the wheel);
- * this class plugs scrolling with arrow keys (up, down, page up, page down) and
- * vim shortcuts (j/k).
+ * This class shares what {@link ScrollPane} does (knobs, handling of the wheel).
  * </p>
  * <p>
  * Drawing the result of {@link #getScrollPane()} will set the shader of {@code batch} if using a distance field or MSDF
@@ -92,20 +87,13 @@ public class TextPanel<T extends Color> {
 	public float borderSize;
 
 	public CornerStyle borderStyle = CornerStyle.ROUNDED;
-
-	/**
-	 * Whether to use 'j' to scroll down, and 'k' to scroll up. Serious
-	 * roguelikes leave that to {@code true}, assuming they don't use j and k for movement...
-	 */
-	public boolean vimShortcuts = true;
-
+	
 	protected /* @Nullable */ IMarkup<T> markup;
 
 	protected BitmapFont font;
-    protected boolean distanceField;
     protected TextCellFactory tcf;
 	/** The text to display */
-	protected List<IColoredString<T>> text;
+	public ArrayList<IColoredString<T>> text;
 
     protected StringBuilder builder;
 
@@ -140,52 +128,6 @@ public class TextPanel<T extends Color> {
         textActor = new TextActor();
 
         this.scrollPane = new ScrollPane(textActor);
-
-        this.scrollPane.addListener(new InputListener() {
-            @Override
-            public boolean keyDown(InputEvent event, int keycode) {
-				/* To receive key up */
-                return true;
-            }
-
-            @Override
-            public boolean keyUp(InputEvent event, int keycode) {
-                final YMoveKind d = UIUtil.YMoveKind.of(keycode, vimShortcuts);
-                if (d == null)
-                    return false;
-                else {
-                    switch (d) {
-                        case DOWN:
-                        case UP: {
-                            handleArrow(!d.isDown());
-                            return true;
-                        }
-                        case PAGE_DOWN:
-                        case PAGE_UP:
-                            final float scrollY = scrollPane.getScrollY();
-                            final int mult = d.isDown() ? 1 : -1;
-                            scrollPane.setScrollY(scrollY + mult * textActor.getHeight());
-                            return true;
-                    }
-                    throw new IllegalStateException(
-                            "Unmatched YMoveKind: " + d);
-                }
-            }
-
-            @Override
-            public boolean keyTyped(InputEvent event, char character) {
-                if (vimShortcuts && (character == 'j' || character == 'k'))
-                    return true;
-                else
-                    return super.keyTyped(event, character);
-            }
-
-            private void handleArrow(boolean up) {
-                final float scrollY = scrollPane.getScrollY();
-                final int mult = up ? -1 : 1;
-                scrollPane.setScrollY(scrollY + (scrollPane.getHeight() * 0.8f * mult));
-            }
-        });
     }
 
     /**
@@ -204,7 +146,6 @@ public class TextPanel<T extends Color> {
         if (distanceFieldFont != null)
         {
             tcf = distanceFieldFont;
-            distanceField = distanceFieldFont.distanceField || distanceFieldFont.msdf;
             tcf.initBySize();
             font = tcf.font();
             if (markup != null)
@@ -213,52 +154,6 @@ public class TextPanel<T extends Color> {
         builder = new StringBuilder(512);
         textActor = new TextActor();
         scrollPane = new ScrollPane(textActor);
-
-        this.scrollPane.addListener(new InputListener() {
-            @Override
-            public boolean keyDown(InputEvent event, int keycode) {
-				/* To receive key up */
-                return true;
-            }
-
-            @Override
-            public boolean keyUp(InputEvent event, int keycode) {
-                final YMoveKind d = UIUtil.YMoveKind.of(keycode, vimShortcuts);
-                if (d == null)
-                    return false;
-                else {
-                    switch (d) {
-                        case DOWN:
-                        case UP: {
-                            handleArrow(!d.isDown());
-                            return true;
-                        }
-                        case PAGE_DOWN:
-                        case PAGE_UP:
-                            final float scrollY = scrollPane.getScrollY();
-                            final int mult = d.isDown() ? 1 : -1;
-                            scrollPane.setScrollY(scrollY + mult * textActor.getHeight());
-                            return true;
-                    }
-                    throw new IllegalStateException(
-							"Unmatched YMoveKind: " + d);
-                }
-            }
-
-            @Override
-            public boolean keyTyped(InputEvent event, char character) {
-                if (vimShortcuts && (character == 'j' || character == 'k'))
-                    return true;
-                else
-                    return super.keyTyped(event, character);
-            }
-
-            private void handleArrow(boolean up) {
-                final float scrollY = scrollPane.getScrollY();
-                final int mult = up ? -1 : 1;
-                scrollPane.setScrollY(scrollY + (scrollPane.getHeight() * 0.8f * mult));
-            }
-        });
     }
 
     /**
@@ -290,7 +185,7 @@ public class TextPanel<T extends Color> {
 	/**
 	 * This method sets the sizes of {@link #scrollPane} and {@link #textActor}.
 	 * This method MUST be called before rendering.
-	 * 
+	 *
 	 * @param maxHeight
 	 *            The maximum height that the scrollpane can take (equal or
 	 *            smaller than the height of the text actor).
@@ -299,30 +194,65 @@ public class TextPanel<T extends Color> {
 	 * @param text
 	 */
 	public void init(float width, float maxHeight, Collection<? extends IColoredString<T>> text) {
-        this.text = new ArrayList<IColoredString<T>>(text);
+		this.text = new ArrayList<IColoredString<T>>(text);
 
-        scrollPane.setWidth(width);
-        textActor.setWidth(width);
+		scrollPane.setWidth(width);
+		textActor.setWidth(width);
 
-        if (tcf == null)
-            throw new NullPointerException(
-                    "The font should be set before calling TextPanel.init()");
+		if (tcf == null)
+			throw new NullPointerException(
+					"The font should be set before calling TextPanel.init()");
 
-        final BitmapFontCache cache = font.getCache();
-        final List<String> toDisplay = getTypesetText();
-        float totalTextHeight = tcf.height();
-        GlyphLayout layout = cache.addText(builder, 0, 0, 0, builder.length(), width, Align.left, true);
-        totalTextHeight += layout.height;
-        if(totalTextHeight < 0)
-            totalTextHeight = 0;
+		typesetText();
+		float totalTextHeight = tcf.height();
+		GlyphLayout layout = font.getCache().addText(builder, 0, 0, 0, builder.length(), width, Align.left, true);
+		totalTextHeight += layout.height;
+		if(totalTextHeight < 0)
+			totalTextHeight = 0;
 		textActor.setHeight(/* Entire height */ totalTextHeight);
 		final boolean yscroll = maxHeight < totalTextHeight;
 		scrollPane.setHeight(/* Maybe not the entire height */ Math.min(totalTextHeight, maxHeight));
-        scrollPane.setActor(new TextActor());
+		scrollPane.setActor(new TextActor());
 		yScrollingCallback(yscroll);
 	}
 
-    public void init(float width, float maxHeight, T color, String... text)
+	/**
+	 * This method sets the sizes of {@link #scrollPane} and {@link #textActor}.
+	 * This method MUST be called before rendering.
+	 *
+	 * @param maxHeight
+	 *            The maximum height that the scrollpane can take (equal or
+	 *            smaller than the height of the text actor).
+	 * @param width
+	 *            The width of the scrollpane and the text actor.
+	 * @param text an ArrayList of IColoredString that will be used directly by this TextPanel (changes
+	 *             to the ArrayList will show up in the TextPanel)
+	 */
+	public void initShared(float width, float maxHeight, ArrayList<IColoredString<T>> text) {
+		this.text = text;
+
+		scrollPane.setWidth(width);
+		textActor.setWidth(width);
+
+		if (tcf == null)
+			throw new NullPointerException(
+					"The font should be set before calling TextPanel.init()");
+
+		final BitmapFontCache cache = font.getCache();
+		typesetText();
+		float totalTextHeight = tcf.height();
+		GlyphLayout layout = cache.addText(builder, 0, 0, 0, builder.length(), width, Align.left, true);
+		totalTextHeight += layout.height;
+		if(totalTextHeight < 0)
+			totalTextHeight = 0;
+		textActor.setHeight(/* Entire height */ totalTextHeight);
+		final boolean yscroll = maxHeight < totalTextHeight;
+		scrollPane.setHeight(/* Maybe not the entire height */ Math.min(totalTextHeight, maxHeight));
+		scrollPane.setActor(new TextActor());
+		yScrollingCallback(yscroll);
+	}
+
+	public void init(float width, float maxHeight, T color, String... text)
     {
         ArrayList<IColoredString.Impl<T>> coll = new ArrayList<>(text.length);
         for(String t : text)
@@ -366,19 +296,34 @@ public class TextPanel<T extends Color> {
 	public /* @Nullable */ List<String> getTypesetText() {
 		if (text == null)
 			return null;
-        builder.delete(0, builder.length());
+		builder.delete(0, builder.length());
 		final List<String> result = new ArrayList<>();
 		for (IColoredString<T> line : text) {
 			/* This code must be consistent with #draw in the custom Actor */
 			final IColoredString<T> tmp = present(line);
-            final String marked = applyMarkup(tmp);
+			final String marked = applyMarkup(tmp);
 			result.add(marked);
-            builder.append(marked);
-            builder.append('\n');
+			builder.append(marked);
+			builder.append('\n');
 		}
-        if(builder.length() > 0)
-            builder.deleteCharAt(builder.length() - 1);
+		if(builder.length() > 0)
+			builder.deleteCharAt(builder.length() - 1);
 		return result;
+	}
+	
+	public void typesetText() {
+		if (text == null)
+			return;
+		builder.setLength(0);
+		for (IColoredString<T> line : text) {
+			/* This code must be consistent with #draw in the custom Actor */
+			final IColoredString<T> tmp = present(line);
+			final String marked = applyMarkup(tmp);
+			builder.append(marked);
+			builder.append('\n');
+		}
+		if(builder.length() > 0)
+			builder.deleteCharAt(builder.length() - 1);
 	}
 
 	/**
@@ -470,9 +415,7 @@ public class TextPanel<T extends Color> {
             final float ty = 0;//getY();
             final float twidth = getWidth();
             final float theight = getHeight();
-
-            final float height = scrollPane.getHeight();
-
+            
             if (backgroundColor != null) {
                 batch.setColor(backgroundColor);
                 batch.draw(tcf.getSolid(), tx, ty, twidth, theight);
@@ -504,9 +447,9 @@ public class TextPanel<T extends Color> {
             }
             float yscroll = scrollPane.getScrollY();
 
-            final float destx = tx, offY = (tcf != null) ? tcf.height * 0.5f : 0;
-            getTypesetText();
-            font.draw(batch, builder, destx, theight + yscroll - offY,
+            final float offY = (tcf != null) ? tcf.height * 0.5f : 0;
+            typesetText();
+            font.draw(batch, builder, tx, theight + yscroll - offY,
                     0, builder.length(), twidth, Align.left, true);
 
         }
