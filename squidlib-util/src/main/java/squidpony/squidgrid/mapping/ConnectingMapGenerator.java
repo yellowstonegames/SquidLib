ec4cc5f75391d846e3f42b0ced2147bff87f71b5
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-05-07 22:14:00 -0700

    Add a bare skeleton of ConnectingMapGenerator while I try to figure it out
    This is meant to help with #207 , but it isn't close to being done yet.

---
+++ squidlib-util/src/main/java/squidpony/squidgrid/mapping/ConnectingMapGenerator.java
@@ -0,0 +1,80 @@
+package squidpony.squidgrid.mapping;
+
+import squidpony.ArrayTools;
+
+/**
+ * A dungeon-or-other-map generator that can start with a pre-placed {@code char[][]} and connect any features in that
+ * map by placing rooms and corridors where there is blank space ({@code ' '}) in the original {@code char[][]}.
+ * <br>
+ * Created by Tommy Ettinger on 5/7/2019.
+ */
+public class ConnectingMapGenerator implements IDungeonGenerator {
+    
+    public int width;
+    public int height;
+    public char[][] dungeon;
+    public int[][] environment;
+    
+    public ConnectingMapGenerator()
+    {
+        this(80, 80);
+    }
+    public ConnectingMapGenerator(int width, int height)
+    {
+        this.width = Math.max(1, width);
+        this.height = Math.max(1, height);
+        dungeon = ArrayTools.fill(' ', this.width, this.height);
+        environment = new int[this.width][this.height];
+    }
+    public ConnectingMapGenerator(char[][] prePlaced)
+    {
+        dungeon = ArrayTools.copy(prePlaced);
+        width = dungeon.length;
+        height = dungeon[0].length;
+        environment = new int[this.width][this.height];
+    }
+    /**
+     * Generates a dungeon or other map as a 2D char array. Any implementation may allow its own configuration and
+     * customization of how dungeons are generated, but each must provide this as a sane default. Most implementations
+     * should use the convention of '#' representing a wall and '.' representing a bare floor, but beyond that, anything
+     * could be present in the char array.
+     *
+     * @return a 2D char array representing some kind of map, probably using standard conventions for walls/floors
+     */
+    @Override
+    public char[][] generate() {
+        return dungeon;
+    }
+
+    /**
+     * Gets the most recently-produced dungeon as a 2D char array, usually produced by calling {@link #generate()} or
+     * some similar method present in a specific implementation. This normally passes a direct reference and not a copy,
+     * so you can normally modify the returned array to propagate changes back into this IDungeonGenerator.
+     *
+     * @return the most recently-produced dungeon/map as a 2D char array
+     */
+    @Override
+    public char[][] getDungeon() {
+        return dungeon;
+    }
+    /**
+     * Gets a 2D array of int constants, each representing a type of environment corresponding to a static field of
+     * MixedGenerator. This array will have the same size as the last char 2D array produced by generate(); the value
+     * of this method if called before generate() is undefined, but probably will be a 2D array of all 0 (UNTOUCHED).
+     * <ul>
+     * <li>MixedGenerator.UNTOUCHED, equal to 0, is used for any cells that aren't near a floor.</li>
+     * <li>MixedGenerator.ROOM_FLOOR, equal to 1, is used for floor cells inside wide room areas.</li>
+     * <li>MixedGenerator.ROOM_WALL, equal to 2, is used for wall cells around wide room areas.</li>
+     * <li>MixedGenerator.CAVE_FLOOR, equal to 3, is used for floor cells inside rough cave areas.</li>
+     * <li>MixedGenerator.CAVE_WALL, equal to 4, is used for wall cells around rough cave areas.</li>
+     * <li>MixedGenerator.CORRIDOR_FLOOR, equal to 5, is used for floor cells inside narrow corridor areas.</li>
+     * <li>MixedGenerator.CORRIDOR_WALL, equal to 6, is used for wall cells around narrow corridor areas.</li>
+     * </ul>
+     *
+     * @return a 2D int array where each element is an environment type constant in MixedGenerator
+     */
+    public int[][] getEnvironment() {
+        return environment;
+    }
+
+}
