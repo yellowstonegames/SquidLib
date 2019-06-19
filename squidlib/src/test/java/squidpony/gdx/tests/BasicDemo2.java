d024a0a0af6046fdf662cf311dc975e2c1c822fd
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-06-18 22:48:56 -0700

    Fix monospacing for 7-12-serif
    I might make these giant Strings of what chars the font covers available via a variable...

--- squidlib/src/test/java/squidpony/gdx/tests/BasicDemo2.java
+++ squidlib/src/test/java/squidpony/gdx/tests/BasicDemo2.java
@@ -71,9 +71,9 @@ public class BasicDemo2 extends ApplicationAdapter {
     /** In number of cells */
     public static final int bonusHeight = 7;
     /** The initial pixel width of a cell */
-    public static final int cellWidth = 14;//10;
+    public static final int cellWidth = 7;//10;
     /** The initial pixel height of a cell */
-    public static final int cellHeight = 27;//17;
+    public static final int cellHeight = 12;//17;
 
     private SquidInput input;
     private Color bgColor;
@@ -111,9 +111,10 @@ public class BasicDemo2 extends ApplicationAdapter {
         // the distance field effect allows the font to be stretched without getting blurry or grainy too easily.
         // this font is covered under the SIL Open Font License (fully free), so there's no reason it can't be used.
         display = new SquidLayers(gridWidth, gridHeight + bonusHeight, cellWidth, cellHeight,
-                new TextCellFactory().font(DefaultResources.getOctagonalMonoFont())
+                new TextCellFactory().font(DefaultResources.getSevenTwelveFont())
                 //DefaultResources.getCrispDejaVuFont()
         );//.setSmoothingMultiplier(1.625f));
+        
         // a bit of a hack to increase the text height slightly without changing the size of the cells they're in.
         // this causes a tiny bit of overlap between cells, which gets rid of an annoying gap between vertical lines.
         // if you use '#' for walls instead of box drawing chars, you don't need this.
