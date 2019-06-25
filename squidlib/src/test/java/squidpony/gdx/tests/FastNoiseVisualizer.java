6ae8a42a02bdb1b7014081b118c642ee0cb4f7c0
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-06-24 17:38:16 -0700

    Nice improvements to FastNoiseVisualizer; it is faster now.

--- squidlib/src/test/java/squidpony/gdx/tests/FastNoiseVisualizer.java
+++ squidlib/src/test/java/squidpony/gdx/tests/FastNoiseVisualizer.java
@@ -3,22 +3,17 @@ package squidpony.gdx.tests;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Files;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ImmediateModeRenderer20;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import squidpony.ArrayTools;
-import squidpony.squidgrid.gui.gdx.SquidInput;
-import squidpony.squidgrid.gui.gdx.TextCellFactory;
 import squidpony.squidmath.FastNoise;
-import squidpony.squidmath.NumberTools;
 
-import static squidpony.squidgrid.gui.gdx.SColor.FLOAT_WHITE;
-import static squidpony.squidgrid.gui.gdx.SColor.floatGet;
-import static squidpony.squidmath.NumberTools.swayRandomized;
-import static squidpony.squidmath.NumberTools.swayTight;
+import static com.badlogic.gdx.Input.Keys.*;
+import static com.badlogic.gdx.graphics.GL20.GL_POINTS;
 
 /**
  */
@@ -31,49 +26,15 @@ public class FastNoiseVisualizer extends ApplicationAdapter {
     private int octaves = 3;
     private float freq = 0.125f;
     private boolean inverse = false;
-    private SpriteBatch batch;
-    //private SparseLayers display;//, overlay;
+    private ImmediateModeRenderer20 renderer;
     
-    private TextCellFactory tcf;
     private static final int width = 512, height = 512;
-    private static final float[][] back = new float[width][height];
 
-    private SquidInput input;
+    private InputAdapter input;
     
     private Viewport view;
     private int ctr = -256;
     private boolean keepGoing = true;
-
-    private double total = 0.0;
-
-    public static float prepare(double n)
-    {
-        //return (float)n * 0.5f + 0.5f;
-        return swayTight((float)n * 1.5f + 0.5f);
-    }
-
-    public static float prepare(float n)
-    {
-        //return (n * 0.5f) + 0.5f;
-        return swayTight(n * 1.5f + 0.5f);
-    }
-
-    public static float prepare(double n, float multiplier)
-    {
-        //return (float)n * 0.5f + 0.5f;
-        return swayTight((float)n * multiplier + 0.5f);
-    }
-
-    public static float prepare(float n, float multiplier)
-    {
-        //return (n * 0.5f) + 0.5f;
-        return swayTight(n * multiplier + 0.5f);
-    }
-
-    public static float basicPrepare(double n)
-    {
-        return (float)n * 0.5f + 0.5f;
-    }
 
     public static float basicPrepare(float n)
     {
@@ -82,111 +43,98 @@ public class FastNoiseVisualizer extends ApplicationAdapter {
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        tcf = new TextCellFactory().includedFont().width(1).height(1).initBySize();
+        renderer = new ImmediateModeRenderer20(width * height, false, true, 0);
         view = new ScreenViewport();
-        ArrayTools.fill(back, FLOAT_WHITE);
 
-        input = new SquidInput(new SquidInput.KeyHandler() {
+        input = new InputAdapter(){
             @Override
-            public void handle(char key, boolean alt, boolean ctrl, boolean shift) {
-                switch (key) {
-                    case '-':                         
+            public boolean keyDown(int keycode) {
+                switch (keycode) {
+                    case MINUS:
                         mode = (mode + MODE_LIMIT - 1) % MODE_LIMIT;
                         break;
-                    case 'u':
-                    case 'U':
-                    case SquidInput.ENTER:                         
-                        if (key == SquidInput.ENTER) {
+                    case U:
+                    case ENTER:
+                        if (keycode == ENTER) {
                             mode++;
                             mode %= MODE_LIMIT;
                             ctr = -256;
                         }
                         putMap();
                         break;
-                    case 'C':
-                    case 'c':
+                    case P: //pause
+                        keepGoing = !keepGoing;
+                    case C:
                         ctr++;
-                        if(alt) keepGoing = !keepGoing;
                         putMap();
                         break;
-                    case 'E':
+                    case E: //earlier seed
                         noise.setSeed(noise.getSeed() - 1);
                         putMap();
                         break;
-                    case 'e':
+                    case S: //seed
                         noise.setSeed(noise.getSeed() + 1);
                         putMap();
                         break;
-                    case 'N':
-                    case 'n':
+                    case N: // noise type
                         noise.setNoiseType((noise.getNoiseType() + 1) % 10);
                         putMap();
                         break;
-                    case 'D':
-                    case 'd':
-                        dim = (dim+1) & 3;
+                    case D: //dimension
+                        dim = (dim + 1) & 3;
                         putMap();
                         break;
-                    case 'F':
-                    case 'f':
-                        noise.setFrequency(NumberTools.swayTight(freq += 0.125f) * 0.25f + 0x1p-7f);
+                    case F: // frequency
+                        noise.setFrequency((float) Math.sin(freq += 0.125f) * 0.25f + 0.25f + 0x1p-7f);
                         putMap();
                         break;
-                    case 'R':
-                    case 'r':
+                    case R: // fRactal type
                         noise.setFractalType((noise.getFractalType() + 1) % 3);
                         putMap();
                         break;
-                    case 'H':
-                    case 'h':
+                    case H: // higher octaves
                         noise.setFractalOctaves((octaves = octaves + 1 & 7) + 1);
                         putMap();
                         break;
-                    case 'L':
-                    case 'l':
+                    case L: // lower octaves
                         noise.setFractalOctaves((octaves = octaves + 7 & 7) + 1);
                         putMap();
                         break;
-                    case 'i':
-                    case 'I':
-                        if(inverse = !inverse) {
+                    case I: // inverse mode
+                        if (inverse = !inverse) {
                             noise.setFractalLacunarity(0.5f);
                             noise.setFractalGain(2f);
-                        }
-                        else {
+                        } else {
                             noise.setFractalLacunarity(2f);
                             noise.setFractalGain(0.5f);
                         }
                         putMap();
                         break;
-                    case 'K': // sKip
-                    case 'k':
+                    case K: // sKip
                         ctr += 1000;
                         putMap();
                         break;
-                    case 'Q':
-                    case 'q':
-                    case SquidInput.ESCAPE: {
+                    case Q:
+                    case ESCAPE: {
                         Gdx.app.exit();
                     }
                 }
+                return true;
             }
-        });
-        input.setRepeatGap(Long.MAX_VALUE);
-        // ABSOLUTELY NEEDED TO HANDLE INPUT
+        };
         Gdx.input.setInputProcessor(input);
-        putMap();
     }
 
     public void putMap() {
+        renderer.begin(view.getCamera().combined, GL_POINTS);
         float bright;
         switch (dim) {
             case 0:
                 for (int x = 0; x < 512; x++) {
                     for (int y = 0; y < 512; y++) {
                         bright = basicPrepare(noise.getConfiguredNoise(x + ctr, y + ctr));
-                        back[x][y] = floatGet(bright, bright, bright, 1f);
+                        renderer.color(bright, bright, bright, 1f);
+                        renderer.vertex(x, y, 0);
                     }
                 }
                 break;
@@ -194,57 +142,51 @@ public class FastNoiseVisualizer extends ApplicationAdapter {
                 for (int x = 0; x < 512; x++) {
                     for (int y = 0; y < 512; y++) {
                         bright = basicPrepare(noise.getConfiguredNoise(x, y, ctr));
-                        back[x][y] = floatGet(bright, bright, bright, 1f);
+                        renderer.color(bright, bright, bright, 1f);
+                        renderer.vertex(x, y, 0);
                     }
                 }
                 break;
             case 2:
                 for (int x = 0; x < 512; x++) {
                     for (int y = 0; y < 512; y++) {
-                        bright = basicPrepare(noise.getConfiguredNoise(x, y, ctr, swayRandomized(0x12345678, 0x1p-4f * (ctr + x + y))));
-                        back[x][y] = floatGet(bright, bright, bright, 1f);
+                        bright = basicPrepare(noise.getConfiguredNoise(x, y, ctr, 0x1p-4f * (x + y - ctr)));
+                        renderer.color(bright, bright, bright, 1f);
+                        renderer.vertex(x, y, 0);
                     }
                 }
                 break;
             case 3:
                 float xx, yy;
                 for (int x = 0; x < 512; x++) {
-                    xx = x * 0x1p-6f;
+                    xx = x * 0x1p-4f;
                     for (int y = 0; y < 512; y++) {
-                        yy = y * 0x1p-6f;
+                        yy = y * 0x1p-4f;
                         bright = basicPrepare(noise.getConfiguredNoise(
-                                ctr + swayRandomized(0x23456781, xx), x + swayRandomized(0x34567812, -yy), y + swayRandomized(0x45678123, -ctr),
-                                ctr - swayRandomized(0x12345678, yy), x - swayRandomized(0x81234567, ctr), y - swayRandomized(0x78123456, -xx)));
-                        back[x][y] = floatGet(bright, bright, bright, 1f);
+                                ctr + xx, x + ctr, y - ctr,
+                                ctr - yy, x +  yy, y - xx));
+                        renderer.color(bright, bright, bright, 1f);
+                        renderer.vertex(x, y, 0);
                     }
                 }
                 break;
         }
+        renderer.end();
+
     }
 
     @Override
     public void render() {
-        // standard clear the background routine for libGDX
-        Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         // not sure if this is always needed...
         Gdx.gl.glDisable(GL20.GL_BLEND);
         Gdx.graphics.setTitle(String.valueOf(Gdx.graphics.getFramesPerSecond()));
-        // need to display the map every frame, since we clear the screen to avoid artifacts.
         if (keepGoing) {
+            // standard clear the background routine for libGDX
+            Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
             ctr++;
             putMap();
         }
-        // if the user clicked, we have a list of moves to perform.
-
-        // if we are waiting for the player's input and get input, process it.
-        if (input.hasNext()) {
-            input.next();
-        }
-        // stage has its own batch and must be explicitly told to draw().
-        batch.begin();
-        tcf.draw(batch, back, 0, 0);
-        batch.end();
     }
 
     @Override
@@ -263,6 +205,7 @@ public class FastNoiseVisualizer extends ApplicationAdapter {
         config.height = height;
         config.foregroundFPS = 0;
         config.vSyncEnabled = false;
+        config.resizable = false;
         config.addIcon("Tentacle-16.png", Files.FileType.Internal);
         config.addIcon("Tentacle-32.png", Files.FileType.Internal);
         config.addIcon("Tentacle-128.png", Files.FileType.Internal);
