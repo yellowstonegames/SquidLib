7804a7da9a7ee0bfa98d84bfb8e66c47ea5af6cc
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-11-10 21:05:54 -0800

    Add another FlawedRandomness, AddRotate.

--- squidlib-util/src/main/java/squidpony/squidmath/FlawedRandomness.java
+++ squidlib-util/src/main/java/squidpony/squidmath/FlawedRandomness.java
@@ -8,6 +8,11 @@ package squidpony.squidmath;
  * Created by Tommy Ettinger on 11/10/2019.
  */
 public interface FlawedRandomness extends RandomnessSource {
+    /**
+     * A flawed randomness source that depends almost entirely on its starting state for any random-seeming results in
+     * its output. Simply outputs a number that starts with the initial seed and increases by {@code 0x1111111111111111}
+     * each time, or {@code 1229782938247303441}.
+     */
     class BigCounter implements FlawedRandomness, StatefulRandomness
     {
         public long state;
@@ -42,8 +47,54 @@ public interface FlawedRandomness extends RandomnessSource {
         }
 
         @Override
-        public RandomnessSource copy() {
-            return null;
+        public BigCounter copy() {
+            return new BigCounter(state);
+        }
+    }
+
+    /**
+     * A flawed randomness source that adds a rotation of its state, to its state, every generation. The rotation amount
+     * is also determined by state. This one's probably pretty bad; I don't really know how bad it will be to a human
+     * observer, but it also depends on what cycle it starts in. The state probably shouldn't ever be 0, since this
+     * will only produce 0 after its state becomes 0. Of course, this is flawed, so it can become 0 in the course of
+     * normal generation.
+     */
+    class AddRotate implements FlawedRandomness, StatefulRandomness
+    {
+        public long state;
+
+        public AddRotate()
+        {
+            this((long) ((Math.random() - 0.5) * 0x10000000000000L)
+                    ^ (long) (((Math.random() - 0.5) * 2.0) * 0x8000000000000000L));
+        }
+        public AddRotate(long state) {
+            this.state = state == 0 ? 1 : state;
+        }
+
+        @Override
+        public long getState() {
+            return state;
+        }
+
+        @Override
+        public void setState(long state) {
+            this.state = state == 0 ? 1 : state;
+        }
+
+        @Override
+        public int next(int bits) {
+            return (int)((state += (state << state | state >>> -state)) >>> 64 - bits);
+        }
+
+        @Override
+        public long nextLong() {
+            return (state += (state << state | state >>> -state));
+        }
+
+        @Override
+        public AddRotate copy() {
+            return new AddRotate(state);
         }
     }
 }
