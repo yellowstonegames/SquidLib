c692eaf6cce9f7105d94545852a5619e960cc288
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-11-12 21:14:02 -0800

    PaletteReducer is now much more ready to be copied into libGDX.

--- squidlib/src/main/java/squidpony/squidgrid/gui/gdx/PaletteReducer.java
+++ squidlib/src/main/java/squidpony/squidgrid/gui/gdx/PaletteReducer.java
@@ -6,8 +6,7 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ByteArray;
 import com.badlogic.gdx.utils.IntIntMap;
-import squidpony.squidmath.IRNG;
-import squidpony.squidmath.NumberTools;
+import com.badlogic.gdx.utils.NumberUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,7 +15,7 @@ import java.util.Comparator;
 
 /**
  * Data that can be used to limit the colors present in a Pixmap or other image, here with the goal of using 256 or less
- * colors in the image (for saving indexed-mode images).
+ * colors in the image (for saving indexed-mode images). This class relies only on libGDX and some JDK code.
  * <br>
  * Created by Tommy Ettinger on 6/23/2018.
  */
@@ -1039,32 +1038,32 @@ public class PaletteReducer {
         return pixmap;
     }
 
-    /**
-     * Retrieves a random non-0 color index for the palette this would reduce to, with a higher likelihood for colors
-     * that are used more often in reductions (those with few similar colors). The index is returned as a byte that,
-     * when masked with 255 as with {@code (palette.randomColorIndex(random) & 255)}, can be used as an index into a
-     * palette array with 256 or less elements that should have been used with {@link #exact(int[])} before to set the
-     * palette this uses.
-     * @param random an IRNG instance, such as a GWTRNG or RNG
-     * @return a randomly selected color index from this palette with a non-uniform distribution, can be any byte but 0
-     */
-    public byte randomColorIndex(IRNG random)
-    {
-        return paletteMapping[random.next(15)];
-    }
-
-    /**
-     * Retrieves a random non-transparent color from the palette this would reduce to, with a higher likelihood for
-     * colors that are used more often in reductions (those with few similar colors). The color is returned as an
-     * RGBA8888 int; you can assign one of these into a Color with {@link Color#rgba8888ToColor(Color, int)} or
-     * {@link Color#set(int)}.
-     * @param random an IRNG instance, such as a GWTRNG or RNG
-     * @return a randomly selected color from this palette with a non-uniform distribution
-     */
-    public int randomColor(IRNG random)
-    {
-        return paletteArray[paletteMapping[random.next(15)] & 255];
-    }
+//    /**
+//     * Retrieves a random non-0 color index for the palette this would reduce to, with a higher likelihood for colors
+//     * that are used more often in reductions (those with few similar colors). The index is returned as a byte that,
+//     * when masked with 255 as with {@code (palette.randomColorIndex(random) & 255)}, can be used as an index into a
+//     * palette array with 256 or less elements that should have been used with {@link #exact(int[])} before to set the
+//     * palette this uses.
+//     * @param random an IRNG instance, such as a GWTRNG or RNG
+//     * @return a randomly selected color index from this palette with a non-uniform distribution, can be any byte but 0
+//     */
+//    public byte randomColorIndex(IRNG random)
+//    {
+//        return paletteMapping[random.next(15)];
+//    }
+//
+//    /**
+//     * Retrieves a random non-transparent color from the palette this would reduce to, with a higher likelihood for
+//     * colors that are used more often in reductions (those with few similar colors). The color is returned as an
+//     * RGBA8888 int; you can assign one of these into a Color with {@link Color#rgba8888ToColor(Color, int)} or
+//     * {@link Color#set(int)}.
+//     * @param random an IRNG instance, such as a GWTRNG or RNG
+//     * @return a randomly selected color from this palette with a non-uniform distribution
+//     */
+//    public int randomColor(IRNG random)
+//    {
+//        return paletteArray[paletteMapping[random.next(15)] & 255];
+//    }
 
     /**
      * Looks up {@code color} as if it was part of an image being color-reduced and finds the closest color to it in the
@@ -1093,10 +1092,10 @@ public class PaletteReducer {
      */
     public float reduceFloat(float packedColor)
     {
-        final int color = NumberTools.floatToIntBits(packedColor); // color is ABGR8888
+        final int color = Integer.reverseBytes(NumberUtils.floatToIntBits(packedColor)); // color is ABGR8888
         if(color >= 0) // if color is non-negative, then alpha is less than half of opaque
             return 0f;
-        return NumberTools.reversedIntBitsToFloat(paletteArray[paletteMapping[
+        return NumberUtils.intBitsToFloat(paletteArray[paletteMapping[
                 (color << 7 & 0x7C00)
                         | (color >>> 6 & 0x3E0)
                         | (color >>> 19 & 0x1F)] & 0xFF] & 0xFFFFFFFE);
@@ -1115,9 +1114,9 @@ public class PaletteReducer {
         if(color.a < 0.5f)
             return color.set(0);
         return color.set(paletteArray[paletteMapping[
-                ((int) (color.r * 0x1f.8p+10) & 0x7C00)
-                        | ((int) (color.g * 0x1f.8p+5) & 0x3E0)
-                        | ((int) (color.r * 0x1f.8p+0))] & 0xFF]);
+                ((int) (color.r * 0x1fp+10 + 0x0.8p+10) & 0x7C00)
+                        | ((int) (color.g * 0x1fp+5 + 0x0.8p+5) & 0x3E0)
+                        | ((int) (color.r * 0x1fp+0 + 0x0.8p+0))] & 0xFF]);
     }
 
 }
