0eff9bcec2c438e8631071a6a8c3e62da4ad5373
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-11-24 20:26:37 -0800

    Add a favor parameter to TweakRNG to produce higher or lower results more often

--- squidlib-util/src/main/java/squidpony/squidmath/TweakRNG.java
+++ squidlib-util/src/main/java/squidpony/squidmath/TweakRNG.java
@@ -6,8 +6,8 @@ import java.io.Serializable;
 
 /**
  * Highly experimental RNG that can be configured to smoothly transition between producing mostly values in the center
- * of its range, to producing more values at or near the extremes. The probability distribution is... unusual, with
- * lumps that rise or fall based on centrality.
+ * of its range, to producing more values at or near the extremes, as well as favoring high or low results. The
+ * probability distribution is... unusual, with lumps that rise or fall based on centrality.
  * <br>
  * <a href="https://i.imgur.com/VCvtlSc.gifv">Here's an animation of the distribution graph changing.</a>
  * <br>
@@ -18,7 +18,7 @@ public class TweakRNG extends AbstractRNG implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private long stateA, stateB, centrality;
+    private long stateA, stateB, centrality, favor;
     public TweakRNG() {
         this((long) ((Math.random() - 0.5) * 0x10000000000000L)
                         ^ (long) (((Math.random() - 0.5) * 2.0) * 0x8000000000000000L),
@@ -37,6 +37,14 @@ public class TweakRNG extends AbstractRNG implements Serializable {
         stateA = seedA;
         stateB = seedB | 1L;
         this.centrality = centrality % 0x10000L;
+        this.favor = 0L;
+    }
+
+    public TweakRNG(final long seedA, final long seedB, final long centrality, final long favor) {
+        stateA = seedA;
+        stateB = seedB | 1L;
+        this.centrality = centrality % 0x10000L;
+        this.favor = favor % 0x10000L;
     }
 
     @Override
@@ -46,13 +54,13 @@ public class TweakRNG extends AbstractRNG implements Serializable {
 
     @Override
     public int nextInt() {
-        return (int) (nextLong() >> 32);
+        return (int) (nextLong() >>> 32);
     }
 
     @Override
     public long nextLong() {
         final long r = internalLong(), s = internalLong();
-        return (long) (NumberTools.atan2_(((r & 0xFF) - (r >>> 8 & 0xFF)) * ((r >>> 16 & 0xFF) - (r >>> 24 & 0xFF)),
+        return (long) (NumberTools.atan2_(((r & 0xFF) - (r >>> 8 & 0xFF)) * ((r >>> 16 & 0xFF) - (r >>> 24 & 0xFF)) - favor,
                 ((r >>> 32 & 0xFF) - (r >>> 40 & 0xFF)) * ((r >>> 48 & 0xFF) - (r >>> 56 & 0xFF)) + centrality) * 0x6000000000000000L)
                 + (s & 0x1FFFFFFFFFFFFFFFL) << 1 ^ (s >>> 63);
     }
@@ -65,14 +73,17 @@ public class TweakRNG extends AbstractRNG implements Serializable {
     @Override
     public double nextDouble() {
         final long r = internalLong();
-        return NumberTools.atan2_(((r & 0xFF) - (r >>> 8 & 0xFF)) * ((r >>> 16 & 0xFF) - (r >>> 24 & 0xFF)),
-                ((r >>> 32 & 0xFF) - (r >>> 40 & 0xFF)) * ((r >>> 48 & 0xFF) - (r >>> 56 & 0xFF)) + centrality) * 0.75
+        return NumberTools.atan2_(((r & 0xFF) - (r >>> 8 & 0xFF)) * ((r >>> 16 & 0xFF) - (r >>> 24 & 0xFF)) - favor,
+                ((r >>> 32 & 0xFF) - (r >>> 40 & 0xFF)) * ((r >>> 48 & 0xFF) - (r >>> 56 & 0xFF)) + (double)centrality) * 0.75
                 + (internalLong() & 0xfffffffffffffL) * 0x1p-54;
     }
 
     @Override
     public float nextFloat() {
-        return (float) nextDouble();
+        final long r = internalLong();
+        return NumberTools.atan2_(((r & 0xFF) - (r >>> 8 & 0xFF)) * ((r >>> 16 & 0xFF) - (r >>> 24 & 0xFF)) - favor,
+                ((r >>> 32 & 0xFF) - (r >>> 40 & 0xFF)) * ((r >>> 48 & 0xFF) - (r >>> 56 & 0xFF)) + centrality) * 0.75f
+                + (internalLong() & 0x7fffffL) * 0x1p-25f;
     }
     
     @Override
@@ -114,6 +125,28 @@ public class TweakRNG extends AbstractRNG implements Serializable {
         this.centrality = centrality % 65536L;
     }
 
+
+    public long getFavor() {
+        return favor;
+    }
+
+    /**
+     * Adjusts the value bias of this TweakRNG, often to positive numbers (which bias toward higher results), but also
+     * often to negative numbers (which bias toward lower results). All results will still be in their normal range, but
+     * will change how often high or low values occur. Unusual results will occur if favor is non-zero and you get a
+     * long with {@link #nextLong()}; in that case, the values are treated as higher when unsigned, so positive favor
+     * makes both high positive values and all negative values more common. Doubles and floats will behave normally.
+     * @param favor should be between -65535 and 65535; positive values bias toward higher (unsigned for longs) results
+     */
+    public void setFavor(long favor) {
+        this.favor = favor % 65536L;
+    }
+
+    /**
+     *{@link TangleRNG}'s algorithm; not all longs will be returned by any individual generator, but all generators as a
+     * whole will return all longs with equal likelihood.
+     * @return a random long in the full range; each state is advanced by 1 step.
+     */
     private long internalLong()
     {
         final long s = (stateA += 0xC6BC279692B5C323L);
