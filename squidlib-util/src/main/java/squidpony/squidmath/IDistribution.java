e846e4ace787cee8fcc1235028d8e5e7128ac43a
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-11-27 20:28:19 -0800

    Add clamped and fractional-with-offset SimpleDistributions.

--- squidlib-util/src/main/java/squidpony/squidmath/IDistribution.java
+++ squidlib-util/src/main/java/squidpony/squidmath/IDistribution.java
@@ -31,7 +31,13 @@ public interface IDistribution {
     double getUpperBound();
     
     abstract class SimpleDistribution implements IDistribution {
-        
+
+        /**
+         * Makes a new SimpleDistribution implementation given any IDistribution (typically one with large or infinite
+         * bounds) by getting the fractional component of a result from {@code otherDistribution}.
+         * @param otherDistribution any other IDistribution
+         * @return a new anonymous implementation of SimpleDistribution that gets the fractional part of {@code otherDistribution}.
+         */
         public static SimpleDistribution fractionalDistribution(final IDistribution otherDistribution)
         {
             return new SimpleDistribution() {
@@ -42,7 +48,44 @@ public interface IDistribution {
                 }
             };
         }
-        
+        /**
+         * Makes a new SimpleDistribution implementation given any IDistribution (typically one with large or infinite
+         * bounds) by getting the fractional component of {@code offset} plus a result from {@code otherDistribution}.
+         * Using the offset allows distributions like {@link GaussianDistribution} to become centered halfway on 0.5,
+         * making the result of this distribution have a Gaussian-like peak on 0.5 instead of of peaking at the bounds
+         * when offset is 0.0.
+         * @param otherDistribution any other IDistribution
+         * @return a new anonymous implementation of SimpleDistribution that gets the fractional part of {@code otherDistribution}.
+         */
+        public static SimpleDistribution fractionalOffsetDistribution(final IDistribution otherDistribution, final double offset)
+        {
+            return new SimpleDistribution() {
+                @Override
+                public double nextDouble(IRNG rng) {
+                    final double v = otherDistribution.nextDouble(rng) + offset;
+                    return v - (v >= 0.0 ? (int) v : (int)v - 1);
+                }
+            };
+        }
+
+        /**
+         * Makes a new SimpleDistribution implementation given any IDistribution (typically one with large or infinite
+         * bounds) by simply clamping results that are below 0 to 0 and at least 1 to 0.9999999999999999 (the largest
+         * double less than 1.0 than can be represented). This will behave very oddly for distributions that are
+         * centered on 0.0.
+         * @param otherDistribution any other IDistribution
+         * @return a new anonymous implementation of SimpleDistribution that clamps {@code otherDistribution} in range.
+         */
+        public static SimpleDistribution clampedDistribution(final IDistribution otherDistribution)
+        {
+            return new SimpleDistribution() {
+                @Override
+                public double nextDouble(IRNG rng) {
+                    return Math.max(0.0, Math.min(0.9999999999999999, otherDistribution.nextDouble(rng)));
+                }
+            };
+        }
+
         /**
          * Gets the lower inclusive bound, which is 0.0.
          *
