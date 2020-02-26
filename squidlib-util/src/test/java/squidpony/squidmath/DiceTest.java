bbf29e9eab02d00b5046749acdde6d3be17ef05f
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-02-25 20:24:15 -0800

    Small changes to BlueNoise.indexedBlueNoisePoint() to improve jitter
    Using the bit count of longs in an incremental sequence seems to vary in quality based on the bit count of the increment. I switched to using nearby primes that have 32 of 64 bits set.

--- squidlib-util/src/test/java/squidpony/squidmath/DiceTest.java
+++ squidlib-util/src/test/java/squidpony/squidmath/DiceTest.java
@@ -1,15 +1,18 @@
 package squidpony.squidmath;
 
-import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Tommy Ettinger on 9/26/2017.
  */
 public class DiceTest
 {
-    //uncomment when you want to know what the seeded RNG will produce; it should produce this reliably
-    //@Test
+    @Test
+    //comment when you want to know what the seeded RNG will produce; it should produce this reliably
+    @Ignore
     public void showDiceResults()
     {
         StatefulRNG r = new StatefulRNG(new LightRNG(0x1337BEEFDEAL));
@@ -49,34 +52,34 @@ public class DiceTest
     {
         StatefulRNG r = new StatefulRNG(new LightRNG(0x1337BEEFDEAL));
         Dice d = new Dice(r);
-        Assert.assertEquals(d.roll("1d6"),   3 );
-        Assert.assertEquals(d.roll("3d8"),   17);
-        Assert.assertEquals(d.roll("1d12+3"),15);
-        Assert.assertEquals(d.roll("1d12*5"),15);
-        Assert.assertEquals(d.roll("3>5d12"),26);
-        Assert.assertEquals(d.roll("3<5d12"),9 );
-        Assert.assertEquals(d.roll("10:20"), 14);
-        Assert.assertEquals(d.roll("10:11"), 11);
-        Assert.assertEquals(d.roll("10:11"), 11);
-        Assert.assertEquals(d.roll("10:11"), 10);
-        Assert.assertEquals(d.roll("5!4"),   28);
-        Assert.assertEquals(d.roll("10!2"),  36);
-        Assert.assertEquals(d.roll("5d6+2d8"), 26);
-        Assert.assertEquals(d.roll("5!6 + 2!8"), 17);
-        Assert.assertEquals(d.roll("3>4d6  *  1!4"), 85);
-        Assert.assertEquals(d.roll("1d20-3"), 17);
-        Assert.assertEquals(d.roll("1d20*-3"), -42);
-        Assert.assertEquals(d.roll("1>10!2"), 5);
-        Assert.assertEquals(d.roll("10:100:200"), 110);
-        Assert.assertEquals(d.roll("3<10!100"), 48);
-        Assert.assertEquals(d.roll("0:-15:15"), -15);
-        Assert.assertEquals(d.roll("d20"), 19);
-        Assert.assertEquals(d.roll("!20"), 9);
-        Assert.assertEquals(d.roll("!20"), 4);
-        Assert.assertEquals(d.roll("42"), 42);
-        Assert.assertEquals(d.roll("42d"), 42);
-        Assert.assertEquals(d.roll("42!"), 42);
-        Assert.assertEquals(d.roll("d"), 0);
-        Assert.assertEquals(d.roll("!"), 0);
+        assertEquals(d.roll("1d6"),   3 );
+        assertEquals(d.roll("3d8"),   17);
+        assertEquals(d.roll("1d12+3"),15);
+        assertEquals(d.roll("1d12*5"),15);
+        assertEquals(d.roll("3>5d12"),26);
+        assertEquals(d.roll("3<5d12"),9 );
+        assertEquals(d.roll("10:20"), 14);
+        assertEquals(d.roll("10:11"), 11);
+        assertEquals(d.roll("10:11"), 11);
+        assertEquals(d.roll("10:11"), 10);
+        assertEquals(d.roll("5!4"),   28);
+        assertEquals(d.roll("10!2"),  36);
+        assertEquals(d.roll("5d6+2d8"), 26);
+        assertEquals(d.roll("5!6 + 2!8"), 17);
+        assertEquals(d.roll("3>4d6  *  1!4"), 85);
+        assertEquals(d.roll("1d20-3"), 17);
+        assertEquals(d.roll("1d20*-3"), -42);
+        assertEquals(d.roll("1>10!2"), 5);
+        assertEquals(d.roll("10:100:200"), 110);
+        assertEquals(d.roll("3<10!100"), 48);
+        assertEquals(d.roll("0:-15:15"), -15);
+        assertEquals(d.roll("d20"), 19);
+        assertEquals(d.roll("!20"), 9);
+        assertEquals(d.roll("!20"), 4);
+        assertEquals(d.roll("42"), 42);
+        assertEquals(d.roll("42d"), 42);
+        assertEquals(d.roll("42!"), 42);
+        assertEquals(d.roll("d"), 0);
+        assertEquals(d.roll("!"), 0);
     }
 }
