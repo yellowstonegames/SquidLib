81130333fe49bf2f29335b5adcacbfbc3222e2d9
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-04-28 01:01:39 -0700

    GridCompression, stage 1 of being able to compress world maps

--- squidlib-extra/src/test/java/squidpony/GreasedRegionCompressionTest.java
+++ squidlib-extra/src/test/java/squidpony/GreasedRegionCompressionTest.java
@@ -7,6 +7,8 @@ import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import squidpony.squidgrid.mapping.WorldMapGenerator;
 import squidpony.squidmath.GreasedRegion;
 
+import java.util.Arrays;
+
 /**
  * Expected output:
  * <br>
@@ -37,7 +39,8 @@ public class GreasedRegionCompressionTest extends ApplicationAdapter {
     @Override
     public void create() {
         super.create();
-        GreasedRegion earth = new WorldMapGenerator.MimicMap().earthOriginal.copy();
+        WorldMapGenerator.MimicMap mimicMap = new WorldMapGenerator.MimicMap();
+        GreasedRegion earth = mimicMap.earthOriginal.copy();
         String baseString = earth.serializeToString();
         String lz = LZSEncoding.compressToUTF16(baseString);
         String me = earth.toCompressedString();
@@ -58,10 +61,57 @@ public class GreasedRegionCompressionTest extends ApplicationAdapter {
         System.out.println("LZ correct? " + (GreasedRegion.deserializeFromString(LZSEncoding.decompressFromUTF16(Gdx.files.local("Earth_LZS.txt").readString("UTF-16"))).equals(earth)));
         System.out.println("Custom correct? " + (GreasedRegion.decompress(Gdx.files.local("Earth_Comp.txt").readString("UTF-16")).equals(earth)));
         System.out.println("Both correct? " + (GreasedRegion.decompress(LZSEncoding.decompressFromUTF16(Gdx.files.local("Earth_Both.txt").readString("UTF-16"))).equals(earth)));
+
+        System.out.println();
+
+        mimicMap.generate(123L);
+        double min = 10000.0, max = -10000.0;
+        for (int x = 0; x < mimicMap.width; x++) {
+            for (int y = 0; y < mimicMap.height; y++) {
+                min = Math.min(min, mimicMap.heatData[x][y]);
+                max = Math.max(max, mimicMap.heatData[x][y]);
+            }
+        }
+        System.out.println("Initial min heat: " + min);
+        System.out.println("Initial max heat: " + max);
+        String gcomp = GridCompression.compress(mimicMap.heatData);
+        double[][] heatData = GridCompression.byteToDoubleGrid(GridCompression.decompress(gcomp));
+
+        min = 10000.0;
+        max = -10000.0;
+        for (int x = 0; x < mimicMap.width; x++) {
+            for (int y = 0; y < mimicMap.height; y++) {
+                min = Math.min(min, heatData[x][y]);
+                max = Math.max(max, heatData[x][y]);
+            }
+        }
+        System.out.println("Post min heat: " + min);
+        System.out.println("Post max heat: " + max);
+
+        me = GridCompression.compress(heatData);
+        baseString = Converters.convertArrayDouble2D.stringify(heatData);
+        lz = LZSEncoding.compressToUTF16(baseString);
+        both = LZSEncoding.compressToUTF16(me);
+        System.out.println("Heat Map");
+        System.out.println("Base size   : " + baseString.length());
+        System.out.println("LZS size    : " + lz.length());
+        System.out.println("Custom size : " + me.length());
+        System.out.println("Both size   : " + both.length());
+        System.out.println("LZ correct? " + (Arrays.deepEquals(Converters.convertArrayDouble2D.restore(LZSEncoding.decompressFromUTF16(lz)), heatData)));
+        System.out.println("Custom correct? " + (Arrays.deepEquals(GridCompression.byteToDoubleGrid(GridCompression.decompress(me)), heatData)));
+        System.out.println("Both correct? " + (Arrays.deepEquals(GridCompression.byteToDoubleGrid(GridCompression.decompress(LZSEncoding.decompressFromUTF16(both))), heatData)));
+        Gdx.files.local("Heat.txt").writeString(baseString, false, "UTF-16");
+        Gdx.files.local("Heat_Comp.txt").writeString(me, false, "UTF-16");
+        Gdx.files.local("Heat_LZS.txt").writeString(lz, false, "UTF-16");
+        Gdx.files.local("Heat_Both.txt").writeString(both, false, "UTF-16");
+        System.out.println("Reading in written Heat files...");
+        System.out.println("LZ correct? " + (Arrays.deepEquals(Converters.convertArrayDouble2D.restore(LZSEncoding.decompressFromUTF16((Gdx.files.local("Heat_LZS.txt").readString("UTF-16")))), heatData)));
+        System.out.println("Custom correct? " + (Arrays.deepEquals(GridCompression.byteToDoubleGrid(GridCompression.decompress((Gdx.files.local("Heat_Comp.txt").readString("UTF-16")))), heatData)));
+        System.out.println("Both correct? " + (Arrays.deepEquals(GridCompression.byteToDoubleGrid(GridCompression.decompress(LZSEncoding.decompressFromUTF16(Gdx.files.local("Heat_Both.txt").readString("UTF-16")))), heatData)));
         
         System.out.println();
-        
-        GreasedRegion australia = new WorldMapGenerator.LocalMimicMap().earthOriginal.copy();
+        WorldMapGenerator.LocalMimicMap localMimicMap = new WorldMapGenerator.LocalMimicMap();
+        GreasedRegion australia = localMimicMap.earthOriginal.copy();
         baseString = australia.serializeToString();
         lz = LZSEncoding.compressToUTF16(baseString);
         me = australia.toCompressedString();
---
+++ squidlib-util/src/main/java/squidpony/GridCompression.java
@@ -0,0 +1,160 @@
+package squidpony;
+
+import squidpony.annotation.Beta;
+import squidpony.squidmath.CoordPacker;
+import squidpony.squidmath.MathExtras;
+
+/**
+ * Very early way of additional compression that can be applied to 2D double arrays. This doesn't compress as well as
+ * using {@link LZSEncoding} on a simply-serialized String produced by {@link Converters#convertArrayDouble2D}, but you
+ * can use LZSEncoding on the results of this class to significantly reduce output size. Testing on a heat map of double
+ * values from 0.0 to 1.0 from a world map:
+ * <pre>
+ * Base size   : 1143142 // this is an uncompressed String produced by Converters
+ * LZS size    : 89170   // this uses LZSEncoding alone on the above Base string
+ * Custom size : 216209  // this uses GridCompression alone
+ * Both size   : 43120   // this uses GridCompression followed by LZSEncoding
+ * </pre>
+ * <br>
+ * Created by Tommy Ettinger on 4/27/2020.
+ */
+@Beta
+public class GridCompression {
+    public static String compress(double[][] grid)
+    {
+        CoordPacker.init();
+        final int width = grid.length, height = grid[0].length;
+        StringBuilder[] packs = new StringBuilder[8];
+        for (int i = 0; i < 8; i++) {
+            packs[i] = new StringBuilder(width * height >> 3);
+        }
+        StringBuilder packing = new StringBuilder(width * height + 128);
+        StringKit.appendHex(packing, width);
+        StringKit.appendHex(packing, height);
+        final int chunksX = width + 255 >> 8, chunksY = height + 127 >> 7;
+        short[] skip = new short[8];
+        boolean[] on = new boolean[8];
+        for (int bigX = 0, baseX = 0; bigX < chunksX; bigX++, baseX += 256) {
+            for (int bigY = 0, baseY = 0; bigY < chunksY; bigY++, baseY += 128) {
+                for (int p = 0; p < 8; p++) {
+                    packs[p].append(';');
+                    skip[p] = 0;
+                    on[p] = false;
+                }
+                boolean current;
+                short hx, hy;
+                int xSize = Math.min(256, width - baseX), ySize = Math.min(128, height - baseY),
+                        limit = 0x8000, mapLimit = xSize * ySize, code;
+                if (xSize <= 128) {
+                    limit >>= 1;
+                    if (xSize <= 64) {
+                        limit >>= 1;
+                        if (ySize <= 64) {
+                            limit >>= 1;
+                            if (ySize <= 32) {
+                                limit >>= 1;
+                                if (xSize <= 32) {
+                                    limit >>= 1;
+                                }
+                            }
+                        }
+                    }
+                }
+                for (int i = 0, ml = 0; i < limit && ml < mapLimit; i++) {
+                    hx = CoordPacker.hilbertX[i];
+                    hy = CoordPacker.hilbertY[i];
+                    if (hx >= xSize || hy >= ySize) {
+                        for (int p = 0; p < 8; p++) {
+                            if (on[p]) {
+                                on[p] = false;
+                                packs[p].append((char) (skip[p] + 256));
+                                skip[p] = 0;
+                            }
+                        }
+                        for (int p = 0; p < 8; p++) {
+                            skip[p]++;
+                        }
+                        continue;
+                    }
+                    ml++;
+                    code = MathExtras.clamp((int) (grid[baseX + hx][baseY + hy] * 256), 0, 255);
+                    code ^= code >>> 1;
+                    for (int p = 0; p < 8; p++) {
+                        current = (code >>> p & 1) == 1;
+                        if (current != on[p]) {
+                            packs[p].append((char) (skip[p] + 256));
+                            skip[p] = 0;
+                            on[p] = current;
+                        }
+                    }
+                    for (int p = 0; p < 8; p++) {
+                        skip[p]++;
+                    }
+                }
+                for (int p = 0; p < 8; p++) {
+                    if (on[p])
+                    {
+                        packs[p].append((char) (skip[p] + 256));
+                    }
+                }
+            }
+        }
+        packing.append(packs[0]);
+        for (int p = 1; p < 8; p++) {
+            packing.append('.').append(packs[p]);
+        }
+        return packing.toString();
+    }
+    public static byte[][] decompress(String compressed)
+    {
+        CoordPacker.init();
+        final int width = StringKit.intFromHex(compressed), height = StringKit.intFromHex(compressed, 8, 16);
+        byte[][] target = new byte[width][height];
+        final int chunksX = width + 255 >> 8, chunksY = height + 127 >> 7;
+        int startPack = 16, endPack, endLayer, idx;
+        boolean on;
+        for (int b = 0; b < 8; b++) {
+            endLayer = compressed.indexOf('.', startPack+1);
+            if (endLayer < 0) endLayer = compressed.length();
+            for (int bigX = 0, baseX = 0; bigX < chunksX; bigX++, baseX += 256) {
+                for (int bigY = 0, baseY = 0; bigY < chunksY; bigY++, baseY += 128) {
+                    ++startPack;
+                    endPack = Math.min(endLayer, compressed.indexOf(';', startPack));
+                    if (endPack < 0) endPack = endLayer;
+                    on = false;
+                    idx = 0;
+                    for (int p = startPack; p < endPack; p++, on = !on) {
+                        if (on) {
+                            for (int toSkip = idx + (compressed.charAt(p) - 256); idx < toSkip && idx < 0x8000; idx++) {
+                                target[CoordPacker.hilbertX[idx] + baseX][CoordPacker.hilbertY[idx] + baseY] |= 1 << b;
+                            }
+                        } else {
+                            idx += compressed.charAt(p) - 256;
+                        }
+                    }
+                    startPack = endPack;
+                }
+            }
+            ++startPack;
+        }
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int b = target[x][y] & 0xFF;
+                b ^= b >>> 1;
+                b ^= b >>> 2;
+                target[x][y] = (byte)(b ^ b >>> 4);
+            }
+        }
+        return target;
+    }
+    public static double[][] byteToDoubleGrid(byte[][] bytes) {
+        final int width = bytes.length, height = bytes[0].length;
+        double[][] doubles = new double[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                doubles[x][y] = (bytes[x][y] & 0xFF) * 0x1p-8;
+            }
+        }
+        return doubles;
+    }
+}
