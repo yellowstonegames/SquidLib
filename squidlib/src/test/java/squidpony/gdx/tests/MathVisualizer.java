1162700273e42caebe17fb2e88b7eb20f9b2bbc4
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-05-01 17:29:34 -0700

    Using erfinv to get Gaussian values is at least interesting.

--- squidlib/src/test/java/squidpony/gdx/tests/MathVisualizer.java
+++ squidlib/src/test/java/squidpony/gdx/tests/MathVisualizer.java
@@ -22,7 +22,7 @@ import java.util.Arrays;
  * Created by Tommy Ettinger on 1/13/2018.
  */
 public class MathVisualizer extends ApplicationAdapter {
-    private int mode = 49;
+    private int mode = 14;
     private int modes = 51;
     private FilterBatch batch;
     private SparseLayers layers;
@@ -79,6 +79,130 @@ public class MathVisualizer extends ApplicationAdapter {
         b = (b & 0x000000007FF007FFL) +     ((b & 0x0007FF0000000000L) >>> 40);
         return (((a & 0x0000000000000FFFL) + ((a & 0x000000007FF00000L) >>> 20))
                 - ((b & 0x0000000000000FFFL) + ((b & 0x000000007FF00000L) >>> 20))) * (0x1p-10f);
+    }
+
+    /*
+     * Licensed to the Apache Software Foundation (ASF) under one or more
+     * contributor license agreements.  See the NOTICE file distributed with
+     * this work for additional information regarding copyright ownership.
+     * The ASF licenses this file to You under the Apache License, Version 2.0
+     * (the "License"); you may not use this file except in compliance with
+     * the License.  You may obtain a copy of the License at
+     *
+     *      http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing, software
+     * distributed under the License is distributed on an "AS IS" BASIS,
+     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     * See the License for the specific language governing permissions and
+     * limitations under the License.
+     */
+    /**
+     * Returns the inverse error function result.
+     * <p>
+     * This implementation is described in the paper:
+     * <a href="http://people.maths.ox.ac.uk/gilesm/files/gems_erfinv.pdf">Approximating
+     * the erfinv function</a> by Mike Giles, Oxford-Man Institute of Quantitative Finance,
+     * which was published in GPU Computing Gems, volume 2, 2010.
+     * The source code is available <a href="http://gpucomputing.net/?q=node/1828">here</a>.
+     * The code as it is here is from Apache Commons Math 3.
+     * </p>
+     * @param x the value
+     * @return t such that x = erf(t)
+     * @since 3.2
+     */
+    public static double erfInv(final double x) {
+
+        // beware that the logarithm argument must be
+        // commputed as (1.0 - x) * (1.0 + x),
+        // it must NOT be simplified as 1.0 - x * x as this
+        // would induce rounding errors near the boundaries +/-1
+        double w = - Math.log((1.0 - x) * (1.0 + x));
+        double p;
+
+        if (w < 6.25) {
+            w -= 3.125;
+            p =  -3.6444120640178196996e-21;
+            p =   -1.685059138182016589e-19 + p * w;
+            p =   1.2858480715256400167e-18 + p * w;
+            p =    1.115787767802518096e-17 + p * w;
+            p =   -1.333171662854620906e-16 + p * w;
+            p =   2.0972767875968561637e-17 + p * w;
+            p =   6.6376381343583238325e-15 + p * w;
+            p =  -4.0545662729752068639e-14 + p * w;
+            p =  -8.1519341976054721522e-14 + p * w;
+            p =   2.6335093153082322977e-12 + p * w;
+            p =  -1.2975133253453532498e-11 + p * w;
+            p =  -5.4154120542946279317e-11 + p * w;
+            p =    1.051212273321532285e-09 + p * w;
+            p =  -4.1126339803469836976e-09 + p * w;
+            p =  -2.9070369957882005086e-08 + p * w;
+            p =   4.2347877827932403518e-07 + p * w;
+            p =  -1.3654692000834678645e-06 + p * w;
+            p =  -1.3882523362786468719e-05 + p * w;
+            p =    0.0001867342080340571352 + p * w;
+            p =  -0.00074070253416626697512 + p * w;
+            p =   -0.0060336708714301490533 + p * w;
+            p =      0.24015818242558961693 + p * w;
+            p =       1.6536545626831027356 + p * w;
+        } else if (w < 16.0) {
+            w = Math.sqrt(w) - 3.25;
+            p =   2.2137376921775787049e-09;
+            p =   9.0756561938885390979e-08 + p * w;
+            p =  -2.7517406297064545428e-07 + p * w;
+            p =   1.8239629214389227755e-08 + p * w;
+            p =   1.5027403968909827627e-06 + p * w;
+            p =   -4.013867526981545969e-06 + p * w;
+            p =   2.9234449089955446044e-06 + p * w;
+            p =   1.2475304481671778723e-05 + p * w;
+            p =  -4.7318229009055733981e-05 + p * w;
+            p =   6.8284851459573175448e-05 + p * w;
+            p =   2.4031110387097893999e-05 + p * w;
+            p =   -0.0003550375203628474796 + p * w;
+            p =   0.00095328937973738049703 + p * w;
+            p =   -0.0016882755560235047313 + p * w;
+            p =    0.0024914420961078508066 + p * w;
+            p =   -0.0037512085075692412107 + p * w;
+            p =     0.005370914553590063617 + p * w;
+            p =       1.0052589676941592334 + p * w;
+            p =       3.0838856104922207635 + p * w;
+        } else if (!Double.isInfinite(w)) {
+            w = Math.sqrt(w) - 5.0;
+            p =  -2.7109920616438573243e-11;
+            p =  -2.5556418169965252055e-10 + p * w;
+            p =   1.5076572693500548083e-09 + p * w;
+            p =  -3.7894654401267369937e-09 + p * w;
+            p =   7.6157012080783393804e-09 + p * w;
+            p =  -1.4960026627149240478e-08 + p * w;
+            p =   2.9147953450901080826e-08 + p * w;
+            p =  -6.7711997758452339498e-08 + p * w;
+            p =   2.2900482228026654717e-07 + p * w;
+            p =  -9.9298272942317002539e-07 + p * w;
+            p =   4.5260625972231537039e-06 + p * w;
+            p =  -1.9681778105531670567e-05 + p * w;
+            p =   7.5995277030017761139e-05 + p * w;
+            p =  -0.00021503011930044477347 + p * w;
+            p =  -0.00013871931833623122026 + p * w;
+            p =       1.0103004648645343977 + p * w;
+            p =       4.8499064014085844221 + p * w;
+        } else {
+            // this branch does not appears in the original code, it
+            // was added because the previous branch does not handle
+            // x = +/-1 correctly. In this case, w is positive infinity
+            // and as the first coefficient (-2.71e-11) is negative.
+            // Once the first multiplication is done, p becomes negative
+            // infinity and remains so throughout the polynomial evaluation.
+            // So the branch above incorrectly returns negative infinity
+            // instead of the correct positive infinity.
+            p = Double.POSITIVE_INFINITY;
+        }
+
+        return p * x;
+    }
+    
+    
+    public double erfGaussian(){
+        return erfInv(rng.nextDouble() * 2.0 - 1.0);
     }
     
     public final float editedCurve()
@@ -740,28 +864,9 @@ public class MathVisualizer extends ApplicationAdapter {
             break;
             case 15: {
                 Gdx.graphics.setTitle(Gdx.graphics.getFramesPerSecond() +
-                        " GaussianDistribution, clamped [-4,4]");
+                        " erfGaussian, clamped [-4,4]");
                 for (int i = 0; i < 0x100000; i++) {
-//                    long a = diver.nextLong();
-//                    a = (a & 0x007C1F07C1F07C1FL) +     ((a & 0x0F8370F8370F8370L) >>>  5);
-//                    a = (a & 0x00003F0003F0003FL) +     ((a & 0x00FC000FC000FC00L) >>>  10);
-//                    a = (a & 0x00007F000000007FL) +     ((a & 0x0000000007F00000L) >>>  20);
-//                    amounts[(int) (((((a & 0x00000000000000FFL) + ((a & 0x00007F0000000000L) >>> 40))) / (12f * 31f)) * 511)]++;
-//                    long a = diver.nextLong(), b = diver.nextLong();
-//                    a = (a & 0x00FF00FF00FF00FFL) +     ((a & 0xFF00FF00FF00FF00L) >>>  8);
-//                    b = (b & 0x00FF00FF00FF00FFL) +     ((b & 0xFF00FF00FF00FF00L) >>>  8);
-//                    a = (a & 0x000001FF000001FFL) +     ((a & 0x01FF000001FF0000L) >>> 16);
-////                    b = (b & 0x00000000000001FFL) +     ((b & 0x0000000001FF0000L) >>> 16);
-//                    amounts[(int) (((
-//                            ((a & 0x00000000000003FFL) + ((a & 0x000003FF00000000L) >>> 32))
-//                                    + (b & 0x00000000000001FFL) + ((b & 0x0000000001FF0000L) >>> 16))
-//                            / (12f * 255.5f)) * 511)]++;
-//                    amounts[256 + (int) (((a & 0x00000000000003FFL) + ((a & 0x000003FF00000000L) >>> 32))
-//                            - ((b & 0x00000000000003FFL) + ((b & 0x000003FF00000000L) >>> 32)) >> 3)]++;
-
-
-                    amounts[Noise.fastFloor(MathUtils.clamp(GaussianDistribution.instance.nextDouble(rng), -0x3.FCp0, 0x3.FCp0) * 64 + 256)]++;
-//                    amounts[MathUtils.round(MathUtils.clamp(fastGaussian() * 64f + 256f, 0f, 511f))]++;
+                    amounts[Noise.fastFloor(MathUtils.clamp(erfGaussian(), -0x3.FCp0, 0x3.FCp0) * 64 + 256)]++;
                 }
                 for (int i = 0; i < 512; i++) {
                     float color = (i & 63) == 0
@@ -771,64 +876,33 @@ public class MathVisualizer extends ApplicationAdapter {
                         layers.backgrounds[i][j] = color;
                     }
                 }
-//                Arrays.fill(amounts, 0);
-//                for (int i = 0; i < 0x1000000; i++) {
-//                    amounts[Noise.fastFloor(MathUtils.clamp(nextGaussian(), -0x3.FCp0f, 0x3.FCp0f) * 64 + 256)]++;
-//                }
-//                for (int i = 0; i < 512; i++) {
-//                    for (int j = 519 - (amounts[i] >> 9); j < 520; j++) {
-//                        layers.backgrounds[i][j] = SColor.lerpFloatColors(layers.backgrounds[i][j],
-//                                -0x1.64b5eap125F, //SColor.AURORA_EMBERS
-//                                0.3f);
-//                    }
-//                }
-
-
                 for (int i = 0; i < 10; i++) {
                     for (int j = 8; j < 520; j += 32) {
                         layers.backgrounds[i][j] = -0x1.7677e8p125F;
                     }
                 }
 
-
 //                Gdx.graphics.setTitle(Gdx.graphics.getFramesPerSecond() +
-//                        " XSP, bitBased & 0x1FFL");
-//                for (int i = 0; i < 0x1000000; i++) {
-//                    amounts[(int) ((xsp.nextLongBit(0x1800000000000000L)) & 0x1FFL)]++;
+//                        " GaussianDistribution, clamped [-4,4]");
+//                for (int i = 0; i < 0x100000; i++) {
+//                    amounts[Noise.fastFloor(MathUtils.clamp(GaussianDistribution.instance.nextDouble(rng), -0x3.FCp0, 0x3.FCp0) * 64 + 256)]++;
+////                    amounts[MathUtils.round(MathUtils.clamp(fastGaussian() * 64f + 256f, 0f, 511f))]++;
 //                }
 //                for (int i = 0; i < 512; i++) {
 //                    float color = (i & 63) == 0
 //                            ? -0x1.c98066p126F // CW Azure
 //                            : -0x1.d08864p126F; // CW Sapphire
-//                    for (int j = 519 - (amounts[i] >> 8); j < 520; j++) {
+//                    for (int j = Math.max(0, 519 - (amounts[i] >> 5)); j < 520; j++) {
 //                        layers.backgrounds[i][j] = color;
 //                    }
 //                }
+//
 //                for (int i = 0; i < 10; i++) {
 //                    for (int j = 8; j < 520; j += 32) {
 //                        layers.backgrounds[i][j] = -0x1.7677e8p125F;
 //                    }
 //                }
 
-//                Gdx.graphics.setTitle(Gdx.graphics.getFramesPerSecond() +
-//                        " RandomXS128, random.nextInt(0x200)");
-//                RandomXS128 random = new RandomXS128();
-//                for (int i = 0; i < 0x1000000; i++) {
-//                    amounts[random.nextInt(512)]++;
-//                }
-//                for (int i = 0; i < 512; i++) {
-//                    float color = (i & 63) == 0
-//                            ? -0x1.c98066p126F // CW Azure
-//                            : -0x1.d08864p126F; // CW Sapphire
-//                    for (int j = 519 - (amounts[i] >> 8); j < 520; j++) {
-//                        layers.backgrounds[i][j] = color;
-//                    }
-//                }
-//                for (int i = 0; i < 10; i++) {
-//                    for (int j = 8; j < 520; j += 32) {
-//                        layers.backgrounds[i][j] = -0x1.7677e8p125F;
-//                    }
-//                }
             }
             break;
             case 16: {
@@ -1334,8 +1408,8 @@ public class MathVisualizer extends ApplicationAdapter {
                 }
                 else {
                     for (int i = 1; i <= 0x400000; i++, p = (p + 1.6180339887498949f) % MathUtils.PI2) {
-//                    amounts[MathUtils.round(MathUtils.sin(p) * 255f + 256f)]++;           // libGDX's LUT way
-                    amounts[MathUtils.round(minimaxSin(p) * 255f + 256f)]++;           // libGDX's LUT way
+                    amounts[MathUtils.round(MathUtils.sin(p) * 255f + 256f)]++;           // libGDX's LUT way
+//                    amounts[MathUtils.round(minimaxSin(p) * 255f + 256f)]++;           // libGDX's LUT way
 //                        amounts[MathUtils.round((NumberTools.sinDegrees(p)) * 255f + 256f)]++;   // SquidLib's no-LUT way
                     }
                     
