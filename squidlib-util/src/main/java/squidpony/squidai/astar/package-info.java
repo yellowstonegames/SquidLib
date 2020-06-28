19386673d9353e58554d309fa47fb7898fc9632b
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-06-28 02:11:13 -0700

    Yay package-info.java for all the packages!

---
+++ squidlib-util/src/main/java/squidpony/squidai/astar/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Most of the classes related to A-Star pathfinding on a grid or arbitrary graph are here.
+ * @see squidpony.squidmath.AStarSearch AStarSearch is not in this package, but uses code from it heavily.
+ */
+package squidpony.squidai.astar;
