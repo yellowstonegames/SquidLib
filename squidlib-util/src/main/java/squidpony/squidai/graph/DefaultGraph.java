cc9dd6e6a85bfd8319c92a70dde5531d43f8e9b0
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-07-13 15:38:51 -0700

    Fix printing graphs after successive runs of A-Star
    Something similar to this may be needed for other algorithms.

--- squidlib-util/src/main/java/squidpony/squidai/graph/DefaultGraph.java
+++ squidlib-util/src/main/java/squidpony/squidai/graph/DefaultGraph.java
@@ -237,7 +237,7 @@ public class DefaultGraph extends UndirectedGraph<Coord> implements Serializable
 		Node<Coord> nc;
 		for (int i = 0; i < vs; i++) {
 			nc = vertexMap.getAt(i);
-			if(!nc.visited || nc.distance >= 9999.5)
+			if(!nc.seen || nc.distance >= 9999.5)
 				continue;
 			int d = (int) (nc.distance + 0.5), x = nc.object.x * 5, y = nc.object.y;
 			cs[y * w5 + x    ] = (d >= 1000) ? (char) ('0' + d / 1000) : ' ';
