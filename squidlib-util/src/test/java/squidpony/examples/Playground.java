9c1c4e1c47096416b9f5ace8dfc2b8f2d2efe2fb
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-07-19 23:47:16 -0700

    Better Mnemonics using the power of reversible randomness!

--- squidlib-util/src/test/java/squidpony/examples/Playground.java
+++ squidlib-util/src/test/java/squidpony/examples/Playground.java
@@ -1,5 +1,6 @@
 package squidpony.examples;
 
+import squidpony.StringKit;
 import squidpony.squidmath.*;
 
 /**
@@ -147,135 +148,29 @@ public class Playground {
         return n << 3 | 0x4000000000000003L;
     }
     private void go() {
-//        float r, r_;
-//        for (int xx = 0; xx < 20; xx++) {
-//            float x = LinnormRNG.determineFloat(xx) * 4f - 2f;
-//            for (int yy = 0; yy < 20; yy++) {
-//                float y = LinnormRNG.determineFloat(yy) * 4f - 2f;
-//                System.out.printf("x: %1.9f, y: %1.9f, atan2: %1.9f, atan2_: %1.9f, diff: %1.9f\n", x, y,
-//                        r = (NumberTools.atan2(y, x) / (3.141592653589793f * 2f) + 1f) % 1f,
-//                        r_ = NumberTools.atan2_(y, x),
-//                        Math.abs(r - r_));
+//        final int SIZE = 0x8000;
+//        double[] xs = new double[SIZE], ys = new double[SIZE];
+//        double closest = 999.0, average, x, y, xx, yy, t;
+//        for (int i = 0; i < SIZE; i++) {
+//            xs[i] = x = robertsAltDetermine2x(i);
+//            ys[i] = y = robertsAltDetermine2y(i);
+//            for (int k = 0; k < i; k++) {
+//                xx = x - xs[k];
+//                yy = y - ys[k];
+//                closest = Math.min(closest, xx * xx + yy * yy);
 //            }
 //        }
-//        LightRNG r = new LightRNG();
-//        System.out.printf("state: 0x%016XL\n\n", r.getState());
-//        long l;
-////        System.out.printf("0x%016X, %d\n", l = GreasedRegion.randomInterleave(r) << 3 | 3L, Long.bitCount(l));
-//        for (int b = 10; b < 18; b++) {
-//            for (int i = 0; i < 5; i++) {
-//                System.out.printf("0x%016X, %d\n", l = splatterBits(r, b), Long.bitCount(l));
-//            }
-//        }
-//        long bits = 0x41C64E6BL, ib = ~bits & 0xFFFFFFFFL;
-//        bits |= (bits << 16);
-//        ib |= (ib << 16);
-//        bits &= 0x0000FFFF0000FFFFL;
-//        ib &= 0x0000FFFF0000FFFFL;
-//        bits |= (bits << 8);
-//        ib |= (ib << 8);
-//        bits &= 0x00FF00FF00FF00FFL;
-//        ib &= 0x00FF00FF00FF00FFL;
-//        bits |= (bits << 4);
-//        ib |= (ib << 4);
-//        bits &= 0x0F0F0F0F0F0F0F0FL;
-//        ib &= 0x0F0F0F0F0F0F0F0FL;
-//        bits |= (bits << 2);
-//        ib |= (ib << 2);
-//        bits &= 0x3333333333333333L;
-//        ib &= 0x3333333333333333L;
-//        bits |= (bits << 1);
-//        ib |= (ib << 1);
-//        bits &= 0x5555555555555555L;
-//        ib &= 0x5555555555555555L;
-//        System.out.printf("\n0x%016X, %d\n", bits = (-8L & (bits | (ib << 1))) | 3L, Long.bitCount(bits));
-//        bits &= 0x7FFFFFFFFFFFFFFBL;
-//        for (int b = 20; b < 29; b++) {
-//            for (int i = 0; i < 5; i++) {
-//                System.out.printf("0x%016X, %d\n", l = (bits & GreasedRegion.approximateBits(r, b)) | 0x4000000000000003L, Long.bitCount(l));
-//                
-//            }
-//        }
-
-        
-//        double x, y, v;
-//        final double PI2 = Math.PI * 2.0;
-//        System.out.println("{");
-//        for (int i = 1; i <= 256; i++) {
-//            v = 0.61803398874989484820458683436563811772 * i + (i / (1.5 * 2.7182818284590452354));
-//            //v = VanDerCorputQRNG.determine(5, i) * PI2;
-//            x = Math.cos(v);
-//            y = Math.sin(v);
-//            System.out.println("{" + x + ", " + y + "},");
-//        }
-//        System.out.println("}");
-
-
-//        double r, a, b;
-//        for (int s = 1; s < 100; s++) {
-//            final int leading = Integer.numberOfLeadingZeros(s);
-//            if((r = Math.abs((a = (Integer.reverse(s) >>> leading) / (double) (1 << (32 - leading))) - (b = (Integer.reverse(s) >>> 1) * 0x1p-31))) > 0.001)
-//                System.out.println("UH OH on " + s + ": " + r);
-//            System.out.println(s + ": " + a + ", " + b);
-//        }
-
-//        long seed = 0x1337DEADBEEFCAFEL;
-//        for (double i = 0.0; i <= 17.0; i += 0x1p-4) {
-//            System.out.printf("% 21.10f : % 3.10f  % 3.10f  % 3.10f  % 3.10f\n", i, NumberTools.sway((float)i), NumberTools.sway(i), NumberTools.swayRandomized(seed, i), NumberTools.swayRandomized(seed+1L, i));
-//            System.out.printf("% 21.10f : % 3.10f  % 3.10f  % 3.10f  % 3.10f\n", -i, NumberTools.sway((float)-i), NumberTools.sway(-i), NumberTools.swayRandomized(seed,-i), NumberTools.swayRandomized(seed+1L,-i));
-//        }
-//        System.out.println("NumberTools.sway(Float.POSITIVE_INFINITY)  :  " + NumberTools.sway(Float.POSITIVE_INFINITY));
-//        System.out.println("NumberTools.sway(Float.NEGATIVE_INFINITY)  :  " + NumberTools.sway(Float.NEGATIVE_INFINITY));
-//        System.out.println("NumberTools.sway(Float.MIN_VALUE)          :  " + NumberTools.sway(Float.MIN_VALUE));
-//        System.out.println("NumberTools.sway(Float.MAX_VALUE)          :  " + NumberTools.sway(Float.MAX_VALUE));
-//        System.out.println("NumberTools.sway(Float.MIN_NORMAL)         :  " + NumberTools.sway(Float.MIN_NORMAL));
-//        System.out.println("NumberTools.sway(Float.NaN)                :  " + NumberTools.sway(Float.NaN));
-//        System.out.println();
-//        System.out.println("NumberTools.sway(Double.POSITIVE_INFINITY) :  " + NumberTools.sway(Double.POSITIVE_INFINITY));
-//        System.out.println("NumberTools.sway(Double.NEGATIVE_INFINITY) :  " + NumberTools.sway(Double.NEGATIVE_INFINITY));
-//        System.out.println("NumberTools.sway(Double.MIN_VALUE)         :  " + NumberTools.sway(Double.MIN_VALUE));
-//        System.out.println("NumberTools.sway(Double.MAX_VALUE)         :  " + NumberTools.sway(Double.MAX_VALUE));
-//        System.out.println("NumberTools.sway(Double.MIN_NORMAL)        :  " + NumberTools.sway(Double.MIN_NORMAL));
-//        System.out.println("NumberTools.sway(Double.NaN)               :  " + NumberTools.sway(Double.NaN));
-//        System.out.println();
-//        System.out.println("swayRandomized(Double.POSITIVE_INFINITY)   :  " + NumberTools.swayRandomized(seed, Double.POSITIVE_INFINITY));
-//        System.out.println("swayRandomized(Double.NEGATIVE_INFINITY)   :  " + NumberTools.swayRandomized(seed, Double.NEGATIVE_INFINITY));
-//        System.out.println("swayRandomized(Double.MIN_VALUE)           :  " + NumberTools.swayRandomized(seed, Double.MIN_VALUE));
-//        System.out.println("swayRandomized(Double.MAX_VALUE)           :  " + NumberTools.swayRandomized(seed, Double.MAX_VALUE));
-//        System.out.println("swayRandomized(Double.MIN_NORMAL)          :  " + NumberTools.swayRandomized(seed, Double.MIN_NORMAL));
-//        System.out.println("swayRandomized(Double.NaN)                 :  " + NumberTools.swayRandomized(seed, Double.NaN));
-
-        final int SIZE = 0x8000;
-        double[] xs = new double[SIZE], ys = new double[SIZE];
-        double closest = 999.0, average, x, y, xx, yy, t;
-        for (int i = 0; i < SIZE; i++) {
-//            xs[i] = x = planarDetermine(0xDE4DBEEF, i + 1); // why do these work so well???
-//            ys[i] = y = planarDetermine(0x1337D00D, i + 1); //  now we know, they were forming lines
-//            xs[i] = x = planarDetermine(57L, i);
-//            ys[i] = y = planarDetermine(73L, i);
-            xs[i] = x = robertsAltDetermine2x(i);
-            ys[i] = y = robertsAltDetermine2y(i);
-            for (int k = 0; k < i; k++) {
-                xx = x - xs[k];
-                yy = y - ys[k];
-                closest = Math.min(closest, xx * xx + yy * yy);
-            }
-        }
-        System.out.printf("Closest distance with Alt Roberts: %.9f\n", Math.sqrt(closest));
-        int s;
-//        for(int p : new int[]{1, 3, 11, 13, 15, 23, 29, 39, 41, 47, 71, 93}) {
-////        for (int p = 1; p < 100; p+=2) {
+//        System.out.printf("Closest distance with Alt Roberts: %.9f\n", Math.sqrt(closest));
+//        int s;
+//        //Closest distance with Halton(2,13): 0.000871727
+//        //Closest distance with Halton(2,39): 0.001147395
+//        for(int p : new int[]{3, 11, 13, 23, 29, 39, 41, 47, 69, 75, 87}) {
 //            closest = 999.0;
 //            average = 0.0;
-//            //p2 = p * 0x2C9277B5 | 1;  //0xAC564B05
 //            for (int i = 0; i < SIZE; i++) {
-//                //s = GreasedRegion.disperseBits(Integer.reverse(i * p + 1));
-////                s = GreasedRegion.disperseBits((int) (VanDerCorputQRNG.altDetermine(p, i) * 0x40000000));
-//                s = GreasedRegion.disperseBits(Integer.reverse((p * 0x2C9277B5 | 1) * (i + 1)));
-//                xs[i] = x = (s & 0xffff) * 0x1p-16;
-//                ys[i] = y = (s >>> 16 & 0xffff) * 0x1p-16;
-////                xs[i] = x = (s & 0x7fff) * 0x1p-15;
-////                ys[i] = y = (s >>> 16 & 0x7fff) * 0x1p-15;
+//                s = i;//(i ^ (i << 7 | i >>> 25) ^ (i << 19 | i >>> 13));
+//                xs[i] = x = VanDerCorputQRNG.determine2(s);
+//                ys[i] = y = VanDerCorputQRNG.determine(p, s);
 //                for (int k = 0; k < i; k++) {
 //                    xx = x - xs[k];
 //                    yy = y - ys[k];
@@ -283,26 +178,30 @@ public class Playground {
 //                    average += t * 0x1p-15;
 //                }
 //            }
-//            System.out.printf("Closest distance with Z-order on altHalton(%d): %.9f, 'average' %9.9f\n", p, Math.sqrt(closest), average);
+//            System.out.printf("Closest distance with Halton(2,%d): %.9f, 'average' %9.9f\n", p, Math.sqrt(closest), average);
 //        }
-        //Closest distance with Halton(2,13): 0.000871727
-        //Closest distance with Halton(2,39): 0.001147395
-        for(int p : new int[]{3, 11, 13, 23, 29, 39, 41, 47, 69, 75, 87}) {
-//        for (int p = 3; p < 100; p+=2) {
-            closest = 999.0;
-            average = 0.0;
-            for (int i = 0; i < SIZE; i++) {
-                s = i;//(i ^ (i << 7 | i >>> 25) ^ (i << 19 | i >>> 13));
-                xs[i] = x = VanDerCorputQRNG.determine2(s);
-                ys[i] = y = VanDerCorputQRNG.determine(p, s);
-                for (int k = 0; k < i; k++) {
-                    xx = x - xs[k];
-                    yy = y - ys[k];
-                    closest = Math.min(closest, t = xx * xx + yy * yy);
-                    average += t * 0x1p-15;
+        int state = 3;
+        for (int i = 0; i < 0x40000000; i++) {
+            state *= 0xae3cc725;
+            if(state > 0 && state < 0x100000){
+                int mmi = MathExtras.modularMultiplicativeInverse(state);
+                if(mmi > 0 && mmi < 0x100000)
+                {
+                    System.out.println("0x" + StringKit.hex(state) + " inverted by 0x" + StringKit.hex(mmi));
                 }
             }
-            System.out.printf("Closest distance with Halton(2,%d): %.9f, 'average' %9.9f\n", p, Math.sqrt(closest), average);
+        }
+        System.out.println("HALFWAY THERE");
+        state = 5;
+        for (int i = 0; i < 0x40000000; i++) {
+            state *= 0xae3cc725;
+            if(state > 0 && state < 0x100000){
+                int mmi = MathExtras.modularMultiplicativeInverse(state);
+                if(mmi > 0 && mmi < 0x100000)
+                {
+                    System.out.println("0x" + StringKit.hex(state) + " inverted by 0x" + StringKit.hex(mmi));
+                }
+            }
         }
     }
 
