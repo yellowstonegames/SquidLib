31b3467b3312bbfa07157bc1a0d0a3e653c1d0db
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-10-25 15:39:33 -0700

    Add Jon Barron's nice bias/gain generalization, made branch-less

--- squidlib-util/src/main/java/squidpony/squidmath/MathExtras.java
+++ squidlib-util/src/main/java/squidpony/squidmath/MathExtras.java
@@ -366,4 +366,48 @@ public final class MathExtras
         }
         return a - (n - a * a >>> 63);
     }
+
+    /**
+     * A generalization on bias and gain functions that can represent both; this version is branch-less.
+     * This is based on <a href="https://arxiv.org/abs/2010.09714">this micro-paper</a> by Jon Barron, which
+     * generalizes the earlier bias and gain rational functions by Schlick. The second and final page of the
+     * paper has useful graphs of what the s (shape) and t (turning point) parameters do; shape should be 0
+     * or greater, while turning must be between 0 and 1, inclusive. This effectively combines two different
+     * curving functions so they continue into each other when x equals turning. The shape parameter will
+     * cause this to imitate "smoothstep-like" splines when greater than 1 (where the values ease into their
+     * starting and ending levels), or to be the inverse when less than 1 (where values start like square
+     * root does, taking off very quickly, but also end like square does, landing abruptly at the ending
+     * level). You should only give x values between 0 and 1, inclusive.
+     * @param x progress through the spline, from 0 to 1, inclusive
+     * @param shape must be greater than or equal to 0; values greater than 1 are "normal interpolations" 
+     * @param turning a value between 0.0 and 1.0, inclusive, where the shape changes
+     * @return a float between 0 and 1, inclusive
+     */
+    public static float barronSpline(final float x, final float shape, final float turning) {
+        final float d = turning - x;
+        final int f = NumberTools.floatToIntBits(d) >> 31, n = f | 1;
+        return ((turning * n - f) * (x + f)) / (Float.MIN_VALUE - f + (x + shape * d) * n) - f;
+    }
+
+    /**
+     * A generalization on bias and gain functions that can represent both; this version is branch-less.
+     * This is based on <a href="https://arxiv.org/abs/2010.09714">this micro-paper</a> by Jon Barron, which
+     * generalizes the earlier bias and gain rational functions by Schlick. The second and final page of the
+     * paper has useful graphs of what the s (shape) and t (turning point) parameters do; shape should be 0
+     * or greater, while turning must be between 0 and 1, inclusive. This effectively combines two different
+     * curving functions so they continue into each other when x equals turning. The shape parameter will
+     * cause this to imitate "smoothstep-like" splines when greater than 1 (where the values ease into their
+     * starting and ending levels), or to be the inverse when less than 1 (where values start like square
+     * root does, taking off very quickly, but also end like square does, landing abruptly at the ending
+     * level). You should only give x values between 0 and 1, inclusive.
+     * @param x progress through the spline, from 0 to 1, inclusive
+     * @param shape must be greater than or equal to 0; values greater than 1 are "normal interpolations" 
+     * @param turning a value between 0.0 and 1.0, inclusive, where the shape changes
+     * @return a double between 0 and 1, inclusive
+     */
+    public static double barronSpline(final double x, final double shape, final double turning) {
+        final double d = turning - x;
+        final int f = NumberTools.doubleToHighIntBits(d) >> 31, n = f | 1;
+        return ((turning * n - f) * (x + f)) / (Double.MIN_VALUE - f + (x + shape * d) * n) - f;
+    }
 }
