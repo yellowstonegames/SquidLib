3cea7b091413deffec2da92cd53afa294185f436
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-03-30 20:17:49 -0700

    Add atan(), update atan2() with faster and more precise method.

--- squidlib-util/src/main/java/squidpony/squidmath/NumberTools.java
+++ squidlib-util/src/main/java/squidpony/squidmath/NumberTools.java
@@ -1153,114 +1153,212 @@ public final class NumberTools {
     }
 
     /**
+     * Arc tangent approximation with very low error, using an algorithm from the 1955 research study
+     * "Approximations for Digital Computers," by RAND Corporation (this is sheet 9's algorithm, which is the
+     * second-fastest and second-least precise). This method is usually much faster than {@link Math#atan(double)},
+     * but is somewhat less precise than Math's implementation.
+     * @param i an input to the inverse tangent function; any finite double is accepted
+     * @return an output from the inverse tangent function, from PI/-2.0 to PI/2.0 inclusive
+     */
+    public static double atan(final double i) {
+        final double n = Math.abs(i);
+        final double c = (n - 1.0) / (n + 1.0);
+        final double c2 = c * c;
+        final double c3 = c * c2;
+        final double c5 = c3 * c2;
+        final double c7 = c5 * c2;
+        return Math.copySign(0.7853981633974483 +
+                (0.999215 * c - 0.3211819 * c3 + 0.1462766 * c5 - 0.0389929 * c7), i);
+    }
+
+    /**
+     * Arc tangent approximation with very low error, using an algorithm from the 1955 research study
+     * "Approximations for Digital Computers," by RAND Corporation (this is sheet 9's algorithm, which is the
+     * second-fastest and second-least precise). This method is usually much faster than {@link Math#atan(double)},
+     * but is somewhat less precise than Math's implementation.
+     * @param i an input to the inverse tangent function; any finite float is accepted
+     * @return an output from the inverse tangent function, from PI/-2.0 to PI/2.0 inclusive
+     */
+    public static float atan(final float i) {
+        final float n = Math.abs(i);
+        final float c = (n - 1f) / (n + 1f);
+        final float c2 = c * c;
+        final float c3 = c * c2;
+        final float c5 = c3 * c2;
+        final float c7 = c5 * c2;
+        return Math.copySign(0.7853981633974483f +
+                (0.999215f * c - 0.3211819f * c3 + 0.1462766f * c5 - 0.0389929f * c7), i);
+    }
+
+    /**
      * Close approximation of the frequently-used trigonometric method atan2, with higher precision than libGDX's atan2
-     * approximation. Maximum error is below 0.001 radians.
+     * approximation. Maximum error is below 0.00009 radians.
      * Takes y and x (in that unusual order) as doubles, and returns the angle from the origin to that point in radians.
-     * It is about 5 times faster than {@link Math#atan2(double, double)} (roughly 17 ns instead of roughly 88 ns for
-     * Math, though the computer was under some load during testing). It is almost identical in speed to libGDX'
-     * MathUtils approximation of the same method; MathUtils seems to have worse average error, though.
-     * Credit to Nic Taylor and imuli, with Taylor publishing
-     * <a href="https://www.dsprelated.com/showarticle/1052.php">this nice post</a> and imuli commenting with very
-     * handy information that makes this approach usable. This method changed from an earlier
-     * technique that was twice as fast but had very poor quality, enough to be visually noticeable. See also
-     * {@link #atan2_(double, double)} if you don't want a mess converting to degrees or some other measurement, since
-     * that method returns an angle from 0.0 (equal to 0 degrees) to 1.0 (equal to 360 degrees).
+     * It is about 5 times faster than {@link Math#atan2(double, double)} (roughly 12 ns instead of roughly 62 ns for
+     * Math, on Java 8 HotSpot). It is slightly faster than libGDX' MathUtils approximation of the same method;
+     * MathUtils seems to have worse average error, though.
+     * <br>
+     * Credit for this goes to the 1955 research study "Approximations for Digital Computers," by RAND Corporation. This
+     * is sheet 9's algorithm, which is the second-fastest and second-least precise. The algorithm on sheet 8 is faster,
+     * but only by a very small degree, and is considerably less precise. That study provides an {@link #atan(double)}
+     * method, and the small code to make that work as atan2() was worked out from Wikipedia.
+     * <br>
+     * See also {@link #atan2_(double, double)} if you don't want a mess converting to degrees or some other
+     * measurement, since that method returns an angle from 0f (equal to 0 degrees) to 1f (equal to 360 degrees). You
+     * can also use {@link #atan2Degrees(double, double)} or {@link #atan2Degrees360(double, double)} to produce a
+     * result in degrees, either from -180 to 180 or 0 to 360.
      * @param y y-component of the point to find the angle towards; note the parameter order is unusual by convention
      * @param x x-component of the point to find the angle towards; note the parameter order is unusual by convention
      * @return the angle to the given point, in radians as a double; ranges from -PI to PI
      */
-    public static double atan2(double y, double x)
-    {
-        if(y == 0.0 && x >= 0.0) return 0.0;
-        double ay = Math.abs(y), ax = Math.abs(x);
-        boolean invert = ay > ax;
-        double z = invert ? ax / ay : ay / ax;
-        z = ((((0.141499  * z) - 0.343315 ) * z - 0.016224 ) * z + 1.003839 ) * z - 0.000158 ;
-        if (invert) z = 1.5707963267948966 - z;
-        if (x < 0) z = 3.141592653589793 - z;
-        return Math.copySign(z, y);
+    public static double atan2(final double y, final double x) {
+        if(x > 0)
+            return atan(y / x);
+        else if(x < 0) {
+            if(y >= 0)
+                return atan(y / x) + 3.14159265358979323846;
+            else
+                return atan(y / x) - 3.14159265358979323846;
+        }
+        else if(y > 0) return 1.5707963267948966;
+        else if(y < 0) return -1.5707963267948966;
+        else return 0.0;
     }
 
     /**
      * Close approximation of the frequently-used trigonometric method atan2, with higher precision than libGDX's atan2
-     * approximation. Maximum error is below 0.001 radians.
+     * approximation. Maximum error is below 0.00009 radians.
      * Takes y and x (in that unusual order) as floats, and returns the angle from the origin to that point in radians.
-     * It is about 5 times faster than {@link Math#atan2(double, double)} (roughly 17 ns instead of roughly 88 ns for
-     * Math, though the computer was under some load during testing). It is almost identical in speed to libGDX'
-     * MathUtils approximation of the same method; MathUtils seems to have worse average error, though.
-     * Credit to Nic Taylor and imuli, with Taylor publishing
-     * <a href="https://www.dsprelated.com/showarticle/1052.php">this nice post</a> and imuli commenting with very
-     * handy information that makes this approach usable. This method changed from an earlier
-     * technique that was twice as fast but had very poor quality, enough to be visually noticeable. See also
-     * {@link #atan2_(float, float)} if you don't want a mess converting to degrees or some other measurement, since
-     * that method returns an angle from 0f (equal to 0 degrees) to 1f (equal to 360 degrees).
+     * It is about 5 times faster than {@link Math#atan2(double, double)} (roughly 12 ns instead of roughly 62 ns for
+     * Math, on Java 8 HotSpot). It is slightly faster than libGDX' MathUtils approximation of the same method;
+     * MathUtils seems to have worse average error, though.
+     * <br>
+     * Credit for this goes to the 1955 research study "Approximations for Digital Computers," by RAND Corporation. This
+     * is sheet 9's algorithm, which is the second-fastest and second-least precise. The algorithm on sheet 8 is faster,
+     * but only by a very small degree, and is considerably less precise. That study provides an {@link #atan(float)}
+     * method, and the small code to make that work as atan2() was worked out from Wikipedia.
+     * <br>
+     * See also {@link #atan2_(float, float)} if you don't want a mess converting to degrees or some other measurement,
+     * since that method returns an angle from 0f (equal to 0 degrees) to 1f (equal to 360 degrees). You can also use
+     * {@link #atan2Degrees(float, float)} or {@link #atan2Degrees360(float, float)} to produce a result in degrees,
+     * either from -180 to 180 or 0 to 360.
      * @param y y-component of the point to find the angle towards; note the parameter order is unusual by convention
      * @param x x-component of the point to find the angle towards; note the parameter order is unusual by convention
      * @return the angle to the given point, in radians as a float; ranges from -PI to PI
      */
-    public static float atan2(float y, float x) {
-        if (y == 0f && x >= 0f) return 0f;
-        float ay = Math.abs(y), ax = Math.abs(x);
-        boolean invert = ay > ax;
-        float z = invert ? ax / ay : ay / ax;
-        z = ((((0.141499f * z) - 0.343315f) * z - 0.016224f) * z + 1.003839f) * z - 0.000158f;
-        if (invert) z = 1.5707963267948966f - z;
-        if (x < 0) z = 3.141592653589793f - z;
-        return Math.copySign(z, y);
+    public static float atan2(final float y, final float x) {
+        if(x > 0)
+            return atan(y / x);
+        else if(x < 0) {
+            if(y >= 0)
+                return atan(y / x) + 3.14159265358979323846f;
+            else
+                return atan(y / x) - 3.14159265358979323846f;
+        }
+        else if(y > 0) return 1.5707963267948966f;
+        else if(y < 0) return -1.5707963267948966f;
+        else return 0.0f;
     }
 
     /**
-     * Altered-range approximation of the frequently-used trigonometric method atan2, taking y and x positions as 
+     * This one's weird; unlike {@link #atan2_(double, double)}, it can return negative results.
+     * @param v any finite double
+     * @return between -0.25 and 0.25
+     */
+    private static double atan_(final double v) {
+        final double n = Math.abs(v);
+        final double c = (n - 1.0) / (n + 1.0);
+        final double c2 = c * c;
+        final double c3 = c * c2;
+        final double c5 = c3 * c2;
+        final double c7 = c5 * c2;
+        return Math.copySign(0.125 + 0.1590300064615682 * c - 0.051117687016646825 * c3 + 0.02328064394867594 * c5
+                - 0.006205912780487965 * c7, v);
+    }
+
+    /**
+     * This one's weird; unlike {@link #atan2_(float, float)}, it can return negative results.
+     * @param v any finite float
+     * @return between -0.25 and 0.25
+     */
+    private static float atan_(final float v) {
+        final float n = Math.abs(v);
+        final float c = (n - 1f) / (n + 1f);
+        final float c2 = c * c;
+        final float c3 = c * c2;
+        final float c5 = c3 * c2;
+        final float c7 = c5 * c2;
+        return Math.copySign(0.125f + 0.1590300064615682f * c - 0.051117687016646825f * c3 + 0.02328064394867594f * c5
+                - 0.006205912780487965f * c7, v);
+    }
+    /**
+     * Altered-range approximation of the frequently-used trigonometric method atan2, taking y and x positions as
      * doubles and returning an angle measured in turns from 0.0 to 1.0 (inclusive), with one cycle over the range
      * equivalent to 360 degrees or 2PI radians. You can multiply the angle by {@code 6.2831855f} to change to radians,
      * or by {@code 360f} to change to degrees. Takes y and x (in that unusual order) as doubles. Will never return a
      * negative number, which may help avoid costly floating-point modulus when you actually want a positive number.
-     * Credit to Nic Taylor and imuli, with Taylor publishing
-     * <a href="https://www.dsprelated.com/showarticle/1052.php">this nice post</a> and imuli commenting with very
-     * handy information that makes this approach usable. Note that
-     * {@link #atan2(double, double)} returns an angle in radians and can return negative results, which may be fine for
-     * many tasks; these two methods are extremely close in implementation and speed.
+     * <br>
+     * Credit for this goes to the 1955 research study "Approximations for Digital Computers," by RAND Corporation. This
+     * is sheet 9's algorithm, which is the second-fastest and second-least precise. The algorithm on sheet 8 is faster,
+     * but only by a very small degree, and is considerably less precise. That study provides an {@link #atan(float)}
+     * method, and the small code to make that work as atan2_() was worked out from Wikipedia.
+     * <br>
+     * Note that {@link #atan2(double, double)} returns an angle in radians and can return negative results, which may
+     * be fine for many tasks; these two methods are extremely close in implementation and speed. There are also
+     * {@link #atan2Degrees(double, double)} and {@link #atan2Degrees360(double, double)}, which provide an angle in
+     * degrees, either from -180 to 180 or 0 to 360.
      * @param y y-component of the point to find the angle towards; note the parameter order is unusual by convention
      * @param x x-component of the point to find the angle towards; note the parameter order is unusual by convention
      * @return the angle to the given point, as a double from 0.0 to 1.0, inclusive
      */
-    public static double atan2_(double y, double x)
-    {
-        if(y == 0.0 && x >= 0.0) return 0.0;
-        double ay = Math.abs(y), ax = Math.abs(x);
-        boolean invert = ay > ax;
-        double z = invert ? ax/ay : ay/ax;
-        z = (((((0.022520265292560102) * z) - (0.054640279287594046)) * z - (0.0025821297967229097)) * z + (0.1597659389184251)) * z - (0.000025146481008519463);
-        if(invert) z = 0.25 - z;
-        if(x < 0) z = 0.5 - z;
-        return y < 0 ? (int)(1+z) - z : z;
-
+    public static double atan2_(final double y, final double x) {
+        if(x > 0) {
+            if(y >= 0)
+                return atan_(y / x);
+            else
+                return atan_(y / x) + 1.0;
+        }
+        else if(x < 0) {
+            return atan_(y / x) + 0.5;
+        }
+        else if(y > 0) return 0.25;
+        else if(y < 0) return 0.75;
+        else return 0.0;
     }
+
     /**
      * Altered-range approximation of the frequently-used trigonometric method atan2, taking y and x positions as floats
      * and returning an angle measured in turns from 0.0f to 1.0f, with one cycle over the range equivalent to 360
      * degrees or 2PI radians. You can multiply the angle by {@code 6.2831855f} to change to radians, or by {@code 360f}
      * to change to degrees. Takes y and x (in that unusual order) as floats. Will never return a negative number, which
      * may help avoid costly floating-point modulus when you actually want a positive number.
-     * Credit to Nic Taylor and imuli, with Taylor publishing
-     * <a href="https://www.dsprelated.com/showarticle/1052.php">this nice post</a> and imuli commenting with very
-     * handy information that makes this approach usable. Note that
-     * {@link #atan2(float, float)} returns an angle in radians and can return negative results, which may be fine for
-     * many tasks; these two methods are extremely close in implementation and speed.
+     * <br>
+     * Credit for this goes to the 1955 research study "Approximations for Digital Computers," by RAND Corporation. This
+     * is sheet 9's algorithm, which is the second-fastest and second-least precise. The algorithm on sheet 8 is faster,
+     * but only by a very small degree, and is considerably less precise. That study provides an {@link #atan(float)}
+     * method, and the small code to make that work as atan2_() was worked out from Wikipedia.
+     * <br>
+     * Note that {@link #atan2(float, float)} returns an angle in radians and can return negative results, which may
+     * be fine for many tasks; these two methods are extremely close in implementation and speed. There are also
+     * {@link #atan2Degrees(float, float)} and {@link #atan2Degrees360(float, float)}, which provide an angle in
+     * degrees, either from -180 to 180 or 0 to 360.
      * @param y y-component of the point to find the angle towards; note the parameter order is unusual by convention
      * @param x x-component of the point to find the angle towards; note the parameter order is unusual by convention
      * @return the angle to the given point, as a float from 0.0f to 1.0f, inclusive
      */
-    public static float atan2_(float y, float x)
-    {
-        if(y == 0.0 && x >= 0.0) return 0f;
-        float ay = Math.abs(y), ax = Math.abs(x);
-        boolean invert = ay > ax;
-        float z = invert ? ax/ay : ay/ax;
-        z = (((((0.022520265292560102f) * z) - (0.054640279287594046f)) * z - (0.0025821297967229097f)) * z + (0.1597659389184251f)) * z - (0.000025146481008519463f);
-        if(invert) z = 0.25f - z;
-        if(x < 0) z = 0.5f - z;
-        return y < 0 ? (int)(1+z) - z : z;
+    public static float atan2_(final float y, final float x) {
+        if(x > 0) {
+            if(y >= 0)
+                return atan_(y / x);
+            else
+                return atan_(y / x) + 1f;
+        }
+        else if(x < 0) {
+            return atan_(y / x) + 0.5f;
+        }
+        else if(y > 0) return 0.25f;
+        else if(y < 0) return 0.75f;
+        else return 0f;
     }
 
     /**
