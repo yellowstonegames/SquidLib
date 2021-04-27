9d9c382c1b6cbcac9be81fef1ef1367f64f7fd45
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-04-26 21:06:49 -0700

    Generalize and initialize.

--- squidlib/src/test/java/squidpony/gdx/tools/BlueNoiseGenerator.java
+++ squidlib/src/test/java/squidpony/gdx/tools/BlueNoiseGenerator.java
@@ -5,24 +5,15 @@ import com.badlogic.gdx.Files;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.PixmapIO;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.GdxRuntimeException;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
-import squidpony.FakeLanguageGen;
-import squidpony.StringKit;
-import squidpony.Thesaurus;
-import squidpony.squidgrid.gui.gdx.FilterBatch;
 import squidpony.squidgrid.gui.gdx.SColor;
-import squidpony.squidgrid.gui.gdx.WorldMapView;
-import squidpony.squidgrid.mapping.WorldMapGenerator;
 import squidpony.squidmath.*;
 
 import java.io.IOException;
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -30,7 +21,7 @@ import java.util.Date;
  * https://bartwronski.com/2021/04/21/superfast-void-and-cluster-blue-noise-in-python-numpy-jax/ for more.
  */
 public class BlueNoiseGenerator extends ApplicationAdapter {
-    private static final int width = 256, height = 256;
+    private static final int size = 64, height = 64;
     private static final double sigma = 1.9, sigma2 = sigma * sigma;
     private Pixmap pm;
     private StatefulRNG rng;
@@ -46,7 +37,7 @@ public class BlueNoiseGenerator extends ApplicationAdapter {
         
         if(!Gdx.files.local(path).exists())
             Gdx.files.local(path).mkdirs();
-        pm = new Pixmap(width, height, Pixmap.Format.RGBA8888);
+        pm = new Pixmap(size, size, Pixmap.Format.RGBA8888);
         pm.setBlending(Pixmap.Blending.None);
 
         writer = new PixmapIO.PNG((int)(pm.getWidth() * pm.getHeight() * 1.5f)); // Guess at deflated size.
@@ -65,21 +56,30 @@ public class BlueNoiseGenerator extends ApplicationAdapter {
     {
         long startTime = System.currentTimeMillis();
 
-        double[][] wrapping = new double[64][64];
-        for (int i = 1; i <= 32; i++) {
-            wrapping[63][64 - i] = wrapping[63][i] = Math.exp(-0.5 * i * i / sigma2);
+        double[][] wrapping = new double[size][size];
+        final int hs = size >>> 1, end = size - 1;
+        for (int i = 1; i <= hs; i++) {
+            wrapping[end][size - i] = wrapping[end][i] = Math.exp(-0.5 * i * i / sigma2);
         }
-        for (int x = 1; x < 63; x++) {
-            for (int y = 1; y < 64; y++) {
-                wrapping[x][y] = wrapping[63][x] * wrapping[63][y];
+        for (int x = 1; x < end; x++) {
+            for (int y = 1; y < size; y++) {
+                wrapping[x][y] = wrapping[end][x] * wrapping[end][y];
             }
         }
         wrapping[0][0] = Double.POSITIVE_INFINITY;
 
+        final int limit = (size >>> 3) * (size >>> 3);
+        ArrayList<Coord> initial = new ArrayList<>(limit);
+        for (int i = 1; i <= limit; i++) {
+            initial.add(VanDerCorputQRNG.roberts(size, size, 0, 0, i));
+        }
+        rng.shuffleInPlace(initial);
+
+
         pm.setColor(SColor.BLACK);
         pm.fill();
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
                 pm.drawPixel(x, y, 0xFF);
             }
         }
@@ -110,7 +110,7 @@ public class BlueNoiseGenerator extends ApplicationAdapter {
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setTitle("SquidLib Tool: Blue Noise Generator");
-        config.setWindowedMode(width, height);
+        config.setWindowedMode(size, size);
         config.setWindowIcon(Files.FileType.Internal, "Tentacle-128.png", "Tentacle-64.png", "Tentacle-32.png", "Tentacle-16.png");
         new Lwjgl3Application(new BlueNoiseGenerator(), config);
     }
