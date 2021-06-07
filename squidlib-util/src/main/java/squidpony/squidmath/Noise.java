1f97c5dd4c138e1c9773beaee2395a358d8aff8e
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-06-06 18:48:25 -0700

    LayeredSpiral6D.

--- squidlib-util/src/main/java/squidpony/squidmath/Noise.java
+++ squidlib-util/src/main/java/squidpony/squidmath/Noise.java
@@ -3697,4 +3697,78 @@ final double x2 = x * -0.8761039177722382 + y * -0.4821223135928635, y2 = x * +0
             return n / ((1 << octaves) - 1.0);
         }
     }
+
+    public static class LayeredSpiral6D implements Noise6D {
+        protected int octaves;
+        protected Noise6D basis;
+        public double frequency;
+        public double lacunarity;
+        public LayeredSpiral6D() {
+            this(SeededNoise.instance);
+        }
+
+        public LayeredSpiral6D(Noise6D basis) {
+            this(basis, 2);
+        }
+
+        public LayeredSpiral6D(Noise6D basis, final int octaves) {
+            this(basis, octaves, 1.0);
+        }
+        public LayeredSpiral6D(Noise6D basis, final int octaves, double frequency) {
+            this(basis, octaves, frequency, 0.5);
+        }
+        public LayeredSpiral6D(Noise6D basis, final int octaves, double frequency, double lacunarity) {
+            this.basis = basis;
+            this.frequency = frequency;
+            this.octaves = Math.max(1, Math.min(63, octaves));
+            this.lacunarity = lacunarity;
+        }
+
+        @Override
+        public double getNoise(double x, double y, double z, double w, double u, double v) {
+            x *= frequency;
+            y *= frequency;
+            z *= frequency;
+            w *= frequency;
+            u *= frequency;
+            v *= frequency;
+            int s = 1;
+            double n = 0.0;
+            for (int o = 0; o < octaves; o++, s <<= 1) {
+                n += basis.getNoise(x, y, z, w, u, v) * s;
+                final double x2 = x * -0.0380778061544851 + y * -0.0303724527465320 + z * +0.4356293160913650 + w * +0.7933753851627695 + u * +0.4197685953950022 + v * +0.0894521710543777;
+                final double y2 = x * -0.0351875174538162 + y * -0.0850113478089023 + z * -0.2035454436153332 + w * -0.3017280518525850 + u * +0.6256003464393485 + v * +0.6883737459015009;
+                final double z2 = x * -0.3085763896526368 + y * -0.7945848211997655 + z * -0.4523290146687495 + w * +0.2204308565934835 + u * -0.0005929883712112 + v * -0.1508373488249853;
+                final double w2 = x * +0.7172834955411770 + y * +0.1566898043832359 + z * -0.5790337995177075 + w * +0.2870787399236230 + u * +0.1649382369085783 + v * -0.1393314322019783;
+                final double u2 = x * -0.5924507996312636 + y * +0.5326785531364373 + z * -0.3844530979937220 + w * +0.0722080755104951 + u * +0.3197386906135813 + v * -0.3372709605140012;
+                final double v2 = x * +0.1958822090885441 + y * -0.2344455917280123 + z * +0.2916686002573239 + w * -0.3847270179266404 + u * +0.5550232778653369 + v * -0.6058996139948896;
+                x = x2 * lacunarity + (o<<6); y = y2 * lacunarity + (o<<6); z = z2 * lacunarity + (o<<6);  w = w2 * lacunarity + (o<<6); u = u2 * lacunarity + (o<<6); v = v2 * lacunarity + (o<<6);
+            }
+            return n / ((1 << octaves) - 1.0);
+        }
+
+        @Override
+        public double getNoiseWithSeed(double x, double y, double z, double w, double u, double v, long seed) {
+            x *= frequency;
+            y *= frequency;
+            z *= frequency;
+            w *= frequency;
+            u *= frequency;
+            v *= frequency;
+            int s = 1;
+            double n = 0.0, i_s = 1.0 / lacunarity;
+            for (int o = 0; o < octaves; o++, s <<= 1) {
+                n += basis.getNoiseWithSeed(x, y, z, w, u, v, (seed += 0x9E3779B97F4A7C15L)) * s;
+                final double x2 = x * -0.0380778061544851 + y * -0.0303724527465320 + z * +0.4356293160913650 + w * +0.7933753851627695 + u * +0.4197685953950022 + v * +0.0894521710543777;
+                final double y2 = x * -0.0351875174538162 + y * -0.0850113478089023 + z * -0.2035454436153332 + w * -0.3017280518525850 + u * +0.6256003464393485 + v * +0.6883737459015009;
+                final double z2 = x * -0.3085763896526368 + y * -0.7945848211997655 + z * -0.4523290146687495 + w * +0.2204308565934835 + u * -0.0005929883712112 + v * -0.1508373488249853;
+                final double w2 = x * +0.7172834955411770 + y * +0.1566898043832359 + z * -0.5790337995177075 + w * +0.2870787399236230 + u * +0.1649382369085783 + v * -0.1393314322019783;
+                final double u2 = x * -0.5924507996312636 + y * +0.5326785531364373 + z * -0.3844530979937220 + w * +0.0722080755104951 + u * +0.3197386906135813 + v * -0.3372709605140012;
+                final double v2 = x * +0.1958822090885441 + y * -0.2344455917280123 + z * +0.2916686002573239 + w * -0.3847270179266404 + u * +0.5550232778653369 + v * -0.6058996139948896;
+                x = x2 * lacunarity + (o<<6); y = y2 * lacunarity + (o<<6); z = z2 * lacunarity + (o<<6);  w = w2 * lacunarity + (o<<6); u = u2 * lacunarity + (o<<6); v = v2 * lacunarity + (o<<6);
+
+            }
+            return n / ((1 << octaves) - 1.0);
+        }
+    }
 }
