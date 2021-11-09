02793fd5734a7641d4188086c9293b5c83a14904
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-11-08 20:20:19 -0800

    Making some progress on PyrNoise.
    It looks like https://i.imgur.com/FmUWTlv.png now.

--- squidlib-util/src/main/java/squidpony/squidmath/PyrNoise.java
+++ squidlib-util/src/main/java/squidpony/squidmath/PyrNoise.java
@@ -44,33 +44,53 @@ public class PyrNoise implements Noise.Noise2D, Noise.Noise3D,
         xFloor *= STEPX;
         yFloor *= STEPY;
         final int cap = hashPart1024(xFloor + STEPX, yFloor + STEPY, seed);
+        if(x == 0.5 && y == 0.5) return cap * (0x1.0040100401004p-10);
         double xd = x - 0.5;
         double yd = y - 0.5;
         double xa = Math.abs(xd);
         double ya = Math.abs(yd);
         if(xa < ya){
             // flat base, cap points up or down
-            if(yd > 0){
+            if(yd >= 0){
                 yFloor += STEPY << 1;
             }
-            x = (xd / ya + 1.0) * 0.5;
-            x *= x * (3 - 2 * x);
+//            x = (xd / ya + 1.0) * 0.5;
+//            x = 1.0 - x;
+            x += ya - 0.5;
             ya *= 2.0;
+            x /= ya;
+            x *= x * (3 - 2 * x);
+//            ya = 0.5 - ya;
+//            ya *= ya * 4.0;
+//            ya = 1.0 - ya;
             ya *= ya * (3 - 2 * ya);
-            return (ya * ((1 - x) * hashPart1024(xFloor, yFloor, seed) + x * hashPart1024(xFloor + STEPX + STEPX, yFloor, seed))
-                    + (1 - ya) * cap) * (0x1.0040100401004p-10);
+            double cc = (1 - ya) * cap;
+            return ((1 - x) * (ya * hashPart1024(xFloor, yFloor, seed) + cc)
+                    + x * (ya * hashPart1024(xFloor + STEPX + STEPX, yFloor, seed) + cc))
+                    * (0x1.0040100401004p-10);
+//            return (ya * ((1 - x) * hashPart1024(xFloor, yFloor, seed) + x * hashPart1024(xFloor + STEPX + STEPX, yFloor, seed))
+//                    + (1 - ya) * cap) * (0x1.0040100401004p-10);
         }
         else {
             // vertical base, cap points left or right
-            if(xd > 0){
+            if(xd >= 0){
                 xFloor += STEPX << 1;
             }
-            y = (yd / xa + 1.0) * 0.5;
-            y *= y * (3 - 2 * y);
+//            y = (yd / xa + 1.0) * 0.5;
+//            y = 1.0 - y;
+            y += xa - 0.5;
             xa *= 2.0;
+            y /= xa;
+            y *= y * (3 - 2 * y);
+//            xa = 0.5 - xa;
+//            xa *= xa * 4.0;
+//            xa = 1.0 - xa;
             xa *= xa * (3 - 2 * xa);
-            return (xa * ((1 - y) * hashPart1024(xFloor, yFloor, seed) + y * hashPart1024(xFloor, yFloor + STEPY + STEPY, seed))
-                    + (1 - xa) * cap) * (0x1.0040100401004p-10);
+            double cc = (1 - xa) * cap;
+            return ((1 - y) * (xa * hashPart1024(xFloor, yFloor, seed) + cc)
+                    + y * (xa * hashPart1024(xFloor, yFloor + STEPY + STEPY, seed) + cc)) * (0x1.0040100401004p-10);
+//            return (xa * ((1 - y) * hashPart1024(xFloor, yFloor, seed) + y * hashPart1024(xFloor, yFloor + STEPY + STEPY, seed))
+//                    + (1 - xa) * cap) * (0x1.0040100401004p-10);
         }
     }
 
