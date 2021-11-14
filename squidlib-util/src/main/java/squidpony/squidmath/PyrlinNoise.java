cc72b20d18cec3230202fde53bd3dfe79ee6ef80
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-11-14 15:35:34 -0800

    Debugging PyrlinNoise. Remove `true ||` to get actual status.

--- squidlib-util/src/main/java/squidpony/squidmath/PyrlinNoise.java
+++ squidlib-util/src/main/java/squidpony/squidmath/PyrlinNoise.java
@@ -40,7 +40,7 @@ public class PyrlinNoise extends ClassicNoise implements Noise.Noise2D, Noise.No
         double yd = y - 0.5;
         double xa = Math.abs(xd);
         double ya = Math.abs(yd);
-        if(xa < ya){
+        if(true || xa < ya){
             // flat base, cap points up or down
 
 //            x = (xd / ya + 1.0) * 0.5;
@@ -72,8 +72,8 @@ public class PyrlinNoise extends ClassicNoise implements Noise.Noise2D, Noise.No
 //            return ((1 - x) * (ya * hashPart1024(xFloor, yFloor, seed) + cc)
 //                    + x * (ya * hashPart1024(xFloor + STEPX + STEPX, yFloor, seed) + cc))
 //                    * (0x1.0040100401004p-10);
-            return (((1 - x) * (ya * gradCoord2D(seed, xFloor, yFloor, x, 1 - ya) + cc))
-                    + x * (ya * gradCoord2D(seed, xFloor + 2, yFloor, x - 1, 1 - ya) + cc)) * 0.7;
+            return (((1 - x) * (ya * gradCoord2D(seed, xFloor, yFloor, x, ya) + cc))
+                    + x * (ya * gradCoord2D(seed, xFloor + 2, yFloor, x - 1, ya) + cc)) * 0.7;
 //            return (ya * ((1 - x) * gradCoord2D(seed, xFloor, yFloor, x, ya) + x * gradCoord2D(seed, xFloor + 2, yFloor, 1 - x, ya))
 //                    + (1 - ya) * cap);
 //            return (ya * ((1 - x) * hashPart1024(xFloor, yFloor, seed) + x * hashPart1024(xFloor + STEPX + STEPX, yFloor, seed))
@@ -109,8 +109,8 @@ public class PyrlinNoise extends ClassicNoise implements Noise.Noise2D, Noise.No
 //            return ((1 - y) * (xa * hashPart1024(xFloor, yFloor, seed) + cc)
 //                    + y * (xa * hashPart1024(xFloor, yFloor + STEPY + STEPY, seed) + cc)) * (0x1.0040100401004p-10);
 
-            return (((1 - y) * (xa * gradCoord2D(seed, xFloor, yFloor, 1 - xa, y) + cc))
-                    + y * (xa * gradCoord2D(seed, xFloor, yFloor + 2, 1 - xa, y - 1) + cc)) * 0.7;
+            return (((1 - y) * (xa * gradCoord2D(seed, xFloor, yFloor, xa, y) + cc))
+                    + y * (xa * gradCoord2D(seed, xFloor, yFloor + 2, xa, y - 1) + cc)) * 0.7;
 //            return (xa * ((1 - y) * gradCoord2D(seed, xFloor, yFloor, xa, y) + y * gradCoord2D(seed, xFloor, yFloor + 2, xa, y - 1))
 //                    + (1 - xa) * cap);
 //            return (xa * ((1 - y) * hashPart1024(xFloor, yFloor, seed) + y * hashPart1024(xFloor, yFloor + 2, seed))
