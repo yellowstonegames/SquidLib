c08e916ea2318c2676f0817edb95c4d872d4ce50
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-01-01 20:36:24 -0800

    Update copyright; welcome to 2022!

--- squidlib-util/src/main/java/squidpony/squidmath/PermutedRNG.java
+++ squidlib-util/src/main/java/squidpony/squidmath/PermutedRNG.java
@@ -1,26 +1,19 @@
 /*
- * Ported to Java from the PCG library. Its copyright header follows:
+ * Copyright (c) 2022  Eben Howard, Tommy Ettinger, and contributors
  *
- * PCG Random Number Generation for C++
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * Copyright 2014 Melissa O'Neill <oneill@pcg-random.org>
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * For additional information about the PCG random number generation scheme,
- * including its license and other licensing options, visit
- *
- *     http://www.pcg-random.org
  */
 package squidpony.squidmath;
 
