7457c5b5292a68a90267a23de95cd7c6165eb919
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-10-25 20:17:25 -0700

    Super-sourcing! Emu needs it too!

--- squidlib-util/src/main/java/squidpony/emu/squidpony/LZSEncoding.java
+++ squidlib-util/src/main/java/squidpony/emu/squidpony/LZSEncoding.java
@@ -93,7 +93,18 @@ public final class LZSEncoding {
     public static String compressToEncodedURIComponent(String uncompressed) {
         if (uncompressed == null)
             return null;
-        return _compress(uncompressed, 6, keyStrUriSafe, 0);
+        String res = _compress(uncompressed, 6, keyStrUriSafe, 0);
+        switch (res.length() & 3) { // To produce valid URI-Encoding
+            case 1:
+                return res + "$$$";
+            case 2:
+                return res + "$$";
+            case 3:
+                return res + "$";
+            case 0:
+            default:
+                return res;
+        }
     }
     /**
      * Decompresses a String that had been compressed with {@link #compressToEncodedURIComponent(String)}.
