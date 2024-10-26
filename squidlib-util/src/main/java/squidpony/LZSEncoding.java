cfc2a5957d9b4cb50d1715e854ede66aa68e0848
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-10-25 19:46:03 -0700

    Fix URI mode for LZSEncoding. Update!
    Gradle is up to 8.10.2, anim8-gdx is up to 0.4.6 .

--- squidlib-util/src/main/java/squidpony/LZSEncoding.java
+++ squidlib-util/src/main/java/squidpony/LZSEncoding.java
@@ -123,7 +123,18 @@ public final class LZSEncoding {
     public static String compressToEncodedURIComponent(String uncompressed) {
         if (uncompressed == null)
             return null;
-        return _compress(uncompressed, 6, keyStrUriSafe);
+        String res = _compress(uncompressed, 6, keyStrUriSafe);
+        switch (res.length() & 3) { // To produce valid URI-encoding
+            case 1:
+                return res + "$$$";
+            case 2:
+                return res + "$$";
+            case 3:
+                return res + "$";
+            case 0:
+            default:
+                return res;
+        }
     }
     /**
      * Decompresses a String that had been compressed with {@link #compressToEncodedURIComponent(String)}.
